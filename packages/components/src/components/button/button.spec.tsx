91a4499c2d15b181c8d9d0c244751636284142db
Duncan Kolba <duncan.kolba@deutschebahn.com>
2023-01-27 10:49:11 +0100

    chore: VUE WIP47

--- packages/components/src/components/button/button.spec.tsx
+++ packages/components/src/components/button/button.spec.tsx
@@ -3,21 +3,21 @@ import AxeBuilder from '@axe-core/playwright';
 
 import { DBButton } from './index';
 
-test.describe('Button block desktop', () => {
+test.describe('DBButton component on desktop', () => {
 	// Old-school CRT monitor screensize
 	test.use({ viewport: { width: 1024, height: 768 } });
 
-	test('Button should contain text', async ({ mount }) => {
+	test('DBButton should contain text', async ({ mount }) => {
 		const component = await mount(<DBButton text="Test" />);
 		await expect(component).toContainText('Test');
 	});
 
-	test('Button should match screenshot', async ({ mount }) => {
+	test('DBButton should match screenshot', async ({ mount }) => {
 		const component = await mount(<DBButton text="Test" />);
 		await expect(component).toHaveScreenshot();
 	});
 
-	test('Button should only have icon', async ({ mount }) => {
+	test('DBButton should only have icon', async ({ mount }) => {
 		const component = await mount(
 			<DBButton icon="account" onlyIcon={true} />
 		);
@@ -25,23 +25,23 @@ test.describe('Button block desktop', () => {
 	});
 });
 
-test.describe('Button block mobile', () => {
+test.describe('DBButton component on mobile', () => {
 	// IPhone 13 / portrait screen size
 	test.use({ viewport: { width: 390, height: 884 } });
 
-	test('Button should contain text', async ({ mount }) => {
+	test('DBButton should contain text', async ({ mount }) => {
 		const component = await mount(<DBButton text="Test" />);
 		await expect(component).toContainText('Test');
 	});
 
-	test('Button should match screenshot', async ({ mount }) => {
+	test('DBButton should match screenshot', async ({ mount }) => {
 		const component = await mount(<DBButton text="Test" />);
 		await expect(component).toHaveScreenshot();
 	});
 });
 
-test.describe('A11y button component', () => {
-	test('button should not have any automatically detectable accessibility issues', async ({
+test.describe('DBButton component A11y', () => {
+	test('DBButton should not have any automatically detectable accessibility issues', async ({
 		page,
 		mount
 	}) => {
@@ -53,7 +53,10 @@ test.describe('A11y button component', () => {
 		expect(accessibilityScanResults.violations).toEqual([]);
 	});
 
-	test('icon only', async ({ page, mount }) => {
+	test('DBButton with icon only should not have any automatically detectable accessibility issues', async ({
+		page,
+		mount
+	}) => {
 		await mount(<DBButton icon="account" text="asdf" onlyIcon={true} />);
 		const accessibilityScanResults = await new AxeBuilder({ page })
 			.include('.db-button')
