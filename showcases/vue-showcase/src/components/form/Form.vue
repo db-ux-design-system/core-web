8ed2bf0cbed6fdd7fa57d7d15053effa85191bdf
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-02-01 13:34:20 +0100

    chore: update vue showcase

---
+++ showcases/vue-showcase/src/components/form/Form.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { DBButton, DBInput } from "../../../../../output/vue/vue3/src";
+import "./index.scss";
+
+import { ref } from "vue";
+const input = ref("");
+
+// eslint-disable-next-line no-alert
+const logAll = () => {
+	alert(
+		JSON.stringify({
+			input: input.value
+		})
+	);
+};
+</script>
+
+<template>
+	<div>
+		<div class="form-container">
+			<fieldset>
+				<form>
+					<p>DbInput:</p>
+					<DBInput
+						label="Infield"
+						name="input-name"
+						class="fullWidth"
+						:value="input"
+						:on-change="
+							($event) => {
+								input = $event.target.value;
+							}
+						"
+					/>
+				</form>
+			</fieldset>
+			<fieldset>
+				<p>DbButton:</p>
+				<DBButton variant="primary" @click="logAll()">
+					Hi from Showcase!
+				</DBButton>
+				<h2>Output</h2>
+				<dl>
+					<dt>inputs value</dt>
+					<dd>{{ input ? input : "No Input set" }}</dd>
+				</dl>
+			</fieldset>
+		</div>
+	</div>
+</template>
