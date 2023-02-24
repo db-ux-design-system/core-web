88ad0eadb19293b4a16fe32e838fef0eb89825ea
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-02-24 15:59:28 +0100

    fix: issue with playwright report

--- packages/components/test/playwright/config.ts
+++ packages/components/test/playwright/config.ts
@@ -18,7 +18,9 @@ const config = defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'github' : 'list',
+	reporter: process.env.CI
+		? [['github'], ['html', { open: 'never' }]]
+		: [['list'], ['html', { open: 'never' }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
