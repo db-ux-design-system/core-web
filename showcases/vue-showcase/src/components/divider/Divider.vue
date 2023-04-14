63d48cc1dbbde416b281f448fc70e8e637287084
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-04-14 11:56:30 +0200

    chore: add divider in showcases + docs + migration

---
+++ showcases/vue-showcase/src/components/divider/Divider.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DefaultComponent from "../DefaultComponent.vue";
+import defaultComponentVariants from "../../../../shared/divider.json";
+import { DBDivider, DBInfotext } from "../../../../../output/vue/vue3/src";
+
+const log = (exampleName: string) => {
+	// eslint-disable-next-line no-alert
+	alert(exampleName);
+};
+</script>
+
+<template>
+	<DefaultComponent title="Divider" :variants="defaultComponentVariants">
+		<template
+			#example="{ exampleIndex, variantIndex, exampleName, exampleProps }"
+		>
+			<DBInfotext size="small" variant="informational">
+				{{ exampleName }}</DBInfotext
+			>
+			<DBDivider :variant="exampleProps.variant" />
+		</template>
+	</DefaultComponent>
+</template>
