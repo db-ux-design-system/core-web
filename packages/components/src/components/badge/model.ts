a59191305a25e66389f9ef561a819da34a7cc8aa
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2023-07-21 16:46:36 +0200

    chore: add badge vue showcase and snapshots

--- packages/components/src/components/badge/model.ts
+++ packages/components/src/components/badge/model.ts
@@ -12,6 +12,10 @@ export interface DBBadgeDefaultProps {
 	 * Angular adds a comment with binding to the component which disables the :empty css selector.
 	 */
 	noText?: boolean;
+
+	/**
+	 * The corner attributes changes the position to absolute and adds a transform based on the placement.
+	 */
 	placement?:
 		| 'inline'
 		| 'corner-top-left'
