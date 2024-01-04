359df7150bcb371d28dad3a87624eea65aa9d9dc
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-01-04 10:46:27 +0100

    chore: tidy-up scripts

--- scripts/dev.js
+++ scripts/dev.js
@@ -34,7 +34,7 @@ inquirer
 	])
 
 	.then((answers) => {
-		let startCommand = 'npm-run-all -p start:foundations dev:sass';
+		let startCommand = 'npm-run-all -p dev:foundations dev:sass';
 		if (answers?.frameworks)
 			for (const answer of answers.frameworks) {
 				startCommand +=
