178e8479435add01abf6b24de30987c05819f243
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2024-01-08 10:03:57 +0100

    refactor: using a loader
    the previously used flag has been removed with Node 19; compare to stackoverflow.com/a/68621282

---
+++ showcases/patternhub/loader.js
@@ -0,0 +1,14 @@
+import { isBuiltin } from 'node:module';
+
+// Noinspection JSUnusedGlobalSymbols
+export const resolve = (
+	specifier,
+	context,
+	nextResolve // This function can be `async` too
+) =>
+	nextResolve(
+		isBuiltin(specifier) || specifier.endsWith('.js')
+			? specifier
+			: `${specifier}.js`,
+		context
+	);
