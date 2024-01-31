cd50823cbcac792646edc3d93143197ad7460cfc
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-01-31 12:47:05 +0100

    fix: issue for webkit

--- showcases/e2e/default.ts
+++ showcases/e2e/default.ts
@@ -27,7 +27,7 @@ export const getDefaultScreenshotTest = (
 				if (isAngular) {
 					config.maxDiffPixels = 1000;
 				} else if (isWebkit) {
-					config.maxDiffPixels = 50;
+					config.maxDiffPixels = 200;
 				} else {
 					config.maxDiffPixels = 1;
 				}
