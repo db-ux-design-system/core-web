0cbec86c3a7728ae2df474ac96caaec961c3ed70
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-01-31 11:52:49 +0100

    fix: issue with cropped header when clicking on link

--- showcases/patternhub/components/link-header/index.tsx
+++ showcases/patternhub/components/link-header/index.tsx
@@ -1,15 +1,13 @@
 import Link from 'next/link';
-import { DBButton } from '../src';
+import { DBIcon } from '../src';
 
 const LinkHeader = ({ id }) => {
 	if (id) {
 		return (
 			<>
 				<a className="anchor" id={id} />
-				<Link href={`#${id}`}>
-					<DBButton variant="text" noText icon="add_link">
-						{id}
-					</DBButton>
+				<Link href={`#${id}`} scroll={false}>
+					<DBIcon icon="add_link">{id}</DBIcon>
 				</Link>
 			</>
 		);
