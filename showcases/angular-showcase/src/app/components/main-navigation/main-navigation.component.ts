a63b4960ebd21fcf60fa6b1c526293f084ecc999
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2024-02-08 09:12:13 +0100

    fix: add missing NavigationContentDirective import

--- showcases/angular-showcase/src/app/components/main-navigation/main-navigation.component.ts
+++ showcases/angular-showcase/src/app/components/main-navigation/main-navigation.component.ts
@@ -3,11 +3,17 @@ import defaultComponentVariants from '../../../../../shared/main-navigation.json
 import { DefaultComponent } from '../default.component';
 import { DBMainNavigation } from '../../../../../../output/angular/src/components/main-navigation/main-navigation';
 import { DBNavigationItem } from '../../../../../../output/angular/src/components/navigation-item/navigation-item';
+import { NavigationContentDirective } from '../../../../../../output/angular/src/components/navigation-item/NavigationContent.directive';
 
 @Component({
 	selector: 'app-main-navigation',
 	templateUrl: './main-navigation.component.html',
-	imports: [DefaultComponent, DBMainNavigation, DBNavigationItem],
+	imports: [
+		DefaultComponent,
+		DBMainNavigation,
+		DBNavigationItem,
+		NavigationContentDirective
+	],
 	standalone: true
 })
 export class MainNavigationComponent {
