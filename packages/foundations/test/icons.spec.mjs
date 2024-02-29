95c3df170e0f05561f97d0074855d2b51be8840a
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-02-29 13:59:34 +0100

    fix: issue with icons for webkit

--- packages/foundations/test/icons.spec.mjs
+++ packages/foundations/test/icons.spec.mjs
@@ -3,6 +3,7 @@ import { expect, test } from '@playwright/test';
 test.describe('Icons', () => {
 	test(`should match screenshot`, async ({ page }) => {
 		await page.goto(`src/icons.html`);
+		await page.waitForTimeout(1000);
 		await expect(page).toHaveScreenshot({ fullPage: true });
 	});
 });
