d1ef0ac5a142aeadbeeff3a1d138a0deab049984
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-05-29 14:44:48 +0200

    feat: add additional colors

--- showcases/patternhub/pages/foundations/colors/overview.tsx
+++ showcases/patternhub/pages/foundations/colors/overview.tsx
@@ -3,14 +3,45 @@ import DefaultPage from '../../../components/default-page';
 import { COLORS } from '../../../components/src/shared/constants';
 import { DBInput } from '../../../components/src';
 
+const additionalColors = [
+	'yellow',
+	'orange',
+	'red',
+	'pink',
+	'violet',
+	'blue',
+	'cyan',
+	'turquoise',
+	'green'
+];
+const getAdditionalColors = () => {
+	const colors: string[] = [];
+
+	for (const color of additionalColors) {
+		colors.push(
+			`${color}-bg-lvl-1`,
+			`${color}-bg-lvl-2`,
+			`${color}-bg-lvl-3`,
+			`${color}-bg-transparent-semi`,
+			`${color}-bg-transparent-full`
+		);
+	}
+
+	return colors;
+};
+
 const ColorOverview = () => {
+	const addColors = getAdditionalColors();
 	const [search, setSearch] = useState<string>('');
+
 	return (
 		<DefaultPage>
 			<h1>Color Overview</h1>
 			<search>
 				<DBInput
-					label="Search"
+					label="Search for color"
+					variant="floating"
+					placeholder="neutral-bg"
 					type="search"
 					onChange={(event) => {
 						setSearch(event.target.value);
@@ -19,9 +50,11 @@ const ColorOverview = () => {
 			</search>
 			<h2>Overview</h2>
 			<div className="color-overview-container">
-				{COLORS.map((color) => (
-					<div className={`db-${color}`}>{color}</div>
-				))}
+				{[...COLORS, ...addColors]
+					.filter((color) => color.includes(search))
+					.map((color) => (
+						<div className={`db-${color}`}>{color}</div>
+					))}
 			</div>
 		</DefaultPage>
 	);
