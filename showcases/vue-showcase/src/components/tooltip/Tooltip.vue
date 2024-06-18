d67aa6e507767854bc2b27cdd4dc962aabc9c1fe
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-06-18 14:41:20 +0200

    fix: make exampleProps optional

--- showcases/vue-showcase/src/components/tooltip/Tooltip.vue
+++ showcases/vue-showcase/src/components/tooltip/Tooltip.vue
@@ -9,18 +9,18 @@ import { DBButton, DBTooltip } from "../../../../../output/vue/src";
 		<template
 			#example="{ exampleIndex, variantIndex, exampleName, exampleProps }"
 		>
-			<DBButton :describedbyid="exampleProps.id">
+			<DBButton :describedbyid="exampleProps?.id">
 				{{ exampleName }}
 				<DBTooltip
-					:width="exampleProps.width"
-					:emphasis="exampleProps.emphasis"
-					:placement="exampleProps.placement"
-					:animation="exampleProps.animation"
-					:delay="exampleProps.delay"
-					:variant="exampleProps.variant"
-					:id="exampleProps.id"
+					:width="exampleProps?.width"
+					:emphasis="exampleProps?.emphasis"
+					:placement="exampleProps?.placement"
+					:animation="exampleProps?.animation"
+					:delay="exampleProps?.delay"
+					:variant="exampleProps?.variant"
+					:id="exampleProps?.id"
 				>
-					{{ exampleProps.content }}
+					{{ exampleProps?.content }}
 				</DBTooltip>
 			</DBButton>
 		</template>
