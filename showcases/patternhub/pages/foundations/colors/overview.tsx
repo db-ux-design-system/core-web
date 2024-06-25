e146c571d99da74b5a6f010a8484c9998c8e6e92
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-06-25 09:35:39 +0200

    feat: adds separated semantic, background and color classes

--- showcases/patternhub/pages/foundations/colors/overview.tsx
+++ showcases/patternhub/pages/foundations/colors/overview.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import DefaultPage from '../../../components/default-page';
 import { COLORS } from '../../../components/src/shared/constants';
-import { DBInput } from '../../../components/src';
+import { DBInput, DBIcon } from '../../../components/src';
 
 const additionalColors = [
 	'yellow',
@@ -30,30 +30,94 @@ const getAdditionalColors = () => {
 	return colors;
 };
 
+const semanticColors = [
+	'neutral',
+	'critical',
+	'successful',
+	'warning',
+	'informational'
+];
+
+const backgroundColors = [
+	'lvl-1',
+	'lvl-2',
+	'lvl-3',
+	'transparent-full',
+	'transparent-semi'
+];
+
+const onBackgroundColors = ['default', 'weak', 'contrast', 'contrast-weak'];
+
 const ColorOverview = () => {
 	const additionalColors = getAdditionalColors();
 	const [search, setSearch] = useState<string>('');
 
 	return (
 		<DefaultPage>
-			<h1>Color Overview</h1>
-			<search>
-				<DBInput
-					label="Search for color"
-					placeholder="neutral-bg"
-					type="search"
-					onChange={(event) => {
-						setSearch(event.target.value);
-					}}
-				/>
-			</search>
-			<h2>Overview</h2>
-			<div className="color-overview-container">
-				{[...COLORS, ...additionalColors]
-					.filter((color) => color.includes(search))
-					.map((color) => (
-						<div className={`db-${color}`}>{color}</div>
+			<div>
+				<h1>Color Overview</h1>
+				<h2>Overview semantic color classes</h2>
+				<p>
+					These classes define the semantic colour set (only css
+					variables) for a container. Texts, icons, backgrounds and
+					components in it then automatically adapt to the colour set.
+				</p>
+				<div className="color-overview-container">
+					{semanticColors.map((semanticColor) => (
+						<div
+							className={`db-semantic-color-${semanticColor} db-bg-color-lvl-3`}>{`db-semantic-color-${semanticColor}`}</div>
 					))}
+				</div>
+				<h2>Overview background color classes</h2>
+				<p>
+					These classes define the type of background colour for a
+					container. The exact colour tone then results from the
+					current semantics (in root <b>neutral</b> by default). This
+					means that each of these background types exists for each
+					semantic colour.
+				</p>
+				<div className="color-overview-container">
+					{backgroundColors.map((backgroundColor) => (
+						<div
+							className={`db-bg-color-${backgroundColor}`}>{`db-bg-color-${backgroundColor}`}</div>
+					))}
+				</div>
+				<h2>Overview on background color classes</h2>
+				<p>
+					This class is used to define the colour for texts and icons.
+					As with the background colours, these are displayed
+					according to the current semantics.
+				</p>
+				<div className="color-overview-container">
+					{onBackgroundColors.map((onBackgroundColor) => (
+						<div className={`db-on-bg-color-${onBackgroundColor}`}>
+							<DBIcon icon="heart" />
+							{`db-on-bg-color-${onBackgroundColor}`}
+						</div>
+					))}
+				</div>
+				<h2>Overview combined classes</h2>
+				<p>
+					These shorthand classes combine <b>semantic</b>,{' '}
+					<b>background</b> and <b>on-background</b> classes
+				</p>
+				<search>
+					<DBInput
+						label="Search for color"
+						placeholder="neutral-bg"
+						type="search"
+						onChange={(event) => {
+							setSearch(event.target.value);
+						}}
+					/>
+				</search>
+				<div className="color-overview-container">
+					{[...COLORS, ...additionalColors]
+						.filter((color) => color.includes(search))
+						.map((color) => (
+							<div className={`db-${color}`}>{`db-${color}`}</div>
+						))}
+				</div>
 			</div>
 		</DefaultPage>
 	);
