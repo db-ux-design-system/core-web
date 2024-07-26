e024f31dedb5da32c09ed742fd6f104506b6af96
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-07-26 14:11:50 +0200

    Update checkbox.lite.tsx

--- packages/components/src/components/checkbox/checkbox.lite.tsx
+++ packages/components/src/components/checkbox/checkbox.lite.tsx
@@ -191,6 +191,11 @@ export default function DBCheckbox(props: DBCheckboxProps) {
 					DEFAULT_INVALID_MESSAGE}
 			</DBInfotext>
 
+			<!--
+	 		* https://www.davidmacd.com/blog/test-aria-describedby-errormessage-aria-live.html
+	 		* Currently VoiceOver isn't supporting changes from aria-describedby.
+	 		* This is an internal Fallback
+			//-->
 			<span data-visually-hidden role="status">
 				{state._voiceOverFallback}
 			</span>
