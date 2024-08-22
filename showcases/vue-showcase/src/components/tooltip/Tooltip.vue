01f39620a7fe58232b389410d1cad7ab067d9673
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-08-22 19:51:21 +0200

    Discard changes to showcases/vue-showcase/src/components/tooltip/Tooltip.vue

--- showcases/vue-showcase/src/components/tooltip/Tooltip.vue
+++ showcases/vue-showcase/src/components/tooltip/Tooltip.vue
@@ -9,7 +9,7 @@ import { DBButton, DBTooltip } from "../../../../../output/vue/vue3/src";
 		<template
 			#example="{ exampleIndex, variantIndex, exampleName, exampleProps }"
 		>
-			<DBButton :describedbyid="exampleProps.id" type="button">
+			<DBButton :describedbyid="exampleProps.id">
 				{{ exampleName }}
 				<DBTooltip
 					:width="exampleProps.width"
