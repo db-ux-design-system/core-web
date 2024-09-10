cb2503e82b2e8b84413ad68be4bef470681ae77d
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2024-09-10 07:02:02 +0200

    feat: provide migration tool

--- packages/foundations/scripts/migration/index.ts
+++ packages/foundations/scripts/migration/index.ts
@@ -2,6 +2,7 @@ import { globSync } from 'glob';
 import { replaceInFileSync, type ReplaceResult } from 'replace-in-file';
 import { type OptionsType } from '../types';
 import { colorUpdateQ32024 } from './color-update';
+import { iconUpdateQ32024 } from './icon-update';
 
 export const migrate = (options?: OptionsType) => {
 	if (options) {
@@ -14,6 +15,18 @@ export const migrate = (options?: OptionsType) => {
 		}).map((path) => path.replaceAll('\\', '/'));
 
 		for (const update of colorUpdateQ32024) {
+			const option = {
+				...update,
+				files: globPaths,
+				dry
+			};
+			const result: ReplaceResult[] = replaceInFileSync(option);
+			if (dry) {
+				console.log(result);
+			}
+		}
+
+		for (const update of iconUpdateQ32024) {
 			const option = {
 				...update,
 				files: globPaths,
