ac67c1e3e57bf177971ea491ee6bd7eea1f21065
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-09-16 08:55:32 +0200

    chore: try to harden screen reader tests to avoid duplication in macOS

--- showcases/screen-reader/default.ts
+++ showcases/screen-reader/default.ts
@@ -40,12 +40,29 @@ const flakyExpressions: Record<string, string> = {
 
 const cleanSpeakInstructions = (phraseLog: string[]): string[] =>
 	phraseLog.map((phrase) => {
-		let result = phrase
-			.split('. ')
+		const phraseParts = phrase.split('. ');
+		let result = phraseParts
 			.filter(
 				(sPhrase) =>
 					!standardPhrases.some((string) => sPhrase.includes(string))
 			)
+			.map((part, index) => {
+				// There is an issue with macOS duplicating some parts, we remove the duplicates here
+				let lastFoundIndex = 0;
+				for (const pPart of phraseParts) {
+					const pPartIndex = phraseParts.indexOf(pPart);
+					if (part.includes(pPart)) {
+						lastFoundIndex = pPartIndex;
+					}
+				}
+
+				if (lastFoundIndex !== index) {
+					return part.replace(phraseParts[lastFoundIndex], '');
+				}
+
+				return part;
+			})
+			.filter((part) => part.length > 0)
 			.join('. ');
 
 		// We need to replace specific phrases, as they are being reported differently on localhost and within CI/CD
@@ -141,7 +158,6 @@ export const testDefault = (defaultTestType: DefaultTestType) => {
 			'&color=neutral-bg-basic-level-1&density=regular'
 	};
 
-	const trimTitleForShortSnapshotName = title.slice(0, 10);
 	if (isWin()) {
 		test?.(title, async ({ page, nvda }, { retry }) => {
 			await runTest({
