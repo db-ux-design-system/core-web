3d8e7bcbb71880ecebe58931b738d90e147bfc5d
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-11-04 17:21:41 +0100

    fix: baseUrl for patternhub playwright

--- showcases/playwright.patternhub-config.ts
+++ showcases/playwright.patternhub-config.ts
@@ -16,7 +16,7 @@ const patternHubConfig: PlaywrightTestConfig = {
 		}
 	],
 	webServer: {
-		command: `cd patternhub && npm run preview`,
+		command: `npx http-server ../build-showcases${process.env.NEXT_PUBLIC_BASE_PATH ?? '/patternhub'}`,
 		port: 8080,
 		reuseExistingServer: !process.env.CI
 	},
