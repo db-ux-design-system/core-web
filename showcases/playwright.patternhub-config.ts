492a07acd4f9090c75ec4b9b96820d521bdabf8a
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-11-06 07:19:01 +0100

    test: running build patternhub

--- showcases/playwright.patternhub-config.ts
+++ showcases/playwright.patternhub-config.ts
@@ -16,7 +16,7 @@ const patternHubConfig: PlaywrightTestConfig = {
 		}
 	],
 	webServer: {
-		command: `npx http-server ../build-showcases${process.env.NEXT_PUBLIC_BASE_PATH ?? '/patternhub'}`,
+		command: `npx http-server ../build-showcases${process.env.CI ? '' : '/patternhub'}`,
 		port: 8080,
 		reuseExistingServer: !process.env.CI
 	},
