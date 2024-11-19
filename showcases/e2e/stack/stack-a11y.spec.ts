c3ac3a92af5a1387852315fedbd6b11230c9eadf
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-11-19 10:27:21 +0100

    chore: update from main

--- showcases/e2e/stack/stack-a11y.spec.ts
+++ showcases/e2e/stack/stack-a11y.spec.ts
@@ -1,7 +1,14 @@
 import { test } from '@playwright/test';
 // @ts-expect-error - required for playwright
-import { getA11yTest } from '../default.ts';
+import { runA11yCheckerTest, runAxeCoreTest } from '../default.ts';
+import { lvl3 } from '../fixtures/variants';
 
 test.describe('DBStack', () => {
-	getA11yTest({ path: '01/stack' });
+	runAxeCoreTest({ path: '01/stack' });
+	runAxeCoreTest({ path: '01/stack', color: lvl3 });
+	runAxeCoreTest({
+		path: '01/stack',
+		density: 'functional'
+	});
+	runA11yCheckerTest({ path: '01/stack' });
 });
