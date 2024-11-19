f9d2ab6f72ceb1a3a82aed438d438c2abc8134db
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-11-19 13:55:41 +0100

    fix: issue with linting

--- showcases/e2e/accordion/accordion-a11y.spec.ts
+++ showcases/e2e/accordion/accordion-a11y.spec.ts
@@ -1,17 +1,20 @@
 import { test } from '@playwright/test';
-// @ts-expect-error - required for playwright
-import { hasWebComponentSyntax, runA11yCheckerTest, runAxeCoreTest } from "../default.ts";
+import {
+	hasWebComponentSyntax,
+	runA11yCheckerTest,
+	runAxeCoreTest
+	// @ts-expect-error - required for playwright
+} from '../default.ts';
 import { lvl3 } from '../fixtures/variants';
 
-
-
 // https://stackoverflow.com/questions/78129019/can-you-have-an-li-in-a-autonomous-custom-element-with-the-parent-ul-not
-const skipAxe= hasWebComponentSyntax(process.env.showcase)
+const skipAxe = hasWebComponentSyntax(process.env.showcase);
 
 test.describe('DBAccordion', () => {
-	runAxeCoreTest({ path: '04/accordion',skipAxe });
-	runAxeCoreTest({ path: '04/accordion', color: lvl3,skipAxe });
-	runAxeCoreTest({ path: '04/accordion', density: 'functional',skipAxe });
+	runAxeCoreTest({ path: '04/accordion', skipAxe });
+	runAxeCoreTest({ path: '04/accordion', color: lvl3, skipAxe });
+	runAxeCoreTest({ path: '04/accordion', density: 'functional', skipAxe });
 	runA11yCheckerTest({
-		path: '04/accordion'});
+		path: '04/accordion'
+	});
 });
