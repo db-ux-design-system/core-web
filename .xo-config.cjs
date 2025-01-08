5ed7aa7d7530aec57972ae07ad9eaedaf6bef31f
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2025-01-08 12:58:10 +0100

    feat: noto sans eu (#3647)
    * feat: reduce fonts by including only EU characters
    
    * feat: reduce fonts by including only EU characters
    
    * Update packages/foundations/assets/fonts/README.md
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * Update packages/foundations/assets/fonts/README.md
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * Update packages/foundations/assets/fonts/README.md
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * Update packages/foundations/assets/fonts/generate-eu-fonts.ts
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
    
    * chore: update snapshots
    
    * Update unicode-eu.txt
    
    * chore: revert snapshots
    
    * chore: update unicodes
    
    * chore: update unicodes
    
    * chore: revert changes
    
    * chore: update snapshots
    
    ---------
    
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>

--- .xo-config.cjs
+++ .xo-config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
 	prettier: true,
-	ignores: ['./showcases/nuxt-showcase/**', './packages/migration/**'],
+	ignores: [
+		'./showcases/nuxt-showcase/**',
+		'./packages/migration/**',
+		'./packages/foundations/assets/**'
+	],
 	overrides: [
 		{
 			files: ['./showcases/angular-showcase/**'],
