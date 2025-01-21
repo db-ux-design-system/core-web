58ebdaffb74424e8a710526dd73064036f70ec19
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2025-01-21 07:39:05 +0100

    Update packages/migration/src/migration/v0.0.6-v0.0.7.ts
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>

--- packages/migration/src/migration/v0.0.6-v0.0.7.ts
+++ packages/migration/src/migration/v0.0.6-v0.0.7.ts
@@ -8,6 +8,11 @@ export const v006_v007: ReplaceInFileConfig[] = [
 	},
 	{
 		files: '',
+		from: /foundations\/build\/css/g,
+		to: 'foundations/build/styles'
+	},
+	{
+		files: '',
 		from: /foundations\/build\/scss/g,
 		to: 'foundations/build/styles'
 	}
