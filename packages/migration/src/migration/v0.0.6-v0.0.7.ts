4e378b6c7c0001d0d73a202d27e0fa4021873a50
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2025-01-21 07:52:53 +0100

    Update packages/migration/src/migration/v0.0.6-v0.0.7.ts
    Co-authored-by: Maximilian Franzke <787658+mfranzke@users.noreply.github.com>

--- packages/migration/src/migration/v0.0.6-v0.0.7.ts
+++ packages/migration/src/migration/v0.0.6-v0.0.7.ts
@@ -8,6 +8,21 @@ export const v006_v007: ReplaceInFileConfig[] = [
 	},
 	{
 		files: '',
+		from: /db-ui-42/g,
+		to: 'relative'
+	},
+	{
+		files: '',
+		from: /db-ui-foundations-/g,
+		to: ''
+	},
+	{
+		files: '',
+		from: /db-ui-foundations/g,
+		to: 'relative'
+	},
+	{
+		files: '',
 		from: /foundations\/build\/css/g,
 		to: 'foundations/build/styles'
 	},
