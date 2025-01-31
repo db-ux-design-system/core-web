d0a9645d25c9ba985e1c3f760674b362a7409329
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-01-31 14:13:10 +0100

    feat: changed import/exports of foundations and components to allow "exports" in package.json

---
+++ packages/foundations/src/icon-types.ts
@@ -0,0 +1,24 @@
+import { BaseIconTypes } from './base-icon-types.js';
+
+/** Provide default values for a string literal type but allow any other string as well.
+ * @example* type Keys = "DEFAULT" | LooseAutocomplete
+ */
+export type LooseAutocomplete = string & Record<never, never>;
+
+/** Assign property types from right to left.
+ * Think `Object.assign` for types.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+ * */
+
+export type Assign<Target, Source> = Omit<Target, keyof Source> & Source;
+
+export interface OverwriteIconsInterface {}
+
+type InternalIcons = Assign<
+	{
+		types: BaseIconTypes | LooseAutocomplete;
+	},
+	OverwriteIconsInterface
+>;
+
+export type IconTypes = InternalIcons['types'];
