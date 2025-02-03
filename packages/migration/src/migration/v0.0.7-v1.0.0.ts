ac01ca6ba921eab2cacefad060c9fb9eedc614ee
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-02-03 14:44:22 +0100

    chore: run migration on repo

--- packages/migration/src/migration/v0.0.7-v1.0.0.ts
+++ packages/migration/src/migration/v0.0.7-v1.0.0.ts
@@ -3,12 +3,17 @@ import type { ReplaceInFileConfig } from 'replace-in-file';
 export const v007_v100: ReplaceInFileConfig[] = [
 	{
 		files: '',
-		from: /container-color/g,
-		to: 'color'
+		from: /db-container-color/g,
+		to: 'db-color'
 	},
 	{
 		files: '',
-		from: /color-scheme/g,
-		to: 'mode'
+		from: /data-container-color/g,
+		to: 'data-color'
+	},
+	{
+		files: '',
+		from: /data-color-scheme/g,
+		to: 'data-mode'
 	}
 ];
