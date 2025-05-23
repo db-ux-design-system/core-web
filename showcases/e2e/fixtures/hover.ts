69d9f58e24c59b482ac0c2083a299bb490ee17af
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-05-23 08:37:34 +0200

    fix: issue with e2e-hover for web-components

--- showcases/e2e/fixtures/hover.ts
+++ showcases/e2e/fixtures/hover.ts
@@ -1,11 +1,18 @@
 /* eslint-disable no-await-in-loop,unicorn/prefer-dom-node-dataset */
 import { type Page } from '@playwright/test';
+import { hasWebComponentSyntax } from '../default';
+
+const webComponentSyntax = hasWebComponentSyntax(process.env.showcase);
 
 export const hoverPre = async (page: Page, selector: string) => {
 	const components = await page.locator('main').locator(selector).all();
 	for (const component of components) {
 		await component.evaluate((comp: HTMLElement) => {
-			comp.parentElement.dispatchEvent(new Event('mouseenter'));
+			(webComponentSyntax
+				? comp.parentElement.parentElement
+				: comp.parentElement
+			).dispatchEvent(new Event('mouseenter'));
+
 			comp.setAttribute('data-e2e-hover', 'true');
 		});
 	}
