d6187b1743c6dfb99b96d752682fb0b92d0f3b6e
michaelmkraus <michael.m.kraus@deutschebahn.com>
2025-05-27 14:12:16 +0200

    fix(link): eliminate leading and trailing whitespace by inlining Show wrapper

--- packages/components/src/components/link/link.lite.tsx
+++ packages/components/src/components/link/link.lite.tsx
@@ -35,7 +35,7 @@ export default function DBLink(props: DBLinkProps) {
 			target={props.target}
 			rel={props.rel}
 			role={props.role}
-			hrefLang={props.hreflang}
+			hreflang={props.hreflang}
 			aria-disabled={getBooleanAsString(props.disabled)}
 			tabIndex={props.disabled ? -1 : 0}
 			aria-selected={getBooleanAsString(props.selected)}
