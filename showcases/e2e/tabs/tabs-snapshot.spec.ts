999b5cd5652b2dfda2b911062eec8d6504ad485e
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-06-05 16:46:26 +0200

    chore: update tabs test

--- showcases/e2e/tabs/tabs-snapshot.spec.ts
+++ showcases/e2e/tabs/tabs-snapshot.spec.ts
@@ -1,36 +1,32 @@
 import { expect, test } from '@playwright/test';
 // @ts-expect-error - required for playwright
-import {
-	getDefaultScreenshotTest,
-	isAngular,
-	isVue,
-	runAriaSnapshotTest
-} from '../default.ts';
+import { getDefaultScreenshotTest, runAriaSnapshotTest } from '../default.ts';
 
 const path = '04/tabs';
-const showcase = process.env.showcase;
+
+const preScreenShot = async (page, project) => {
+	if (
+		(project.name === 'webkit' || project.name === 'mobile_safari') &&
+		process.env.showcase.startsWith('vue')
+	) {
+		// There is a bug in webkit where the scroll buttons are not visible 50% of the time
+		// Probably due to the scrollWidth or clientWidth not being calculated correctly
+		// Only in Vue
+		// TODO: Investigate further
+		test.skip();
+	}
+
+	const scrollRight = page.locator('[data-icon=chevron_right]');
+	await expect(scrollRight).toBeVisible();
+};
 
 test.describe('DBTabs', () => {
 	getDefaultScreenshotTest({
-		path
+		path,
+		preScreenShot
 	});
 	runAriaSnapshotTest({
 		path,
-		async preScreenShot(page, project) {
-			if (
-				(project.name === 'webkit' ||
-					project.name === 'mobile_safari') &&
-				(isAngular(showcase) || isVue(showcase))
-			) {
-				// There is a bug in webkit where the scroll buttons are not visible 50% of the time
-				// Probably due to the scrollWidth or clientWidth not being calculated correctly
-				// Only in Vue and Angular
-				// TODO: Investigate further
-				test.skip();
-			}
-
-			const scrollRight = page.locator('[data-icon=chevron_right]');
-			await expect(scrollRight).toBeVisible();
-		}
+		preScreenShot
 	});
 });
