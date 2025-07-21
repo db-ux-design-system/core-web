3173e25e1783a695542633b115450bcca8e613a5
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-07-21 08:47:28 +0200

    refactor: using long form of parameters for clarity

--- .config/.lintstagedrc.js
+++ .config/.lintstagedrc.js
@@ -2,7 +2,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-	'*.md': 'markdownlint -c .config/.markdown-lint.yml',
+	'*.md': 'markdownlint --config .config/.markdown-lint.yml',
 	// In case that we're changing the stylelints configuration files content, we would need to validate it
 	'.stylelintrc.*': 'stylelint --validate --allow-empty-input',
 	'stylelint.config.*': 'stylelint --validate --allow-empty-input',
