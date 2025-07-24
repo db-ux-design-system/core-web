a9bbfe203d5e70e785f96bc34074e3d1ce4995f4
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-07-24 10:54:38 +0200

    refactor: further adaptions

--- showcases/screen-reader/translations.ts
+++ showcases/screen-reader/translations.ts
@@ -11,7 +11,8 @@ export const translations: Record<string, string[]> = {
 	clickable: ['anklickbar'],
 	'has auto complete': ['mit Auto Vervollständigung'],
 	required: ['erforderlich'],
-	'Please fill out this field..': ['Fülle dieses Feld aus..'], // Codespell:ignore Feld
+	// Codespell:ignore Feld
+	'Please fill out this field..': ['Fülle dieses Feld aus..'],
 	unselected: ['nicht ausgewählt'],
 	selected: ['ausgewählt'],
 	'': ['. Nummernblock eingeschaltet'],
