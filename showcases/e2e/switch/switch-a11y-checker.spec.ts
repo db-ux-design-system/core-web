808eafc23de86bb2c3bb782a1aa6220bdae56e58
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-07-24 13:28:11 +0200

    fix: resolving issue

--- showcases/e2e/switch/switch-a11y-checker.spec.ts
+++ showcases/e2e/switch/switch-a11y-checker.spec.ts
@@ -3,8 +3,6 @@ import { runA11yCheckerTest } from '../default.ts';
 
 test.describe('DBSwitch', () => {
 	runA11yCheckerTest({
-		path: '03/switch',
-		// It's an issue in the tool: https://github.com/IBMa/equal-access/issues/842
-		aCheckerDisableRules: ['aria_attribute_valid']
+		path: '03/switch'
 	});
 });
