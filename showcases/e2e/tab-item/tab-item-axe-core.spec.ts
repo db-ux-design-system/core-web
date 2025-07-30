2ba8702c24d5f5ddc9464a364ba3551e4a7ff6aa
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-07-30 16:22:50 +0200

    Update tab-item-axe-core.spec.ts

--- showcases/e2e/tab-item/tab-item-axe-core.spec.ts
+++ showcases/e2e/tab-item/tab-item-axe-core.spec.ts
@@ -6,13 +6,6 @@ import { lvl3 } from '../fixtures/variants';
 // TODO: There might be an issue in our implementation of which elements get which roles
 // So we disabled "aria-allowed-role" for now
 const axeDisableRules = ['aria-allowed-role'];
-const aCheckerDisableRules = [
-	'aria_child_tabbable',
-	'input_checkboxes_grouped',
-	'aria_role_valid'
-];
-// TODO: We skip this for now until mitosis output is correct
-const skipChecker = isStencil(process.env.showcase);
 
 test.describe('DBTabItem', () => {
 	runAxeCoreTest({ path: '04/tab-item', axeDisableRules });
