7c72f06df008c3424d57989dc1bc1b6ab1c764f6
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-07-30 16:33:14 +0200

    Update textarea-axe-core.spec.ts

--- showcases/e2e/textarea/textarea-axe-core.spec.ts
+++ showcases/e2e/textarea/textarea-axe-core.spec.ts
@@ -2,9 +2,6 @@ import { test } from '@playwright/test';
 import { runAxeCoreTest } from '../default.ts';
 import { lvl3 } from '../fixtures/variants';
 
-// TODO: disabled element_scrollable_tabbable it's a false-positive: https://github.com/IBMa/equal-access/issues/1911
-const aCheckerDisableRules = ['element_scrollable_tabbable'];
-
 test.describe('DBTextarea', () => {
 	runAxeCoreTest({ path: '03/textarea' });
 	runAxeCoreTest({ path: '03/textarea', color: lvl3 });
