467423bd7243d84a555284450370ae72416e6427
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-07-30 16:27:12 +0200

    Update link-axe-core.spec.ts

--- showcases/e2e/link/link-axe-core.spec.ts
+++ showcases/e2e/link/link-axe-core.spec.ts
@@ -2,10 +2,6 @@ import { test } from '@playwright/test';
 import { isStencil, runAxeCoreTest } from '../default.ts';
 import { lvl3 } from '../fixtures/variants';
 
-const aCheckerDisableRules = isStencil(process.env.showcase)
-	? ['text_contrast_sufficient', 'aria_attribute_valid']
-	: ['aria_attribute_valid']; // TODO: This is a false positive -> add an issue in https://github.com/IBMa/equal-access
-
 test.describe('DBLink', () => {
 	runAxeCoreTest({ path: '02/link' });
 	runAxeCoreTest({ path: '02/link', color: lvl3 });
