05def112da85866d95a8ea7dcd5e373ac4d45908
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-08-03 08:25:36 +0200

    test: enforce relevant changes for a screen reader test

--- packages/components/src/components/page/page.lite.tsx
+++ packages/components/src/components/page/page.lite.tsx
@@ -69,7 +69,7 @@ export default function DBPage(props: DBPageProps) {
 			data-fonts-loaded={getBooleanAsString(state.fontsLoaded)}>
 			<Slot name="header" />
 			<main class={cls('db-main', props.mainClass)}>
-				{props.children}
+				test{props.children}
 			</main>
 			<Slot name="footer" />
 		</div>
