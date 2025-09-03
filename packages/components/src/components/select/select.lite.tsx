fd791ee6a4713efd478d054cbb9860071ec86696
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2025-09-03 17:42:22 +0200

    Revert "refactor: let's give this a try"
    This reverts commit 8ff0b448830805dba139ca4c9fca050035eb8ad3.

--- packages/components/src/components/select/select.lite.tsx
+++ packages/components/src/components/select/select.lite.tsx
@@ -163,12 +163,6 @@ export default function DBSelect(props: DBSelectProps) {
 		state._invalidMessageId = mId + DEFAULT_INVALID_MESSAGE_ID_SUFFIX;
 		state._placeholderId = mId + DEFAULT_PLACEHOLDER_ID_SUFFIX;
 		state._invalidMessage = props.invalidMessage || DEFAULT_INVALID_MESSAGE;
-
-		useTarget({
-			angular: () => {
-				this.writeValue?.(this.value?.());
-			}
-		});
 	});
 
 	onUpdate(() => {
