4bf4e75e1e7d67c2ddd7b9635971903f170c1bfc
Maximilian Franzke <maximilian.franzke@deutschebahn.com>
2025-09-08 13:23:28 +0200

    refactor: another optimization

--- packages/foundations/postcss.config.cjs
+++ packages/foundations/postcss.config.cjs
@@ -1,10 +1,7 @@
 module.exports = {
 	plugins: [
 		require('postcss-inline-svg')({
-			paths: [
-				'../../node_modules/@db-ux/db-theme-icons/build/assets/',
-				'../../node_modules/@db-ux-inner-source/db-theme-icons/build/assets/'
-			]
+			paths: ['./assets/icons/', '../../assets/icons/']
 		}),
 		require('cssnano')({
 			preset: [
