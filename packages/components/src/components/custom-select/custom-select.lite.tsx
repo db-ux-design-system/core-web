0a65aab72bbe7f9f0421f78fd25ec3d57ad473f9
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-09-25 08:24:41 +0200

    fix: placeholder layout shift issue for select components

--- packages/components/src/components/custom-select/custom-select.lite.tsx
+++ packages/components/src/components/custom-select/custom-select.lite.tsx
@@ -1111,6 +1111,7 @@ export default function DBCustomSelect(props: DBCustomSelectProps) {
 			</Show>
 
 			<span
+				class="db-custom-select-placeholder"
 				aria-hidden={getBooleanAsString(true)}
 				id={state._placeholderId}>
 				{props.placeholder ?? props.label}
