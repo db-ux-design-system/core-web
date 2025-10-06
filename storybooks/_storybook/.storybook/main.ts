7e6c781813af6a13a1f52a90a8489f57d509bbb2
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-10-06 10:12:35 +0200

    test: storybook published along with patternhub

---
+++ storybooks/_storybook/.storybook/main.ts
@@ -0,0 +1,68 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+import { dirname, join } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+	return dirname(require.resolve(join(value, 'package.json')));
+}
+const config: StorybookConfig = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: ['@storybook/addon-docs'],
+	framework: {
+		name: getAbsolutePath('@storybook/react-vite'),
+		options: {}
+	},
+	async viteFinal(config) {
+		const { mergeConfig } = await import('vite');
+
+
+		const baseUrl = process.env.BASE_URL || '';
+		return mergeConfig(config, {
+			// TODO: Remove `/storybook` after removing patternhub
+			base: `${baseUrl}/storybook`
+		});
+	},
+	refs: (_, { configType }) => {
+		if (configType === 'DEVELOPMENT') {
+			return {
+				angular: {
+					title: 'Angular',
+					url: 'http://localhost:6006',
+					expanded: false
+				},
+				react: {
+					title: 'React',
+					url: 'http://localhost:6005',
+					expanded: false
+				},
+				vue: {
+					title: 'Vue',
+					url: 'http://localhost:6007',
+					expanded: false
+				}
+			};
+		}
+		return {
+			angular: {
+				title: 'Angular',
+				url: 'BASE_URL/angular-storybook',
+				expanded: false
+			},
+			react: {
+				title: 'React',
+				url: 'BASE_URL/react-storybook',
+				expanded: false
+			},
+			vue: {
+				title: 'Vue',
+				url: 'BASE_URL/vue-storybook',
+				expanded: false
+			}
+		};
+	}
+};
+export default config;
---
+++ storybooks/angular-storybook/.storybook/main.ts
@@ -0,0 +1,38 @@
+import { StorybookConfig } from '@storybook/angular';
+import * as webpack from 'webpack';
+
+import { dirname, join } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+	return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const BASE_URL = process.env.BASE_URL || '/BASE_URL/angular/';
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [],
+	framework: {
+		name: getAbsolutePath('@storybook/angular'),
+		options: {}
+	},
+	webpackFinal: async (config) => {
+		if (config.output) {
+			config.output.publicPath = BASE_URL;
+		} else {
+			config.output = { publicPath: BASE_URL };
+		}
+		config.plugins = [
+			...(config.plugins || []),
+			new webpack.DefinePlugin({
+				'process.env.BASE_URL': JSON.stringify(BASE_URL)
+			})
+		];
+		return config;
+	}
+};
+export default config;
