bb15ddb5942250ec5b0573b9fd74085dbb191e40
nmerget <nmerget@users.noreply.github.com>
2025-10-06 13:17:16 +0000

    deploy: d4bfc6d8b681117c732fda4d4613e428acd4126c

--- review/test-storybooks/react-storybook/assets/Header.stories-Dj4pDlyy.js
+++
@@ -1,7 +0,0 @@
-import{H as a}from"./Header-B7B7mi9E.js";import"./Button-CSAN_N_m.js";import"./iframe-nb7EqBRI.js";import"./preload-helper-Crdds7VR.js";const{fn:r}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:r(),onLogout:r(),onCreateAccount:r()}},e={args:{user:{name:"Jane Doe"}}},o={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source}}};const p=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,p as __namedExportsOrder,m as default};
