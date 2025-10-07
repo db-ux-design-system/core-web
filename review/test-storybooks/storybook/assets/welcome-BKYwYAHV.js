6774546a54ea533572959f95d18943a705719f0a
nmerget <nmerget@users.noreply.github.com>
2025-10-07 08:23:15 +0000

    deploy: 3064b818ac4f7cc15d379653a41918f3a5195b86

---
+++ review/test-storybooks/storybook/assets/welcome-BKYwYAHV.js
@@ -0,0 +1,3 @@
+import{j as t,M as m}from"./blocks-C_vze-to.js";import{useMDXComponents as r}from"./index-9R_mjXwN.js";import"./preload-helper-C1xAS4OG.js";import"./iframe-BVkvDxTA.js";import"./index-NYIa3FwG.js";function o(e){const n={h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Welcome"}),`
+`,t.jsx(n.h1,{id:"welcome",children:"Welcome"}),`
+`,t.jsx(n.p,{children:"XXX"})]})}function a(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{a as default};
