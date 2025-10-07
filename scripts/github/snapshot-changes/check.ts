0550347d79630464ba8ac9e7e36d81aaa7cdf3fb
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-10-07 07:27:39 +0200

    chore: revert logs inside check.ts

--- scripts/github/snapshot-changes/check.ts
+++ scripts/github/snapshot-changes/check.ts
@@ -9,18 +9,12 @@ export const checkSnapshotChanges = (aria = true, branch = ''): boolean => {
 			.split('\n')
 			.filter(Boolean);
 
-		console.log(`Changed files:`, changedFiles);
-
 		// Check if any file in __snapshots__ with the name *-aria-snapshot.yml has changed
-		const ariaSnapshots = changedFiles.some(
+		return changedFiles.some(
 			(file) =>
 				file.startsWith('__snapshots__') &&
 				file.endsWith(aria ? '-aria-snapshot.yaml' : '-screenshot.png')
 		);
-
-		console.log(`ariaSnapshots changed`, ariaSnapshots);
-
-		return ariaSnapshots;
 	} catch (error) {
 		console.error('Error while checking for changes:', error.message);
 		return false;
