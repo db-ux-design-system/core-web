34462c37236fffa99db982647c7a0a267715061c
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-10-08 08:47:56 +0200

    Apply suggestions from code review

--- scripts/tests/github/get-release.spec.ts
+++ scripts/tests/github/get-release.spec.ts
@@ -68,7 +68,7 @@ describe('build-gh-page', () => {
 		}
 
 		expect(result.toString().trim()).toEqual(
-			'Command failed: npx --no tsx github/get-release.ts\nBots have no permission to publish!'
+			'Command failed: npx --no tsx github/get-release.ts\nDependabot has no permission to publish!'
 		);
 	});
 });
