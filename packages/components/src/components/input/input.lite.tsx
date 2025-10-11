3401fb72815af1fdb3e681e7b38cfd6f76de8b10
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2025-10-11 17:26:48 +0200

    refactor: added missing types

--- packages/components/src/components/input/input.lite.tsx
+++ packages/components/src/components/input/input.lite.tsx
@@ -277,7 +277,7 @@ export default function DBInput(props: DBInputProps) {
 				aria-describedby={props.ariaDescribedBy ?? state._descByIds}
 				// iOS Safari Voiceover input:is([type="date"], [type="datetime-local"], [type="time"]) hack
 				role={
-					['datetime-local', 'date', 'time'].includes(
+					['datetime-local', 'date', 'time', 'week', 'month'].includes(
 						props.type ?? ''
 					) && state.isIOSSafari()
 						? 'textbox'
