6dc11e49008235bd48446b6784670485cfabd72b
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-11-07 12:36:07 +0100

    refactor: upgraded storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import xoConfig from 'eslint-config-xo';
 import { defineConfig, globalIgnores } from 'eslint/config';
