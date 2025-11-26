25d45ec5377a8845cd469541a8a1c9c0a975b75b
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-11-26 07:46:13 +0100

    refactor: prevent warning
    Refactor the code to use ESM-compatible imports. Replace the CommonJS require usage with dynamic imports or other ESM-compatible approaches.

--- storybooks/angular-storybook/.storybook/main.ts
+++ storybooks/angular-storybook/.storybook/main.ts
@@ -6,8 +6,11 @@ import { dirname, join } from 'node:path';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
-	return dirname(require.resolve(join(value, 'package.json')));
+async function getAbsolutePath(value: string) {
+	const pkgPath = (await import(join(value, 'package.json'))) as {
+		default: string;
+	};
+	return dirname(pkgPath.default);
 }
 
 const config: StorybookConfig = {
