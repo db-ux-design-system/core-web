b1aa65a3ab6eb2b52a4c6f613defc848c55cac84
nmerget <nmerget@users.noreply.github.com>
2025-12-01 01:44:19 +0000

    deploy: 4a84b6e94e67827849829345c746b6fa1ebc2516

--- version/v3.0.4/_next/static/chunks/pages/foundations/icons-023beb48613b00b4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8609],{74146:(_,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>t});var u=s(6029),e=s(51617);let t=()=>(0,u.jsx)(e.A,{})},88868:(_,n,s)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/foundations/icons",function(){return s(74146)}])}},_=>{_.O(0,[4256,310,7830,3118,3998,636,6593,8792],()=>_(_.s=88868)),_N_E=_.O()}]);
