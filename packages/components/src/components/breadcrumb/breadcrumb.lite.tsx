77733aacd4d421023b78f3cae03f4e22a77ba2bb
Sarah Bräuning <sarah.braeuning@deutschebahn.com>
2025-12-05 09:13:26 +0100

    fix(breadcrumb): update useRef type to allow any for broader compatibility

--- packages/components/src/components/breadcrumb/breadcrumb.lite.tsx
+++ packages/components/src/components/breadcrumb/breadcrumb.lite.tsx
@@ -19,7 +19,7 @@ useDefaultProps<DBBreadcrumbProps>({
 });
 
 export default function DBBreadcrumb(props: DBBreadcrumbProps) {
-	const _ref = useRef<HTMLElement | null>(null);
+	const _ref = useRef<HTMLElement | any>(null);
 
 	const state = useStore<DBBreadcrumbState>({
 		isExpanded: false,
