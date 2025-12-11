fd942d3e32015348abd1181700e0f07b2948a3d0
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-12-11 07:37:00 +0100

    refactor: updated storybook to the newest version

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 // For more info, see https://github.com/storybookjs/storybook/tree/next/code/lib/eslint-plugin#configuration-eslintconfigcmjs
 // import storybook from "eslint-plugin-storybook";
 
