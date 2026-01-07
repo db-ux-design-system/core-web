498a8aadc92b051b3ee267791f219c17a7b37938
mfranzke <mfranzke@users.noreply.github.com>
2026-01-07 16:28:15 +0000

    deploy: da31cca280b77c12509266cda35d3b67fb9b9b13

---
+++ review/refactor-simplification/composition-storybook/assets/formatter-QJ4M4OGQ-BJb9-l8Y.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-kzaXiKqb.js";import{b as o}from"./iframe-CqC-ZBW2.js";import"./preload-helper-B88cwfic.js";import"./index-COfsalD8.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
