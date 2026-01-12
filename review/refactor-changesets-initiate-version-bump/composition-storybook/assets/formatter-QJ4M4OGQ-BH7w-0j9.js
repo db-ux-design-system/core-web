3e1ff978dc17150f85f92eae0ea599807aa99063
mfranzke <mfranzke@users.noreply.github.com>
2026-01-12 10:10:19 +0000

    deploy: 11cd098a229c709d3935c873b3e23c317710652a

---
+++ review/refactor-changesets-initiate-version-bump/composition-storybook/assets/formatter-QJ4M4OGQ-BH7w-0j9.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-C3nSLArd.js";import{b as o}from"./iframe-kw_nl2ed.js";import"./preload-helper-CYfZnJVk.js";import"./index-gc1gWTBA.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
