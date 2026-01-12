488ff5247bfb4eb0aab99ee62d7c7a19c3c2a91a
nmerget <nmerget@users.noreply.github.com>
2026-01-12 11:42:41 +0000

    deploy: ae2d2dfbbb823feed0c5b78a7b92e8a0df8acaaf

---
+++ review/feat-shell/composition-storybook/assets/formatter-QJ4M4OGQ-DcUntR4W.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-7nKf3oQX.js";import{b as o}from"./iframe-m3WX6spr.js";import"./preload-helper-Ck2ouxx4.js";import"./index-Rox2aSSE.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
