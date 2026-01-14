9b3ba45a9a30b23597b1d2912ba3fda2a64cfa3a
nmerget <nmerget@users.noreply.github.com>
2026-01-14 07:00:17 +0000

    deploy: bbf1efc2a714a6853bf7b719a8e804b06a9a7f4e

---
+++ review/fix-guidepup-tests/vue-storybook/assets/formatter-QJ4M4OGQ-C7OAYhHz.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-GHJI37HO-DLk4E2b7.js";import{O as o}from"./iframe-CMzJRbH-.js";import"./preload-helper-CrFGrhiy.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
