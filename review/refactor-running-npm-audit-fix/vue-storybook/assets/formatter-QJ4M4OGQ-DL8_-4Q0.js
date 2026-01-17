abee9e610ebd5a8a0f04347d4204034816d2369c
mfranzke <mfranzke@users.noreply.github.com>
2026-01-17 07:07:58 +0000

    deploy: b202d9be5704e436a264b7193adbd7823d707fb7

---
+++ review/refactor-running-npm-audit-fix/vue-storybook/assets/formatter-QJ4M4OGQ-DL8_-4Q0.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-GHJI37HO-BDlFLRe_.js";import{O as o}from"./iframe-CpX-re4K.js";import"./preload-helper-CFgWGzQ2.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
