9457c27a893811b62140d29e97bd48c4aed4f58d
mfranzke <mfranzke@users.noreply.github.com>
2026-01-17 19:26:43 +0000

    deploy: e0d4f539e07f6e6adbf9668ad5c713b01743a8c9

---
+++ review/copilot/configure-playwright-fonts/react-storybook/assets/formatter-QJ4M4OGQ-CBE3dy8u.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-THj-T8CM.js";import{F as t}from"./iframe-BtBVGwQD.js";import"./preload-helper-bHMdYvUY.js";import"./react-18-Dh3sk6hA.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
