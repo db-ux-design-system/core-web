0c72ad4866ff1ebc9665889c5b0e516a81f635c4
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-01-21 10:02:40 +0100

    chore: remove `contain-intrinsic-size` for patternhub because we deliver it to Marketingportal

--- packages/components/test/playwright/boilerplate/index.ts
+++ packages/components/test/playwright/boilerplate/index.ts
@@ -2,4 +2,3 @@
 /* eslint  import/no-extraneous-dependencies: 0 */
 import '@db-ux/core-components/build/styles/rollup.css';
 import '@db-ux/core-foundations/build/styles/colors/classes/all.css';
-import '../test-optimizations.css';
