8b0e13cf1254e60da647781ea1e0c44579ee466b
sarahbrng <sarahbrng@users.noreply.github.com>
2026-01-21 12:22:43 +0000

    deploy: 3f8fdb50ada43e03abc53e8b4936afaca92fcc44

---
+++ review/1425-footer-concept-phase-2/react-storybook/assets/formatter-QJ4M4OGQ-XQtBYGrD.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-BZk8XSlW.js";import{F as t}from"./iframe-Bbc1gQEh.js";import"./preload-helper-Wao4K5wC.js";import"./react-18-BKP1Grv5.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
---
+++ review/1425-footer-concept-phase-2/vue-storybook/assets/formatter-QJ4M4OGQ-CHVBTdY6.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-GHJI37HO-CMgNJ62d.js";import{O as o}from"./iframe-CXDGy4UN.js";import"./preload-helper-J29wahVO.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
