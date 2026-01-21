4575735624cc8fda789a2e9e468b39a4a648c312
nmerget <nmerget@users.noreply.github.com>
2026-01-21 08:29:24 +0000

    deploy: 574136244c68adb7343dcb18b03c191e4348d29c

---
+++ review/copilot/fix-4206/composition-storybook/assets/formatter-QJ4M4OGQ-Ck_Ibod0.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-6PzzEtKv.js";import{b as o}from"./iframe-C_OheHlM.js";import"./preload-helper-Cjj7g4Lz.js";import"./index-BshZ2gA4.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
