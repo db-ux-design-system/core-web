59556ddae861e5feed9f0d0d091150ba1c56bba1
sarahbrng <sarahbrng@users.noreply.github.com>
2026-01-22 13:33:33 +0000

    deploy: 64b3acb1fdbdae48dfdce91ba5a92741c88a745a

---
+++ review/5285-describe-required-fields-that-add-todo-placeholder-elements-when-omitted/react-storybook/assets/formatter-QJ4M4OGQ-DMpuJpR8.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-BMy8Unpr.js";import{F as t}from"./iframe-B_bWLBD0.js";import"./preload-helper-kqTXPUif.js";import"./react-18-CdQFAS4a.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
