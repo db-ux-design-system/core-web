3aeb32a0b2b30e8b8a63b754efd6bbcadb5ccbef
mfranzke <mfranzke@users.noreply.github.com>
2026-01-23 15:28:14 +0000

    deploy: 75490b2dd8ab024fb8bdcee944f710f00e9db230

---
+++ review/refactor-linter-feedback/react-storybook/assets/formatter-QJ4M4OGQ-C9gAcJBq.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-CgiDoJyq.js";import{F as t}from"./iframe-DomxRl75.js";import"./preload-helper-B3hn64fJ.js";import"./react-18-DCP3ZoOp.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
