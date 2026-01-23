ea63646bd0b8e36e228e4c9cb12ecd29d1814d9f
nmerget <nmerget@users.noreply.github.com>
2026-01-23 09:41:06 +0000

    deploy: f2287063e18d4a4ef8f85a424443ee21ece25d15

---
+++ review/mfranzke-patch-1/react-storybook/assets/formatter-QJ4M4OGQ-D9rhD7di.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-CDFmm0eS.js";import{F as t}from"./iframe-C48mOW-H.js";import"./preload-helper-wZs71wXw.js";import"./react-18-B197-D8J.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
