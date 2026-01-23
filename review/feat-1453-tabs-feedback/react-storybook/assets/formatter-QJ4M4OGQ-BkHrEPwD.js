6382681bab74c227d89a97dce1375edca89a4718
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-01-23 08:53:11 +0000

    deploy: 6aedec71069415ac16f9317df548d2defab69a82

--- review/feat-1453-tabs-feedback/react-storybook/assets/formatter-QJ4M4OGQ-BkHrEPwD.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-GHJI37HO-5dqIcC1C.js";import{F as t}from"./iframe-Dv1AEdk_.js";import"./preload-helper--tLyh50B.js";import"./react-18-BC9V7eN0.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
