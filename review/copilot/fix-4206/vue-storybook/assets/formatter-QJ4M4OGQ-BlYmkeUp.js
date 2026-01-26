5a4c305a1a21ba6c2c35be44fef87545c57cd6b9
mfranzke <mfranzke@users.noreply.github.com>
2026-01-26 07:12:30 +0000

    deploy: a297a086494f585293f2f4a6fae76b472205abd2

---
+++ review/copilot/fix-4206/vue-storybook/assets/formatter-QJ4M4OGQ-BlYmkeUp.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-GHJI37HO-B5TNDHB0.js";import{O as o}from"./iframe-B6GpEZsl.js";import"./preload-helper-BxASG2TS.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
