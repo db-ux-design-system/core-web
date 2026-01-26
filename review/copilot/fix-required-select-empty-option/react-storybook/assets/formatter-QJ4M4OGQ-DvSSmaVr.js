790a52719f83d8dce0498b2350eafe01eb40b48c
mfranzke <mfranzke@users.noreply.github.com>
2026-01-26 12:15:29 +0000

    deploy: b0b24ef509c12f9bdae6a6432b7bf3f959ffc1cf

---
+++ review/copilot/fix-required-select-empty-option/react-storybook/assets/formatter-QJ4M4OGQ-DvSSmaVr.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-I2kxVPDt.js";import{F as t}from"./iframe-CL1edeO0.js";import"./preload-helper-BaiVWYx2.js";import"./react-18-CP_GrjV_.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
