24885a708ceb3956290984c9ea59439237615705
mfranzke <mfranzke@users.noreply.github.com>
2026-01-27 07:46:32 +0000

    deploy: 40b59e3c8b32cdfcea4e0916a28d19028cf23d31

---
+++ review/copilot/configure-playwright-fonts/composition-storybook/assets/formatter-EIJCOSYU-B3oUinzd.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-BRuk_s7q.js";import{b as o}from"./iframe-B1iEvQE3.js";import"./preload-helper-BZeZKKgi.js";import"./index-B1MuSbXl.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
---
+++ review/copilot/configure-playwright-fonts/react-storybook/assets/formatter-EIJCOSYU-C6KN2CTJ.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-DgsDw_eP.js";import{F as t}from"./iframe-DphSxkyT.js";import"./preload-helper-bHMdYvUY.js";import"./react-18-BY_0VFs-.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
