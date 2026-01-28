a37b0deea5b58ac630b006229873f9c685aa450c
nmerget <nmerget@users.noreply.github.com>
2026-01-28 07:35:35 +0000

    deploy: 0c18631e2380ba3a1ee6700673fd940bacc72630

--- review/refactor-running-npm-audit-fix/vue-storybook/assets/formatter-EIJCOSYU-B5e56wiE.js
+++
@@ -1,1 +0,0 @@
-import{d as i,b as m}from"./DocsRenderer-LL677BLK-CpaogtF_.js";import{O as o}from"./iframe-D4X3cB1B.js";import"./preload-helper-CFgWGzQ2.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
