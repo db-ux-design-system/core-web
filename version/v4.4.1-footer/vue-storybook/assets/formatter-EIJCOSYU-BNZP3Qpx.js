da45c4d8210e97d0a2c6c5ce6ad63d51d72acfa4
sarahbrng <sarahbrng@users.noreply.github.com>
2026-01-29 14:14:21 +0000

    deploy: 28739e39bcb99e08c6bf38f59c060a2912664838

---
+++ version/v4.4.1-footer/vue-storybook/assets/formatter-EIJCOSYU-BNZP3Qpx.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-CGNvNbKz.js";import{O as o}from"./iframe-BMWWNsDL.js";import"./preload-helper-mtrElwq6.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
