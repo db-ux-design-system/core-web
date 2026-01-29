75446275ca194f4c26deca39049e120ac9856e74
nmerget <nmerget@users.noreply.github.com>
2026-01-29 01:51:36 +0000

    deploy: 590762a76ebe6a643cef1addc75525d438dab273

--- review/fix-5794-adapt-event-type/composition-storybook/assets/formatter-QJ4M4OGQ-DFZiYzet.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as e}from"./blocks-0qvfW3UI.js";import{b as o}from"./iframe-lnr3ImgX.js";import"./preload-helper-BhbwgrXv.js";import"./index-Cc_lErSL.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
