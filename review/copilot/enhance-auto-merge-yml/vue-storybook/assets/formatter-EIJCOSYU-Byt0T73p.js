fac81b22e873c035f4c3da9d363fd4af958132e6
nmerget <nmerget@users.noreply.github.com>
2026-02-04 01:52:50 +0000

    deploy: f2b45ed5e2a359397ff04f0e02c88e1729a1b2d5

--- review/copilot/enhance-auto-merge-yml/vue-storybook/assets/formatter-EIJCOSYU-Byt0T73p.js
+++
@@ -1,1 +0,0 @@
-import{d as i,b as m}from"./DocsRenderer-LL677BLK-D9R9Rj5M.js";import{O as o}from"./iframe-DQ0JGvt2.js";import"./preload-helper-BhVe2ZFR.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
