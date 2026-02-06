8875c4ffcd6ced1c6b755ae9d0d4faa74b40c30c
mfranzke <mfranzke@users.noreply.github.com>
2026-02-06 12:19:07 +0000

    deploy: e96562129bbfd8820fafca77405a1cb6ae19b61a

---
+++ review/chore-reorder-for-clarity/react-storybook/assets/formatter-EIJCOSYU-BM-WqxpO.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-DRWZB9jr.js";import{F as t}from"./iframe-B8fKzrF-.js";import"./preload-helper-Cq0ZBq18.js";import"./react-18-BbCKy4o-.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
