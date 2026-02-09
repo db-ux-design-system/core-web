45601f02a266eae79044cdf962881ca3a25407dc
nmerget <nmerget@users.noreply.github.com>
2026-02-09 09:07:03 +0000

    deploy: eae3e76dffe30a5b2bc0ee439f9e4203567f2f1c

---
+++ review/dependabot-npm_and_yarn-playwright-71cb4f0720/vue-storybook/assets/formatter-EIJCOSYU-D-Zgmbiu.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-D3T_DOro.js";import{O as o}from"./iframe-CaShm7-a.js";import"./preload-helper-DECEBwpG.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
