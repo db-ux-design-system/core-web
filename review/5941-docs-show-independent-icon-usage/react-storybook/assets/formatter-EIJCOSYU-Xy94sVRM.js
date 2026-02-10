9cc71d884149b9ffafe2449ff9eb09ea42318fb0
mfranzke <mfranzke@users.noreply.github.com>
2026-02-10 16:12:16 +0000

    deploy: 03c385c205cb40f3c7a67f4f73a6118db6e20fcd

---
+++ review/5941-docs-show-independent-icon-usage/react-storybook/assets/formatter-EIJCOSYU-Xy94sVRM.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-Da0fiScB.js";import{F as t}from"./iframe-BBFH7hss.js";import"./preload-helper-B2ZZ8upz.js";import"./react-18-seWDyXUW.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
