8527685f9a17fa89de9d3df4a0b2fd3bf5c5e774
mfranzke <mfranzke@users.noreply.github.com>
2026-02-10 07:08:35 +0000

    deploy: 72df66463641d4bc5ec7a164265fca8f6facbf25

---
+++ review/5941-docs-show-independent-icon-usage/composition-storybook/assets/formatter-EIJCOSYU-BsonhTgj.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-DZgUFSFp.js";import{b as o}from"./iframe-C-qBAYIW.js";import"./preload-helper-DCD-bzvm.js";import"./index-BkaWf4WW.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
