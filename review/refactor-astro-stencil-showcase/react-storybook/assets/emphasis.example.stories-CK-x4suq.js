f7e213fb3689ffd4dd2bca784664940229fd0dcf
nmerget <nmerget@users.noreply.github.com>
2026-02-18 08:04:31 +0000

    deploy: 7c78c219a331e940b03d57d454ecd3ab645c82b5

---
+++ review/refactor-astro-stencil-showcase/react-storybook/assets/emphasis.example.stories-CK-x4suq.js
@@ -0,0 +1,12 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import{D as r}from"./tag-BseHWbl1.js";import"./index-20fQ7N7z.js";import"./iframe-D-PfmYcM.js";import"./preload-helper-DTBwH11z.js";import"./constants-CD69XpC7.js";import"./tooltip-BUPZbecD.js";import"./document-scroll-listener-Bcz2tjmG.js";import"./floating-components-BNmGdAgy.js";const{fn:g}=__STORYBOOK_MODULE_TEST__,x={title:"Components/DBTag/Emphasis",component:r,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{emphasis:{control:"select",options:["weak","strong"]},semantic:{control:"select",options:["adaptive","neutral","critical","informational","warning","successful"]},behavior:{control:"select",options:["static","removable"]},showIcon:{control:"boolean"},noText:{control:"boolean"},content:{control:"text"},showCheckState:{control:"boolean"},overflow:{control:"boolean"},removeButton:{control:"text"},text:{control:"text"},value:{control:"text"},id:{control:"text"},autofocus:{control:"boolean"},onRemove:{action:"onRemove"}}},o={args:{children:"(Default) Weak"},render:t=>n.jsx(r,{...t})},e={args:{emphasis:"strong",children:"Strong"},render:t=>n.jsx(r,{...t})};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "children": "(Default) Weak"
+  },
+  render: (properties: any) => <DBTag {...properties} />
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    "emphasis": "strong",
+    "children": "Strong"
+  },
+  render: (properties: any) => <DBTag {...properties} />
+}`,...e.parameters?.docs?.source}}};const f=["DefaultWeak","Strong"];export{o as DefaultWeak,e as Strong,f as __namedExportsOrder,x as default};
