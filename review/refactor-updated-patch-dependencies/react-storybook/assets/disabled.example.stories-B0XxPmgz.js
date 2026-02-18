056a029ad418d7004840ce6b7d526f9266a9aca4
mfranzke <mfranzke@users.noreply.github.com>
2026-02-18 10:27:05 +0000

    deploy: 63c6d638d6a778bc86573ca9e19b837771146c11

---
+++ review/refactor-updated-patch-dependencies/react-storybook/assets/disabled.example.stories-B0XxPmgz.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{D as a}from"./navigation-item-DIKZIyuv.js";import"./index-cGbbigiG.js";import"./iframe-B3atPrN1.js";import"./preload-helper-DsbWi-6z.js";import"./constants-CD69XpC7.js";import"./floating-components-BNmGdAgy.js";import"./button-CcPUb-5r.js";const{fn:m}=__STORYBOOK_MODULE_TEST__,x={title:"Components/DBNavigationItem/Disabled",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{disabled:{control:"boolean"},active:{control:"boolean"},showIcon:{control:"boolean"},width:{control:"select",options:["full","auto"]},wrap:{control:"boolean"},text:{control:"text"},subNavigationExpanded:{control:"boolean"},backButtonId:{control:"text"},backButtonText:{control:"text"},id:{control:"text"},autofocus:{control:"boolean"},onClick:{action:"onClick"}}},o={args:{disabled:!1,children:e.jsx("a",{href:"#",children:"(Default) False"})},render:t=>e.jsx("ul",{children:e.jsx(a,{...t})})},r={args:{disabled:!0,children:e.jsx("a",{href:"#",children:"True"})},render:t=>e.jsx("ul",{children:e.jsx(a,{...t})})};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "disabled": false,
+    "children": <a href="#">(Default) False</a>
+  },
+  render: (properties: any) => <ul><DBNavigationItem {...properties} /></ul>
+}`,...o.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    "disabled": true,
+    "children": <a href="#">True</a>
+  },
+  render: (properties: any) => <ul><DBNavigationItem {...properties} /></ul>
+}`,...r.parameters?.docs?.source}}};const f=["DefaultFalse","True"];export{o as DefaultFalse,r as True,f as __namedExportsOrder,x as default};
