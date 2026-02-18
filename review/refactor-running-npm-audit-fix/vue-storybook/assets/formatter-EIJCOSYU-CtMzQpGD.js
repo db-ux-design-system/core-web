22555e2e2018a45baf501a6003595e960e4321d8
mfranzke <mfranzke@users.noreply.github.com>
2026-02-18 12:12:54 +0000

    deploy: 4978c85f902f3c02c71450850476e13525eeb999

---
+++ review/refactor-running-npm-audit-fix/vue-storybook/assets/formatter-EIJCOSYU-CtMzQpGD.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-Bahj-p4z.js";import{Y as o}from"./iframe-BFew9HnG.js";import"./preload-helper-CFgWGzQ2.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
