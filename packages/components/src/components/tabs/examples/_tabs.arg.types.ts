a740bdc65f3dea0d7bf5807c67b61663dbf19063
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-02-18 15:41:41 +0100

    fix: fix storybook arg types and reset outdated screenshots

--- packages/components/src/components/tabs/examples/_tabs.arg.types.ts
+++ packages/components/src/components/tabs/examples/_tabs.arg.types.ts
@@ -3,7 +3,7 @@ import type { InputType } from 'storybook/internal/csf';
 export const StorybookTabsArgTypes: Record<string, InputType> = {
 	orientation: { control: 'select', options: ['horizontal', 'vertical'] },
 	width: { control: 'select', options: ['full', 'auto'] },
-	alignment: { control: 'select', options: ['start', 'center'] },
+	contentAlignment: { control: 'select', options: ['start', 'center'] },
 	behavior: { control: 'select', options: ['scrollbar', 'arrows'] },
 	initialSelectedIndex: { control: 'number' },
 	initialSelectedMode: { control: 'select', options: ['auto', 'manually'] },
