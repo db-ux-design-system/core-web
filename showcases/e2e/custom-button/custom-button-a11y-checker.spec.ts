4e6cf5c57fcd261e91476933cada1cfa237e5acd
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-02-19 09:42:01 +0100

    chore: update a11y checker test

--- showcases/e2e/custom-button/custom-button-a11y-checker.spec.ts
+++ showcases/e2e/custom-button/custom-button-a11y-checker.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@playwright/test';
 import { runA11yCheckerTest } from '../default.ts';
 
+const aCheckerDisableRules = ['aria_attribute_valid']; // TODO: This is a false positive -> add an issue in https://github.com/IBMa/equal-access
+
 test.describe('DBCustomButton', () => {
-	runA11yCheckerTest({ path: '02/custom-button' });
+	runA11yCheckerTest({ path: '02/custom-button', aCheckerDisableRules });
 });
