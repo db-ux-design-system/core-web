85bcfa16359acaf10e224fde860e982749d65590
nmerget <nmerget@users.noreply.github.com>
2026-02-20 09:39:02 +0000

    deploy: b595216c6f9a5706831d367ef5c9640b6843a77e

---
+++ review/feat-custom-button/vue-storybook/assets/show-icon-leading.example.stories-CPiTKIHV.js
@@ -0,0 +1,121 @@
+import{_ as n}from"./custom-button-Db_yJ4HL.js";import{_ as e}from"./infotext-CAzaDNGj.js";import"./iframe-D2eu3nvZ.js";import"./preload-helper-BVUwd7FV.js";import"./index-CABgWjJz.js";const{fn:i}=__STORYBOOK_MODULE_TEST__,f={title:"Components/DBCustomButton/Show Icon Leading",component:n,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{control:"select",options:["outlined","brand","ghost","filled"]},noText:{control:"boolean"},showIcon:{control:"boolean"},showIconLeading:{control:"boolean"},showIconTrailing:{control:"boolean"},width:{control:"select",options:["full","auto"]},size:{control:"select",options:["small","medium"]},id:{control:"text"},autofocus:{control:"boolean"}}},o={args:{icon:"x_placeholder",showIcon:!1,default:'<button type="button">Button</button>'},render:t=>({components:{DBCustomButton:n,DBInfotext:e},setup(){return{args:t}},template:`<DBCustomButton v-bind="args"   >${t.default}</DBCustomButton>`})},r={args:{icon:"x_placeholder",showIcon:!1,default:`<label
+  ><input type="checkbox" />
+  Checkbox
+</label>`},render:t=>({components:{DBCustomButton:n,DBInfotext:e},setup(){return{args:t}},template:`<DBCustomButton v-bind="args"   >${t.default}</DBCustomButton>`})},s={args:{icon:"x_placeholder",showIcon:!1,default:'<a href="#">Link</a>'},render:t=>({components:{DBCustomButton:n,DBInfotext:e},setup(){return{args:t}},template:`<DBCustomButton v-bind="args"   >${t.default}</DBCustomButton>`})},a={args:{icon:"x_placeholder",showIcon:!0,default:'<button type="button">Button</button>'},render:t=>({components:{DBCustomButton:n,DBInfotext:e},setup(){return{args:t}},template:`<DBCustomButton v-bind="args"   >${t.default}</DBCustomButton>`})},u={args:{icon:"x_placeholder",showIcon:!0,default:`<label
+  ><input type="checkbox" />
+  Checkbox
+</label>`},render:t=>({components:{DBCustomButton:n,DBInfotext:e},setup(){return{args:t}},template:`<DBCustomButton v-bind="args"   >${t.default}</DBCustomButton>`})},c={args:{icon:"x_placeholder",showIcon:!0,default:'<a href="#">Link</a>'},render:t=>({components:{DBCustomButton:n,DBInfotext:e},setup(){return{args:t}},template:`<DBCustomButton v-bind="args"   >${t.default}</DBCustomButton>`})};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "x_placeholder",
+    "showIcon": false,
+    "default": \`<button type="button">Button</button>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomButton,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCustomButton v-bind="args"   >\${args.default}</DBCustomButton>\`
+  })
+}`,...o.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "x_placeholder",
+    "showIcon": false,
+    "default": \`<label
+  ><input type="checkbox" />
+  Checkbox
+</label>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomButton,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCustomButton v-bind="args"   >\${args.default}</DBCustomButton>\`
+  })
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "x_placeholder",
+    "showIcon": false,
+    "default": \`<a href="#">Link</a>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomButton,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCustomButton v-bind="args"   >\${args.default}</DBCustomButton>\`
+  })
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "x_placeholder",
+    "showIcon": true,
+    "default": \`<button type="button">Button</button>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomButton,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCustomButton v-bind="args"   >\${args.default}</DBCustomButton>\`
+  })
+}`,...a.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "x_placeholder",
+    "showIcon": true,
+    "default": \`<label
+  ><input type="checkbox" />
+  Checkbox
+</label>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomButton,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCustomButton v-bind="args"   >\${args.default}</DBCustomButton>\`
+  })
+}`,...u.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "x_placeholder",
+    "showIcon": true,
+    "default": \`<a href="#">Link</a>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomButton,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCustomButton v-bind="args"   >\${args.default}</DBCustomButton>\`
+  })
+}`,...c.parameters?.docs?.source}}};const D=["DefaultFalseButton","DefaultFalseCheckbox","DefaultFalseLink","TrueButton","TrueCheckbox","TrueLink"];export{o as DefaultFalseButton,r as DefaultFalseCheckbox,s as DefaultFalseLink,a as TrueButton,u as TrueCheckbox,c as TrueLink,D as __namedExportsOrder,f as default};
