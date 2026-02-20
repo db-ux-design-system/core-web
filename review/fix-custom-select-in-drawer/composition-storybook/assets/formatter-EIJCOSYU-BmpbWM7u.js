59fadcaf7c635bae3e7a4a6c75b7ca85e0cca4d2
nmerget <nmerget@users.noreply.github.com>
2026-02-20 14:19:33 +0000

    deploy: e3b9cad8d3e4feb006a4561b1edf6bd16571878a

---
+++ review/fix-custom-select-in-drawer/composition-storybook/assets/formatter-EIJCOSYU-BmpbWM7u.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-2OxDKfho.js";import{b as o}from"./iframe-C9MUaC8s.js";import"./preload-helper-y-csNDbY.js";import"./index-BxMXk6QI.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
