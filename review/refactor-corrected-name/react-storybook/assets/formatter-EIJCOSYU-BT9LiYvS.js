f0e18d3902fcabcc4ac37680f93cbde4abdc8ee3
mfranzke <mfranzke@users.noreply.github.com>
2026-02-21 06:26:28 +0000

    deploy: c891d71a887465db20a98f2267201c0fb2cde98e

---
+++ review/refactor-corrected-name/react-storybook/assets/formatter-EIJCOSYU-BT9LiYvS.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-BLn_E53G.js";import{G as t}from"./iframe-pAWjk7_u.js";import"./preload-helper-BfQWj4FA.js";import"./react-18-DcsZ-jsx.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
