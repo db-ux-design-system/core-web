b33fc3bb84cee113f59a7244ddcc5b86ed35988a
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-02-23 14:53:40 +0100

    chore: add exception for angular and stencil in table-a11y-checker.spec.ts

--- showcases/e2e/table/table-a11y-checker.spec.ts
+++ showcases/e2e/table/table-a11y-checker.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@playwright/test';
-import { runA11yCheckerTest } from '../default.ts';
+import { hasWebComponentSyntax, runA11yCheckerTest } from '../default.ts';
 
+// The a11y checker will clash with the wrapping custom element from angular and stencil
+const skipChecker = hasWebComponentSyntax(process.env.showcase);
 test.describe('DBTable', () => {
-	runA11yCheckerTest({ path: '04/table' });
+	runA11yCheckerTest({ path: '04/table', skipChecker });
 });
