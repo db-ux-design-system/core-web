fca330382de3ac7e7ddd33d0b8405097eec53afe
nmerget <nmerget@users.noreply.github.com>
2026-02-23 09:02:06 +0000

    deploy: 79ef1af2e058a18ee83cc433e4661eff5128f2f5

---
+++ review/feat-table/vue-storybook/assets/formatter-EIJCOSYU-B7rxvqgO.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-f1LNLMdC.js";import{a3 as a}from"./iframe-DQPjiEIV.js";import"./preload-helper-MpUUyRtn.js";var o=a(m(),1),d=(0,o.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
