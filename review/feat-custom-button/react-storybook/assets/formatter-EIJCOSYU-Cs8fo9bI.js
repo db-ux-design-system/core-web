a639820388edc1a9eaaadb8a5fc088db76425aca
nmerget <nmerget@users.noreply.github.com>
2026-02-24 14:01:29 +0000

    deploy: bedd1a70f6fe32fbf1b4f6f54226b8e29ed02e77

--- review/feat-custom-button/react-storybook/assets/formatter-EIJCOSYU-Cs8fo9bI.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-BUnQyZ9E.js";import{G as t}from"./iframe-DJ5uuXbQ.js";import"./preload-helper-COTrxSA1.js";import"./react-18-Ue1tWs-n.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
