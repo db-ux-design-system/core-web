ddf2fbd263ea6f387e1a6fa31f57993413937763
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-02-24 16:37:51 +0000

    deploy: d68aeface30bea8bd4050e9c771b65d2635d07ea

---
+++ review/feat-1453-tabs-feedback/react-storybook/assets/density.example.stories-DHsoY1_W.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{D as s,a as n}from"./tab-list-CWEuWkRr.js";import"./index-C0vvPcr0.js";import"./iframe-C5TfW_GS.js";import"./preload-helper--tLyh50B.js";import"./tooltip-zWRg58bh.js";import"./constants-C-ysBZRi.js";import"./document-scroll-listener-C19Ex2M_.js";import"./floating-components-BNmGdAgy.js";const{fn:x}=__STORYBOOK_MODULE_TEST__,D={title:"Components/DBTabItem/Density",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{label:{control:"text"},active:{control:"boolean"},disabled:{control:"boolean"},showIcon:{control:"boolean"},showIconLeading:{control:"boolean"},showIconTrailing:{control:"boolean"},noText:{control:"boolean"},id:{control:"text"},autofocus:{control:"boolean"}}},a={args:{"data-density":"functional",label:"Functional"},render:r=>e.jsx(n,{children:e.jsx(s,{...r})})},o={args:{"data-density":"regular",label:"(Default) Regular"},render:r=>e.jsx(n,{children:e.jsx(s,{...r})})},t={args:{"data-density":"expressive",label:"Expressive"},render:r=>e.jsx(n,{children:e.jsx(s,{...r})})};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "functional",
+    "label": "Functional"
+  },
+  render: (properties: any) => <DBTabList><DBTabItem {...properties} /></DBTabList>
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "label": "(Default) Regular"
+  },
+  render: (properties: any) => <DBTabList><DBTabItem {...properties} /></DBTabList>
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "expressive",
+    "label": "Expressive"
+  },
+  render: (properties: any) => <DBTabList><DBTabItem {...properties} /></DBTabList>
+}`,...t.parameters?.docs?.source}}};const T=["Functional","DefaultRegular","Expressive"];export{o as DefaultRegular,t as Expressive,a as Functional,T as __namedExportsOrder,D as default};
---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/truncation.example.stories-DZRFTiMd.js
@@ -0,0 +1,40 @@
+import{_ as e,a as n}from"./tabs-Bt01EKEd.js";import{a as o,_ as l}from"./tab-list-D3cqDLcA.js";import{_ as r}from"./infotext-BvusPNAW.js";import"./iframe-CCLJr_BJ.js";import"./preload-helper-CDE7oBwp.js";import"./index-liOe68zv.js";import"./button-DU2C4Mg1.js";import"./tooltip-iR3v2AwC.js";import"./constants-CvUrIGJS.js";import"./document-scroll-listener-CmN1vgKm.js";import"./floating-components-jmncKSnU.js";const{fn:f}=__STORYBOOK_MODULE_TEST__,I={title:"Components/DBTabs/Truncation",component:e,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{orientation:{control:"select",options:["horizontal","vertical"]},width:{control:"select",options:["full","auto"]},contentAlignment:{control:"select",options:["start","center"]},behavior:{control:"select",options:["scrollbar","arrows"]},initialSelectedIndex:{control:"number"},initialSelectedMode:{control:"select",options:["auto","manually"]},name:{control:"text"},tabs:{control:"object"},arrowScrollDistance:{control:"number"},id:{control:"text"},autofocus:{control:"boolean"}}},t={args:{orientation:"vertical",width:"auto",default:`<DBTabList
+  ><DBTabItem label="Very long tab label that gets truncated"></DBTabItem
+  ><DBTabItem label="Another long label"></DBTabItem
+  ><DBTabItem label="Short"></DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>`},render:a=>({components:{DBTabs:e,DBInfotext:r,DBTabItem:l,DBTabList:o,DBTabPanel:n},setup(){return{args:a}},template:`<div class="fit-content-container" :style="{
+  width: '100%'
+}"  ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    truncated tab label (vertical only):
+                </DBInfotext><DBTabs v-bind="args"   >${a.default}</DBTabs></div>`})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    "orientation": "vertical",
+    "width": "auto",
+    "default": \`<DBTabList
+  ><DBTabItem label="Very long tab label that gets truncated"></DBTabItem
+  ><DBTabItem label="Another long label"></DBTabItem
+  ><DBTabItem label="Short"></DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabs,
+      DBInfotext,
+      DBTabItem,
+      DBTabList,
+      DBTabPanel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div class="fit-content-container" :style="{
+  width: '100%'
+}"  ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    truncated tab label (vertical only):
+                </DBInfotext><DBTabs v-bind="args"   >\${args.default}</DBTabs></div>\`
+  })
+}`,...t.parameters?.docs?.source}}};const _=["truncated"];export{_ as __namedExportsOrder,I as default,t as truncated};
