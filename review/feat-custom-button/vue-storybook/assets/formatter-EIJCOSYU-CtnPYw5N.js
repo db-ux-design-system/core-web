f9575e1d12c1f6db4d41f8ce60e42610274f8f06
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-02-24 09:35:43 +0000

    deploy: 2bd65f8898de08460267181b51aea8dc69fc2b42

---
+++ review/feat-custom-button/vue-storybook/assets/formatter-EIJCOSYU-CtnPYw5N.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-BFd5EfsV.js";import{Y as o}from"./iframe-BqcwKG60.js";import"./preload-helper-BVUwd7FV.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
