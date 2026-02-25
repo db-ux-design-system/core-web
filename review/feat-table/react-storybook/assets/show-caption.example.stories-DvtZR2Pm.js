f080bfc3ce9bd9dd2c4f5f5297ffac561026ff83
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-02-25 14:10:39 +0000

    deploy: 590021cf28241621b1251852ab05bb73df39246d

---
+++ review/feat-table/react-storybook/assets/show-caption.example.stories-DvtZR2Pm.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{d as i}from"./data-1Z1tDx00.js";import{D as n}from"./table-DLk6RAEb.js";import{D as r}from"./infotext-BY5SJ9Aq.js";import"./index-C0vvPcr0.js";import"./iframe-D80j_IDB.js";import"./preload-helper-BD8fvMO7.js";import"./link-CoBYe3sx.js";const{fn:u}=__STORYBOOK_MODULE_TEST__,g={title:"Components/DBTable/ShowCaption",component:n,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{id:{control:"text"},caption:{control:"text"},captionPlain:{control:"text"},data:{control:"object"},divider:{control:"select",options:["none","both","horizontal","vertical"]},showCaption:{control:"boolean"},size:{control:"select",options:["x-small","small","medium","large"]},variant:{control:"select",options:["joined","zebra","floating"]},mobileVariant:{control:"select",options:["table","list"]},stickyHeader:{control:"select",options:["none","both","horizontal","vertical"]},columnSizes:{control:"object"}}},t={args:{captionPlain:"(Default) False",data:i},render:o=>e.jsxs("div",{style:{minInlineSize:"300px",display:"flex",flexDirection:"column",alignItems:"center",gap:"var(--db-spacing-fixed-md)"},children:[e.jsx(r,{semantic:"informational",size:"small",icon:"none",children:"(Default) False"}),e.jsx(n,{...o})]})},a={args:{captionPlain:"True",data:i,showCaption:!0},render:o=>e.jsx("div",{style:{minInlineSize:"300px",display:"flex",flexDirection:"column",alignItems:"center",gap:"var(--db-spacing-fixed-md)"},children:e.jsx(n,{...o})})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "(Default) False",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    (Default) False
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...t.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "True",
+    "data": defaultTable,
+    "showCaption": true
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBTable {...properties} /></div>
+}`,...a.parameters?.docs?.source}}};const D=["DefaultFalse","True"];export{t as DefaultFalse,a as True,D as __namedExportsOrder,g as default};
---
+++ review/feat-table/react-storybook/assets/vertical-alignment.example.stories-C3i2Ixb0.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-u17CrQMm.js";import{D as o,a as c,b as a,c as l,d as s,e as d}from"./table-DLk6RAEb.js";import{D as m}from"./infotext-BY5SJ9Aq.js";import{D as p}from"./button-BwhDp_A-.js";import"./index-C0vvPcr0.js";import"./iframe-D80j_IDB.js";import"./preload-helper-BD8fvMO7.js";import"./link-CoBYe3sx.js";const{fn:C}=__STORYBOOK_MODULE_TEST__,f={title:"Components/DBTable/Vertical Alignment",component:o,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{id:{control:"text"},caption:{control:"text"},captionPlain:{control:"text"},data:{control:"object"},divider:{control:"select",options:["none","both","horizontal","vertical"]},showCaption:{control:"boolean"},size:{control:"select",options:["x-small","small","medium","large"]},variant:{control:"select",options:["joined","zebra","floating"]},mobileVariant:{control:"select",options:["table","list"]},stickyHeader:{control:"select",options:["none","both","horizontal","vertical"]},columnSizes:{control:"object"}}},t={args:{captionPlain:"Start",divider:"both",children:e.jsxs(e.Fragment,{children:[e.jsx(c,{children:e.jsxs(a,{children:[e.jsx(l,{scope:"col",verticalAlignment:"start",children:"Header A"}),e.jsx(l,{scope:"col",verticalAlignment:"start",children:"Header B"})]})}),e.jsx(s,{children:e.jsxs(a,{children:[e.jsx(l,{scope:"row",verticalAlignment:"start",children:"Comp 1"}),e.jsx(d,{verticalAlignment:"start",children:e.jsx(p,{children:"Click"})})]})})]})},render:n=>e.jsxs("div",{style:{minInlineSize:"300px",display:"flex",flexDirection:"column",alignItems:"center",gap:"var(--db-spacing-fixed-md)"},children:[e.jsx(m,{semantic:"informational",size:"small",icon:"none",children:"Start"}),e.jsx(o,{...n})]})},r={args:{captionPlain:"(Default) Center",divider:"both",children:e.jsxs(e.Fragment,{children:[e.jsx(c,{children:e.jsxs(a,{children:[e.jsx(l,{scope:"col",verticalAlignment:"center",children:"Header A"}),e.jsx(l,{scope:"col",verticalAlignment:"center",children:"Header B"})]})}),e.jsx(s,{children:e.jsxs(a,{children:[e.jsx(l,{scope:"row",verticalAlignment:"center",children:"Comp 1"}),e.jsx(d,{verticalAlignment:"center",children:e.jsx(p,{children:"Click"})})]})})]})},render:n=>e.jsxs("div",{style:{minInlineSize:"300px",display:"flex",flexDirection:"column",alignItems:"center",gap:"var(--db-spacing-fixed-md)"},children:[e.jsx(m,{semantic:"informational",size:"small",icon:"none",children:"(Default) Center"}),e.jsx(o,{...n})]})},i={args:{captionPlain:"End",divider:"both",children:e.jsxs(e.Fragment,{children:[e.jsx(c,{children:e.jsxs(a,{children:[e.jsx(l,{scope:"col",verticalAlignment:"end",children:"Header A"}),e.jsx(l,{scope:"col",verticalAlignment:"end",children:"Header B"})]})}),e.jsx(s,{children:e.jsxs(a,{children:[e.jsx(l,{scope:"row",verticalAlignment:"end",children:"Comp 1"}),e.jsx(d,{verticalAlignment:"end",children:e.jsx(p,{children:"Click"})})]})})]})},render:n=>e.jsxs("div",{style:{minInlineSize:"300px",display:"flex",flexDirection:"column",alignItems:"center",gap:"var(--db-spacing-fixed-md)"},children:[e.jsx(m,{semantic:"informational",size:"small",icon:"none",children:"End"}),e.jsx(o,{...n})]})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "Start",
+    "divider": "both",
+    "children": <><DBTableHead><DBTableRow><DBTableHeaderCell scope="col" verticalAlignment="start">
+                                Header A
+                            </DBTableHeaderCell><DBTableHeaderCell scope="col" verticalAlignment="start">
+                                Header B
+                            </DBTableHeaderCell></DBTableRow></DBTableHead><DBTableBody><DBTableRow><DBTableHeaderCell scope="row" verticalAlignment="start">
+                                Comp 1
+                            </DBTableHeaderCell><DBTableDataCell verticalAlignment="start"><DBButton>Click</DBButton></DBTableDataCell></DBTableRow></DBTableBody></>
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    Start
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...t.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "(Default) Center",
+    "divider": "both",
+    "children": <><DBTableHead><DBTableRow><DBTableHeaderCell scope="col" verticalAlignment="center">
+                                Header A
+                            </DBTableHeaderCell><DBTableHeaderCell scope="col" verticalAlignment="center">
+                                Header B
+                            </DBTableHeaderCell></DBTableRow></DBTableHead><DBTableBody><DBTableRow><DBTableHeaderCell scope="row" verticalAlignment="center">
+                                Comp 1
+                            </DBTableHeaderCell><DBTableDataCell verticalAlignment="center"><DBButton>Click</DBButton></DBTableDataCell></DBTableRow></DBTableBody></>
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    (Default) Center
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...r.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "End",
+    "divider": "both",
+    "children": <><DBTableHead><DBTableRow><DBTableHeaderCell scope="col" verticalAlignment="end">
+                                Header A
+                            </DBTableHeaderCell><DBTableHeaderCell scope="col" verticalAlignment="end">
+                                Header B
+                            </DBTableHeaderCell></DBTableRow></DBTableHead><DBTableBody><DBTableRow><DBTableHeaderCell scope="row" verticalAlignment="end">
+                                Comp 1
+                            </DBTableHeaderCell><DBTableDataCell verticalAlignment="end"><DBButton>Click</DBButton></DBTableDataCell></DBTableRow></DBTableBody></>
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    End
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...i.parameters?.docs?.source}}};const j=["Start","DefaultCenter","End"];export{r as DefaultCenter,i as End,t as Start,j as __namedExportsOrder,f as default};
