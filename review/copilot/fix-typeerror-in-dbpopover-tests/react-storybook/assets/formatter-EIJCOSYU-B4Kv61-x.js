33f6535c2047222cfc04d3558155df96829097e8
nmerget <nmerget@users.noreply.github.com>
2026-02-25 07:36:04 +0000

    deploy: c446a9e51e74a2096c1393a85bc87dfcc14cc450

---
+++ review/copilot/fix-typeerror-in-dbpopover-tests/react-storybook/assets/formatter-EIJCOSYU-B4Kv61-x.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-0j9LsE7K.js";import{G as t}from"./iframe-XUMMrRTJ.js";import"./preload-helper-C0fRkUp2.js";import"./react-18-DmRF_kW4.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
