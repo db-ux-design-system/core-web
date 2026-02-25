8615df8f8b8542c941af18c493f10f23d44c23cb
nmerget <nmerget@users.noreply.github.com>
2026-02-25 11:09:39 +0000

    deploy: f2484c77ca0797087f19c2016daf37b5776eae36

---
+++ review/fix-no-text-button-scale/composition-storybook/assets/formatter-EIJCOSYU-Cy2pd-07.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-CNEm_0Ax.js";import{b as o}from"./iframe-DSTwnvhb.js";import"./preload-helper-CHhCMAxE.js";import"./index-DgHmym9I.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
