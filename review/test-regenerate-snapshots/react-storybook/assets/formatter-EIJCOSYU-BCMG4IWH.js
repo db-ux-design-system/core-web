95b4e3a4a5715eaedb6b8c96fa566670be6d1aa4
nmerget <nmerget@users.noreply.github.com>
2026-02-26 15:28:45 +0000

    deploy: 8f1a6cf2466529f93e26afe819b1e5d7377c2e95

--- review/test-regenerate-snapshots/react-storybook/assets/formatter-EIJCOSYU-BCMG4IWH.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-B-0SBufH.js";import{G as t}from"./iframe-DfOCqRfS.js";import"./preload-helper-Dgpd6-J5.js";import"./react-18-CUaCYRSf.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
