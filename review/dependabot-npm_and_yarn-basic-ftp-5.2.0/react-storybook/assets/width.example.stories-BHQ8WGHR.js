efa67e7427076ad5cec96a37c8ff66b754fcd43f
nmerget <nmerget@users.noreply.github.com>
2026-02-26 10:19:58 +0000

    deploy: 464cbe391b3b73e7de9b597736100dd36335255f

---
+++ review/dependabot-npm_and_yarn-basic-ftp-5.2.0/react-storybook/assets/width.example.stories-BHQ8WGHR.js
@@ -0,0 +1,16 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import{D as a}from"./navigation-item-BtmMUSqg.js";import"./index-C0vvPcr0.js";import"./iframe-BAXfcV_p.js";import"./preload-helper-B7Dm1o2Y.js";import"./constants-C-ysBZRi.js";import"./floating-components-DAXMbqch.js";import"./button-9bY3XOHj.js";const{fn:m}=__STORYBOOK_MODULE_TEST__,h={title:"Components/DBNavigationItem/Width",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{disabled:{control:"boolean"},active:{control:"boolean"},showIcon:{control:"boolean"},width:{control:"select",options:["full","auto"]},wrap:{control:"boolean"},text:{control:"text"},subNavigationExpanded:{control:"boolean"},backButtonId:{control:"text"},backButtonText:{control:"text"},id:{control:"text"},autofocus:{control:"boolean"},onClick:{action:"onClick"}}},o={args:{children:t.jsx("a",{href:"#",children:"(Default) Auto"})},render:r=>t.jsx("ul",{style:{width:"400px"},children:t.jsx(a,{...r})})},e={args:{width:"full",children:t.jsx("a",{href:"#",children:"Full"})},render:r=>t.jsx("ul",{style:{width:"400px"},children:t.jsx(a,{...r})})};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "children": <a href="#">(Default) Auto</a>
+  },
+  render: (properties: any) => <ul style={{
+    width: '400px'
+  }}><DBNavigationItem {...properties} /></ul>
+}`,...o.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    "width": "full",
+    "children": <a href="#">Full</a>
+  },
+  render: (properties: any) => <ul style={{
+    width: '400px'
+  }}><DBNavigationItem {...properties} /></ul>
+}`,...e.parameters?.docs?.source}}};const x=["DefaultAuto","Full"];export{o as DefaultAuto,e as Full,x as __namedExportsOrder,h as default};
