83885102eb4df99f0f0847c0a3931a587d3ec31e
nmerget <nmerget@users.noreply.github.com>
2026-02-26 14:10:04 +0000

    deploy: 71bfce01b6c1fdb2d78c2686373c7ee288ab9d2d

---
+++ review/copilot/fix-input-field-decimal-separator/react-storybook/assets/formatter-EIJCOSYU-A8A1uGRh.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-CIhOx3QV.js";import{G as t}from"./iframe-UwdR7b7v.js";import"./preload-helper-CAbtcdlK.js";import"./react-18-CxR5IRHs.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
