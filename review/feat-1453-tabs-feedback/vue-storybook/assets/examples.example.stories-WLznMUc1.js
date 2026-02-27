38f45e35060400326a2501499abf08ce04cb2bdd
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-02-27 11:24:43 +0000

    deploy: 81b9dc166d6177e56581ba64780738ea5e999d5d

---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/examples.example.stories-WLznMUc1.js
@@ -0,0 +1,65 @@
+import{_ as t,a as o}from"./tabs-Dsi9l_fc.js";import{a as s,_ as l}from"./tab-list-Dd57VKPL.js";import{_ as T}from"./infotext-rn_RQV6R.js";import"./iframe-CnhgBaeZ.js";import"./preload-helper-CDE7oBwp.js";import"./index-C2ygMkoK.js";import"./button-DRiA8i7O.js";import"./tooltip-xBQtJFAI.js";import"./constants-CvUrIGJS.js";import"./document-scroll-listener-Bxs16huG.js";import"./floating-components-CKmcRn_b.js";const{fn:P}=__STORYBOOK_MODULE_TEST__,u={title:"Components/DBTabs/Examples",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{orientation:{control:"select",options:["horizontal","vertical"]},width:{control:"select",options:["full","auto"]},contentAlignment:{control:"select",options:["start","center"]},behavior:{control:"select",options:["scrollbar","arrows"]},initialSelectedIndex:{control:"number"},initialSelectedMode:{control:"select",options:["auto","manually"]},name:{control:"text"},tabs:{control:"object"},arrowScrollDistance:{control:"number"},id:{control:"text"},autofocus:{control:"boolean"}}},a={args:{initialSelectedIndex:1,default:`<DBTabList
+  ><DBTabItem>Test 1</DBTabItem><DBTabItem>Test 2</DBTabItem
+  ><DBTabItem>Test 3</DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>`},render:e=>({components:{DBTabs:t,DBInfotext:T,DBTabItem:l,DBTabList:s,DBTabPanel:o},setup(){return{args:e}},template:`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    2nd tab selected:
+                </DBInfotext><DBTabs v-bind="args"   >${e.default}</DBTabs></div>`})},n={args:{initialSelectedMode:"manually",default:`<DBTabList
+  ><DBTabItem>Test 1</DBTabItem><DBTabItem>Test 2</DBTabItem
+  ><DBTabItem>Test 3</DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>`},render:e=>({components:{DBTabs:t,DBInfotext:T,DBTabItem:l,DBTabList:s,DBTabPanel:o},setup(){return{args:e}},template:`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    nothing selected:
+                </DBInfotext><DBTabs v-bind="args"   >${e.default}</DBTabs></div>`})};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "initialSelectedIndex": 1,
+    "default": \`<DBTabList
+  ><DBTabItem>Test 1</DBTabItem><DBTabItem>Test 2</DBTabItem
+  ><DBTabItem>Test 3</DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabs,
+      DBInfotext,
+      DBTabItem,
+      DBTabList,
+      DBTabPanel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    2nd tab selected:
+                </DBInfotext><DBTabs v-bind="args"   >\${args.default}</DBTabs></div>\`
+  })
+}`,...a.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    "initialSelectedMode": "manually",
+    "default": \`<DBTabList
+  ><DBTabItem>Test 1</DBTabItem><DBTabItem>Test 2</DBTabItem
+  ><DBTabItem>Test 3</DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabs,
+      DBInfotext,
+      DBTabItem,
+      DBTabList,
+      DBTabPanel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    nothing selected:
+                </DBInfotext><DBTabs v-bind="args"   >\${args.default}</DBTabs></div>\`
+  })
+}`,...n.parameters?.docs?.source}}};const g=["SecondTestselected","nothingselected"];export{a as SecondTestselected,g as __namedExportsOrder,u as default,n as nothingselected};
