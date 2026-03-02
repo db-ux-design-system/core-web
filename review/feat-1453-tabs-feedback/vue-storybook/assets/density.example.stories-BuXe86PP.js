7f837de637ec4a718c4c0d40f345640431ee0111
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-03-02 13:32:27 +0000

    deploy: a557f92d67f0dd02598e223d3ea03f36f9f1b322

---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/density.example.stories-BuXe86PP.js
@@ -0,0 +1,55 @@
+import{_ as s,a as r}from"./tab-list-Bu4aJsO5.js";import"./iframe-bvFetpIT.js";import"./preload-helper-CDE7oBwp.js";import"./index-BsBlz6iG.js";import"./tooltip-C3eQIFVz.js";import"./constants-y2N5m1XS.js";import"./document-scroll-listener-D6BJnLBr.js";import"./floating-components-CKmcRn_b.js";const{fn:p}=__STORYBOOK_MODULE_TEST__,D={title:"Components/DBTabItem/Density",component:r,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{label:{control:"text"},active:{control:"boolean"},disabled:{control:"boolean"},showIcon:{control:"boolean"},showIconLeading:{control:"boolean"},showIconTrailing:{control:"boolean"},noText:{control:"boolean"},id:{control:"text"},autofocus:{control:"boolean"}}},a={args:{"data-density":"functional",label:"Functional",default:""},render:e=>({components:{DBTabItem:r,DBTabList:s},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},t={args:{"data-density":"regular",label:"(Default) Regular",default:""},render:e=>({components:{DBTabItem:r,DBTabList:s},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},n={args:{"data-density":"expressive",label:"Expressive",default:""},render:e=>({components:{DBTabItem:r,DBTabList:s},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "functional",
+    "label": "Functional",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...a.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "label": "(Default) Regular",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "expressive",
+    "label": "Expressive",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...n.parameters?.docs?.source}}};const T=["Functional","DefaultRegular","Expressive"];export{t as DefaultRegular,n as Expressive,a as Functional,T as __namedExportsOrder,D as default};
---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/states.example.stories-Cxy3Rot4.js
@@ -0,0 +1,54 @@
+import{_ as s,a as r}from"./tab-list-Bu4aJsO5.js";import"./iframe-bvFetpIT.js";import"./preload-helper-CDE7oBwp.js";import"./index-BsBlz6iG.js";import"./tooltip-C3eQIFVz.js";import"./constants-y2N5m1XS.js";import"./document-scroll-listener-D6BJnLBr.js";import"./floating-components-CKmcRn_b.js";const{fn:p}=__STORYBOOK_MODULE_TEST__,T={title:"Components/DBTabItem/States",component:r,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{label:{control:"text"},active:{control:"boolean"},disabled:{control:"boolean"},showIcon:{control:"boolean"},showIconLeading:{control:"boolean"},showIconTrailing:{control:"boolean"},noText:{control:"boolean"},id:{control:"text"},autofocus:{control:"boolean"}}},a={args:{label:"(Default) Enabled",default:""},render:e=>({components:{DBTabItem:r,DBTabList:s},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},t={args:{label:"active",active:!0,default:""},render:e=>({components:{DBTabItem:r,DBTabList:s},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},n={args:{label:"disabled",disabled:!0,default:""},render:e=>({components:{DBTabItem:r,DBTabList:s},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "(Default) Enabled",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...a.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "active",
+    "active": true,
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "disabled",
+    "disabled": true,
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...n.parameters?.docs?.source}}};const D=["DefaultEnabled","active","disabled"];export{a as DefaultEnabled,D as __namedExportsOrder,t as active,T as default,n as disabled};
