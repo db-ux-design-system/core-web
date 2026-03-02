84e09c68f34c0eeea309f3815fe99d3969ee0dbd
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-03-02 14:32:00 +0000

    deploy: 82c6d2bf9f5bdf61ced874cbcef0629c4be86fd1

---
+++ review/fix-preserve-whitespaces-infotext/composition-storybook/assets/formatter-EIJCOSYU-DwQHXoL2.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-CZRlahUi.js";import{b as o}from"./iframe-CLHuJI_Y.js";import"./preload-helper-DUWzm4uB.js";import"./index-DdZ4ftwC.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
