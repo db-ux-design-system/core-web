f58223f7f0c686e98ce6242fee5664d8f2d6c7bc
mfranzke <mfranzke@users.noreply.github.com>
2026-03-03 05:17:00 +0000

    deploy: a793e00ff7eea15cba56733381bfca68e57789cc

---
+++ review/dependabot-github_actions-actions-download-artifact-8/composition-storybook/assets/formatter-EIJCOSYU-BrUSjzTM.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-Dx3WityX.js";import{b as o}from"./iframe-DPHIe-bq.js";import"./preload-helper-CYPC2WgR.js";import"./index-BdmephBJ.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
