9f417ff771c8e50ee18229b7702d22ed79406972
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-03-03 09:40:00 +0100

    fix: remove not working script that should fix content for stencil

--- packages/components/configs/stencil/index.cjs
+++ packages/components/configs/stencil/index.cjs
@@ -1,7 +1,6 @@
 const onClickPlugin = require('../plugins/on-click.cjs');
 const slotsPlugin = require('../plugins/stencil/slots.cjs');
 const attributePassingPlugin = require('../plugins/attribute-passing/index.cjs');
-const hostDisplayContentsPlugin = require('../plugins/stencil/host-display-contents.cjs');
 
 /**
  * @type {import('@builder.io/mitosis').ToStencilOptions}
@@ -12,5 +11,5 @@ module.exports = {
 		enabled: true,
 		customRef: '_ref'
 	},
-	plugins: [attributePassingPlugin, slotsPlugin, onClickPlugin, hostDisplayContentsPlugin]
+	plugins: [attributePassingPlugin, slotsPlugin, onClickPlugin]
 };
