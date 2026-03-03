a50bd40bb0d894039b7993c486902adb1a061d60
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-03-03 14:45:18 +0000

    deploy: 4a8c4ef612d29d1255fade02d6ab3af8b1f375b0

---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/show-icon-trailing.example.stories-DxCtsmK8.js
@@ -0,0 +1,39 @@
+import{_ as o,a as t}from"./tab-list-Gpgtvqdn.js";import"./iframe-COxeOdOJ.js";import"./preload-helper-CDE7oBwp.js";import"./index-csvFxEYc.js";import"./tooltip-BUtwXAVJ.js";import"./document-scroll-listener-BSUHJGOU.js";import"./floating-components-CKmcRn_b.js";const{fn:b}=__STORYBOOK_MODULE_TEST__,p={title:"Components/DBTabItem/Show Icon Trailing",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{label:{control:"text"},active:{control:"boolean"},disabled:{control:"boolean"},showIcon:{control:"boolean"},showIconLeading:{control:"boolean"},showIconTrailing:{control:"boolean"},noText:{control:"boolean"},id:{control:"text"},autofocus:{control:"boolean"}}},a={args:{label:"(Default) False",iconTrailing:"x_placeholder",showIconTrailing:!1,default:""},render:e=>({components:{DBTabItem:t,DBTabList:o},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},n={args:{label:"True",iconTrailing:"x_placeholder",showIconTrailing:!0,default:""},render:e=>({components:{DBTabItem:t,DBTabList:o},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "(Default) False",
+    "iconTrailing": "x_placeholder",
+    "showIconTrailing": false,
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...a.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "True",
+    "iconTrailing": "x_placeholder",
+    "showIconTrailing": true,
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...n.parameters?.docs?.source}}};const u=["DefaultFalse","True"];export{a as DefaultFalse,n as True,u as __namedExportsOrder,p as default};
