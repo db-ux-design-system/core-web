a65c5ad09c80033008ed6c22c2e17310d917c729
mfranzke <mfranzke@users.noreply.github.com>
2026-03-03 05:35:46 +0000

    deploy: b1152f4a71905b0cc1d7c89339db22b8d2ee6000

---
+++ review/docs-added-code-styles-section-for-github-actions-instructions-in-copilot-file/vue-storybook/assets/formatter-EIJCOSYU-D9bAgy0-.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-B4Qo2Wz1.js";import{Y as o}from"./iframe-C5y8L_AR.js";import"./preload-helper-Bo55khbC.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
