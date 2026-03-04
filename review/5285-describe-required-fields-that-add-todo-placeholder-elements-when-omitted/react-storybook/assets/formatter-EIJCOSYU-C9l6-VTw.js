decf417b70fa8f6bfba6b8e38a6c00c196055686
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-03-04 08:52:50 +0000

    deploy: a5ee93fcdb6acac023df29bef723bdb2dd57c843

---
+++ review/5285-describe-required-fields-that-add-todo-placeholder-elements-when-omitted/react-storybook/assets/formatter-EIJCOSYU-C9l6-VTw.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-2_pB_PKX.js";import{G as t}from"./iframe-DiQ9cMp3.js";import"./preload-helper-kqTXPUif.js";import"./react-18-LzbmTrST.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
