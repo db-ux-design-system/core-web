d7bf918911b67354b6659b1c44a4bd0d0179ee39
mfranzke <mfranzke@users.noreply.github.com>
2026-03-09 09:19:41 +0000

    deploy: 155550f2f6359c399919911496657c4c2a95d993

---
+++ review/copilot/fix-pnpm-compatibility-issue/react-storybook/assets/formatter-EIJCOSYU-BKkhJ3OM.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-BVL-hh-B.js";import{G as t}from"./iframe-CAOQV43B.js";import"./preload-helper-CBUVLafE.js";import"./react-18-CAZuhqWT.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
