ae3cb40d57700dc9075bcb851517b02b37412e76
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-03-11 08:54:27 +0000

    deploy: 8b5b0b7d7c2028fa6138043f3dc0f5d760ba24c8

---
+++ review/docs-research-pagination/composition-storybook/assets/formatter-EIJCOSYU-6wLhPIdZ.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-BrC2EQxo.js";import{b as o}from"./iframe-BZeFzse0.js";import"./preload-helper-CSsWtTwL.js";import"./index-BzYeaJSl.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
---
+++ review/docs-research-pagination/vue-storybook/assets/formatter-EIJCOSYU-CuIStbbH.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-CRI04MVC.js";import{Y as o}from"./iframe-CwdqohEs.js";import"./preload-helper-dVuaIo5Z.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
