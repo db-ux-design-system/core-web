52e784e78f7080efc686b30619c25446a356dc49
mfranzke <mfranzke@users.noreply.github.com>
2026-03-12 07:21:36 +0000

    deploy: 95a8b5fe508b752551406b3bc092a75fe118eec7

---
+++ review/copilot/fix-screen-reader-navigation/vue-storybook/assets/formatter-EIJCOSYU--oUf6g_u.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-uvhbTuqb.js";import{Y as o}from"./iframe-9Jg8KQPx.js";import"./preload-helper-lIT-FiXL.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
