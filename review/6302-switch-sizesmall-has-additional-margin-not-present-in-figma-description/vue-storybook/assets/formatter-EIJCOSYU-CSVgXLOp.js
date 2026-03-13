ec595bdd1899d68fe87ad21d60158b9e77805775
mfranzke <mfranzke@users.noreply.github.com>
2026-03-13 14:34:22 +0000

    deploy: a9166c5dc80a3662c413290e3d67fb5558756316

---
+++ review/6302-switch-sizesmall-has-additional-margin-not-present-in-figma-description/vue-storybook/assets/formatter-EIJCOSYU-CSVgXLOp.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-DGu2zdAN.js";import{Y as o}from"./iframe-CRCngBIr.js";import"./preload-helper-DJORJv9G.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
