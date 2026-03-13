462d5b680eed069fbaf536881846e17d4c211ea1
mfranzke <mfranzke@users.noreply.github.com>
2026-03-13 16:41:30 +0000

    deploy: a6d4c2688868ecf9b5cb958a5d1b1c5eef240384

---
+++ review/copilot/migrate-tooltip-popover-css/react-storybook/assets/formatter-EIJCOSYU-C0rs81XE.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-DYp53-79.js";import{G as t}from"./iframe-BIUsqFJq.js";import"./preload-helper-DG2MXvbe.js";import"./react-18-dNiMMXtA.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
