325f2bc26e0caebc1be79990bf3bdd153f9616c7
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-03-13 13:59:06 +0100

    fix(tab-item): remove passing of data-density to internal button

--- packages/components/src/components/tab-item/tab-item.lite.tsx
+++ packages/components/src/components/tab-item/tab-item.lite.tsx
@@ -146,7 +146,6 @@ export default function DBTabItem(props: DBTabItemProps) {
 			ref={_ref}
 			type="button"
 			role="tab"
-			data-density={props.density || props['data-density']}
 			class={cls('db-tab-item', props.className)}
 			// suppresses native browser tooltips inherited from parent elements
 			title=""
