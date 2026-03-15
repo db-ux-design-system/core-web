2f9849e816fdacd0bf1e50ea864fee28fa4253a2
nmerget <nmerget@users.noreply.github.com>
2026-03-15 02:10:02 +0000

    deploy: 3c62cdc1b9dd5c54e246d9940110508fd1fe7b79

--- review/copilot/evaluate-sass-to-postcss-migration/react-storybook/assets/formatter-EIJCOSYU-D9ZB-YgA.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-LL677BLK-CQItS7OF.js";import{G as t}from"./iframe-D1-rpRnk.js";import"./preload-helper-Bz_S03pW.js";import"./react-18-Ch2wA-xd.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
