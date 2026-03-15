28ff45c4beed01ab5ce4d006e96860ca95118b97
mfranzke <mfranzke@users.noreply.github.com>
2026-03-15 11:22:55 +0000

    deploy: 53387516869f9bf59248412f3148a7ba9ef24b9f

---
+++ review/copilot/add-documentation-for-db-ux-eslint-plugin/vue-storybook/assets/formatter-EIJCOSYU-DlPJtfY0.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-LL677BLK-t15noRSJ.js";import{Y as o}from"./iframe-nIazu2Rn.js";import"./preload-helper-6-lW8AGB.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
