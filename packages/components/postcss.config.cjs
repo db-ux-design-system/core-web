c7e87fb8c322f5926d71ae6612d0ff8cb2a53e33
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2026-03-16 05:45:29 +0100

    refactor: adapted further aspects

--- packages/components/postcss.config.cjs
+++ packages/components/postcss.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
 	plugins: [
+		require('postcss-import'),
+		require('postcss-each'),
+		require('postcss-transform-mixins'),
 		require('cssnano')({
 			preset: [
 				'default',
