e2e85dd54aa1af7b35cecfcd04e058f84799cc5f
nmerget <nmerget@users.noreply.github.com>
2026-03-26 02:08:28 +0000

    deploy: 4c5fc9266402d9585087f4738a1a800cff1515f1

--- review/docs-vite-mentioning-version-8-configuration/vue-storybook/assets/formatter-EIJCOSYU-CBJEOZzW.js
+++
@@ -1,1 +0,0 @@
-import{d as i,b as m}from"./DocsRenderer-LL677BLK-DgNWT-U9.js";import{Y as o}from"./iframe-DAGjbJWK.js";import"./preload-helper-DrtYv9rr.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
