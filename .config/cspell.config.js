bb4e5777d622a6f7082c4c5edca5f6d5c52f8b38
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-03-31 16:58:35 +0200

    chore(mcp-server): change filetype of cspell.config to js, add icon-migration to ignored files

--- .config/cspell.config.js
+++ .config/cspell.config.js
@@ -37,6 +37,7 @@ export default defineConfig({
 		'showcases/patternhub/**',
 		'packages/mcp-server/src/manifest.json',
 		'storybooks/angular-storybook/src/components/documentation.json',
+		'docs/migration/icon-migration.md',
 		// Examples
 		'showcases/react-showcase/src/components/form/index.tsx',
 		'showcases/patternhub/pages/foundations/densities/examples.tsx',
