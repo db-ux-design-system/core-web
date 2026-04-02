ee127c2036d2203bf6ed46baead9829ca179fe68
nmerget <nmerget@users.noreply.github.com>
2026-04-02 06:20:13 +0000

    deploy: 3fbf10aa06c0943066a7e57ea7e17376d14b1fa3

---
+++ review/docs-user-testing/react-storybook/assets/formatter-EIJCOSYU-BrdNSE4D.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-LL677BLK-4wYcrK_B.js";import{G as t}from"./iframe-DApk7mEj.js";import"./preload-helper-D5_NMOIt.js";import"./react-18-BEoSh4AX.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
