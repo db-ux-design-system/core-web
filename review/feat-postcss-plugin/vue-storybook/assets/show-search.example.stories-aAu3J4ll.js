982e4ef2653633c513609b905eeab78494441aa3
nmerget <nmerget@users.noreply.github.com>
2026-04-08 08:57:04 +0000

    deploy: fee6c6080fd24113df9a258b277dcbb2391edba9

---
+++ review/feat-postcss-plugin/vue-storybook/assets/show-search.example.stories-aAu3J4ll.js
@@ -0,0 +1,98 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t,x as n}from"./src-DzOr8I67.js";var r,i,a,o,s;e((()=>{t(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/DBCustomSelect/Show Search`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],args:{onAmountChange:r(),onOptionSelected:r(),onDropdownToggle:r(),onSearch:r()},argTypes:{options:{control:`object`},label:{control:`text`},placeholder:{control:`text`},id:{control:`text`},multiple:{control:`boolean`},variant:{control:`select`,options:[`above`,`floating`]},values:{control:`object`},showLabel:{control:`boolean`},message:{control:`text`},showMessage:{control:`boolean`},showIcon:{control:`boolean`},validation:{control:`select`,options:[`invalid`,`valid`,`no-validation`]},invalidMessage:{control:`text`},validMessage:{control:`text`},required:{control:`boolean`},showRequiredAsterisk:{control:`boolean`},disabled:{control:`boolean`},name:{control:`text`},form:{control:`text`},ariaDescribedBy:{control:`text`},formFieldWidth:{control:`select`,options:[`full`,`auto`]},dropdownWidth:{control:`select`,options:[`auto`,`fixed`]},placement:{control:`select`,options:[`top`,`bottom`,`top-start`,`top-end`,`bottom-start`,`bottom-end`]},selectedType:{control:`select`,options:[`amount`,`text`,`tag`]},showNoResults:{control:`boolean`},noResultsText:{control:`text`},showLoading:{control:`boolean`},loadingText:{control:`text`},showSearch:{control:`boolean`},showSelectAll:{control:`boolean`},showClearSelection:{control:`boolean`},removeTagsTexts:{control:`object`},searchValue:{control:`text`},searchLabel:{control:`text`},searchPlaceholder:{control:`text`},selectedLabels:{control:`text`},selectedPrefix:{control:`text`},selectAllLabel:{control:`text`},listLabel:{control:`text`},clearSelectionText:{control:`text`},amountText:{control:`text`},mobileCloseButtonText:{control:`text`},open:{control:`boolean`},autofocus:{control:`boolean`},onAmountChange:{action:`onAmountChange`},onOptionSelected:{action:`onOptionSelected`},onDropdownToggle:{action:`onDropdownToggle`},onSearch:{action:`onSearch`}}},a={args:{label:`(Default) False`,selectAllLabel:`Select all`,searchLabel:`Search`,listLabel:`id-10254-(Default) False`,options:[{value:`Option 1`,id:`09nure8co`},{value:`Option 2`,id:`74n91hp14`},{value:`Option 3`,id:`74n91hp15`},{value:`Option 4`,id:`74n91hp16`},{value:`Option 5`,id:`74n91hp17`},{value:`Option 6`,id:`74n91hp18`},{value:`Option 7`,id:`74n91hp19`},{value:`Option 8`,id:`74n91hp24`},{value:`Option 9`,id:`74n91hp34`},{value:`Option 10`,id:`74n91hp44`}],showSearch:!1,multiple:!0,default:``},render:e=>({components:{DBCustomSelect:n},setup(){return{args:e}},template:`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >${e.default}</DBCustomSelect></div>`})},o={args:{selectAllLabel:`Select all`,searchLabel:`Search`,label:`True`,listLabel:`id-10255-True`,options:[{value:`Option 1`,id:`ktmbve0k2`},{value:`Option 2`,id:`uf9wakkc1`},{value:`Option 3`,id:`uf9wakkc2`},{value:`Option 4`,id:`uf9wakkc3`},{value:`Option 5`,id:`uf9wakkc4`}],showSearch:!0,multiple:!0,default:``},render:e=>({components:{DBCustomSelect:n},setup(){return{args:e}},template:`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >${e.default}</DBCustomSelect></div>`})},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "(Default) False",
+    "selectAllLabel": "Select all",
+    "searchLabel": "Search",
+    "listLabel": "id-10254-(Default) False",
+    "options": [{
+      value: 'Option 1',
+      id: '09nure8co'
+    }, {
+      value: 'Option 2',
+      id: '74n91hp14'
+    }, {
+      value: 'Option 3',
+      id: '74n91hp15'
+    }, {
+      value: 'Option 4',
+      id: '74n91hp16'
+    }, {
+      value: 'Option 5',
+      id: '74n91hp17'
+    }, {
+      value: 'Option 6',
+      id: '74n91hp18'
+    }, {
+      value: 'Option 7',
+      id: '74n91hp19'
+    }, {
+      value: 'Option 8',
+      id: '74n91hp24'
+    }, {
+      value: 'Option 9',
+      id: '74n91hp34'
+    }, {
+      value: 'Option 10',
+      id: '74n91hp44'
+    }],
+    "showSearch": false,
+    "multiple": true,
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomSelect
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >\${args.default}</DBCustomSelect></div>\`
+  })
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "selectAllLabel": "Select all",
+    "searchLabel": "Search",
+    "label": "True",
+    "listLabel": "id-10255-True",
+    "options": [{
+      value: 'Option 1',
+      id: 'ktmbve0k2'
+    }, {
+      value: 'Option 2',
+      id: 'uf9wakkc1'
+    }, {
+      value: 'Option 3',
+      id: 'uf9wakkc2'
+    }, {
+      value: 'Option 4',
+      id: 'uf9wakkc3'
+    }, {
+      value: 'Option 5',
+      id: 'uf9wakkc4'
+    }],
+    "showSearch": true,
+    "multiple": true,
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomSelect
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >\${args.default}</DBCustomSelect></div>\`
+  })
+}`,...o.parameters?.docs?.source}}},s=[`DefaultFalse`,`True`]}))();export{a as DefaultFalse,o as True,s as __namedExportsOrder,i as default};
