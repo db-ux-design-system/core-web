fd292608b63472e405e3507a55f5dc177725c218
nmerget <nmerget@users.noreply.github.com>
2026-04-10 07:46:03 +0000

    deploy: 17629755c0c249808c0bb69686f167677d4181ee

---
+++ review/main/vue-storybook/assets/density.example.stories-m9McKqDj.js
@@ -0,0 +1,49 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{S as t,t as n}from"./src-ColJ0xlm.js";var r,i,a,o,s,c;e((()=>{n(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/DBTag/Density`,component:t,parameters:{layout:`centered`},tags:[`autodocs`],args:{onRemove:r()},argTypes:{emphasis:{control:`select`,options:[`weak`,`strong`]},semantic:{control:`select`,options:[`adaptive`,`neutral`,`critical`,`informational`,`warning`,`successful`]},behavior:{control:`select`,options:[`static`,`removable`]},showIcon:{control:`boolean`},noText:{control:`boolean`},content:{control:`text`},showCheckState:{control:`boolean`},overflow:{control:`boolean`},removeButton:{control:`text`},text:{control:`text`},value:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`},onRemove:{action:`onRemove`}}},a={args:{"data-density":`functional`,default:`Functional`},render:e=>({components:{DBTag:t},setup(){return{args:e}},template:`<DBTag v-bind="args"   >${e.default}</DBTag>`})},o={args:{"data-density":`regular`,default:`(Default) Regular`},render:e=>({components:{DBTag:t},setup(){return{args:e}},template:`<DBTag v-bind="args"   >${e.default}</DBTag>`})},s={args:{"data-density":`expressive`,default:`Expressive`},render:e=>({components:{DBTag:t},setup(){return{args:e}},template:`<DBTag v-bind="args"   >${e.default}</DBTag>`})},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "functional",
+    "default": \`Functional\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTag
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTag v-bind="args"   >\${args.default}</DBTag>\`
+  })
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "default": \`(Default) Regular\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTag
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTag v-bind="args"   >\${args.default}</DBTag>\`
+  })
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "expressive",
+    "default": \`Expressive\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTag
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTag v-bind="args"   >\${args.default}</DBTag>\`
+  })
+}`,...s.parameters?.docs?.source}}},c=[`Functional`,`DefaultRegular`,`Expressive`]}))();export{o as DefaultRegular,s as Expressive,a as Functional,c as __namedExportsOrder,i as default};
