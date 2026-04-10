5921a4611748bb2f7ac9a60088de4787f27dd6a2
nmerget <nmerget@users.noreply.github.com>
2026-04-10 09:56:35 +0000

    deploy: 8d1fb61594a780a07d3d8586b09ae7a0cd2e414a

---
+++ review/fix-changeset-fixed-package/react-storybook/assets/density.example.stories-CgVhLiaW.js
@@ -0,0 +1,19 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-DnaggZxp.js";import{A as n,t as r}from"./src-C7TXoUjo.js";var i,a,o,s,c,l,u;e((()=>{r(),i=t(),{fn:a}=__STORYBOOK_MODULE_TEST__,o={title:`Components/DBBrand/Density`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{hideLogo:{control:`boolean`},showIcon:{control:`boolean`},text:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},s={args:{"data-density":`functional`,children:`Functional`},render:e=>(0,i.jsx)(n,{...e})},c={args:{"data-density":`regular`,children:`(Default) Regular`},render:e=>(0,i.jsx)(n,{...e})},l={args:{"data-density":`expressive`,children:`Expressive`},render:e=>(0,i.jsx)(n,{...e})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "functional",
+    "children": "Functional"
+  },
+  render: (properties: any) => <DBBrand {...properties} />
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "children": "(Default) Regular"
+  },
+  render: (properties: any) => <DBBrand {...properties} />
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "expressive",
+    "children": "Expressive"
+  },
+  render: (properties: any) => <DBBrand {...properties} />
+}`,...l.parameters?.docs?.source}}},u=[`Functional`,`DefaultRegular`,`Expressive`]}))();export{c as DefaultRegular,l as Expressive,s as Functional,u as __namedExportsOrder,o as default};
---
+++ review/fix-changeset-fixed-package/vue-storybook/assets/show-icon.example.stories-BX_dHL03.js
@@ -0,0 +1,43 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{p as t,t as n}from"./src-ColJ0xlm.js";var r,i,a,o,s;e((()=>{n(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/DBNotification/Show Icon`,component:t,parameters:{layout:`centered`},tags:[`autodocs`],args:{onClose:r()},argTypes:{headline:{control:`text`},showIcon:{control:`boolean`},variant:{control:`select`,options:[`docked`,`standalone`,`overlay`]},semantic:{control:`select`,options:[`adaptive`,`neutral`,`critical`,`informational`,`warning`,`successful`]},closeable:{control:`boolean`},linkVariant:{control:`select`,options:[`block`,`inline`]},showHeadline:{control:`boolean`},showTimestamp:{control:`boolean`},timestamp:{control:`text`},ariaLive:{control:`select`,options:[`assertive`,`polite`,`off`]},text:{control:`text`},role:{control:`text`},closeButtonId:{control:`text`},closeButtonText:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`},onClose:{action:`onClose`}}},a={args:{icon:`information_circle`,showIcon:!0,default:`(Default) True`},render:e=>({components:{DBNotification:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},o={args:{icon:`information_circle`,showIcon:!1,default:`False`},render:e=>({components:{DBNotification:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "information_circle",
+    "showIcon": true,
+    "default": \`(Default) True\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "information_circle",
+    "showIcon": false,
+    "default": \`False\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...o.parameters?.docs?.source}}},s=[`DefaultTrue`,`False`]}))();export{a as DefaultTrue,o as False,s as __namedExportsOrder,i as default};
