9e40716eea152961191490d7cd9d9137a3ee3ff5
mfranzke <mfranzke@users.noreply.github.com>
2026-04-17 14:32:21 +0000

    deploy: 583cce9a6758f6bd2c64c5168f541041ebcf159b

---
+++ review/main/vue-storybook/assets/checked.example.stories-BSBq_3VF.js
@@ -0,0 +1,35 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{E as t,t as n}from"./src-CGBMmFt6.js";var r,i,a,o,s;e((()=>{n(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/DBCheckbox/Checked`,component:t,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{name:{control:`text`},disabled:{control:`boolean`},checked:{control:`boolean`},indeterminate:{control:`boolean`},validation:{control:`select`,options:[`invalid`,`valid`,`no-validation`]},invalidMessage:{control:`text`},validMessage:{control:`text`},message:{control:`text`},showMessage:{control:`boolean`},size:{control:`select`,options:[`small`,`medium`]},required:{control:`boolean`},showLabel:{control:`boolean`},showRequiredAsterisk:{control:`boolean`},label:{control:`text`},placeholder:{control:`text`},variant:{control:`select`,options:[`above`,`floating`]},value:{control:`text`},form:{control:`text`},ariaDescribedBy:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},a={args:{name:`State`,checked:!1,default:`(Default) False`},render:e=>({components:{DBCheckbox:t},setup(){return{args:e}},template:`<DBCheckbox v-bind="args"   >${e.default}</DBCheckbox>`})},o={args:{name:`State`,checked:!0,default:`True`},render:e=>({components:{DBCheckbox:t},setup(){return{args:e}},template:`<DBCheckbox v-bind="args"   >${e.default}</DBCheckbox>`})},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "name": "State",
+    "checked": false,
+    "default": \`(Default) False\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCheckbox
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCheckbox v-bind="args"   >\${args.default}</DBCheckbox>\`
+  })
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "name": "State",
+    "checked": true,
+    "default": \`True\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCheckbox
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBCheckbox v-bind="args"   >\${args.default}</DBCheckbox>\`
+  })
+}`,...o.parameters?.docs?.source}}},s=[`DefaultFalse`,`True`]}))();export{a as DefaultFalse,o as True,s as __namedExportsOrder,i as default};
