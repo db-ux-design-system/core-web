064d2d67695387328c52d7bd97408c4c445c3021
mfranzke <mfranzke@users.noreply.github.com>
2026-04-17 06:51:03 +0000

    deploy: 10b78ab2c696f801b3a9fb4afd583ddc5903ff4a

---
+++ review/fix-warnings-on-package-json-structure/vue-storybook/assets/width.example.stories-BLhVQ7MC.js
@@ -0,0 +1,217 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{A as t,g as n,h as r,k as i,m as a,t as o,v as s}from"./src-D8ePn3Ki.js";var c,l,u,d,f,p;e((()=>{o(),{fn:c}=__STORYBOOK_MODULE_TEST__,l={title:`Components/DBHeader/Width`,component:s,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{width:{control:`select`,options:[`full`,`medium`,`large`,`small`]},forceMobile:{control:`boolean`},drawerOpen:{control:`boolean`},burgerMenuLabel:{control:`text`},closeButtonId:{control:`text`},closeButtonText:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},u={args:{default:`<DBNavigation aria-label="Full" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"><a href="#">Full</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Full disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>`},render:e=>({components:{DBHeader:s,DBBrand:t,DBButton:i,DBLink:n,DBNavigationItem:a,DBNavigation:r},setup(){return{args:e}},template:`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >${e.default}</DBHeader></div>`})},d={args:{width:`medium`,default:`<DBNavigation aria-label="Medium" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Medium</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Medium disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>`},render:e=>({components:{DBHeader:s,DBBrand:t,DBButton:i,DBLink:n,DBNavigationItem:a,DBNavigation:r},setup(){return{args:e}},template:`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >${e.default}</DBHeader></div>`})},f={args:{width:`large`,default:`<DBNavigation aria-label="Large" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Large</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Large disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>`},render:e=>({components:{DBHeader:s,DBBrand:t,DBButton:i,DBLink:n,DBNavigationItem:a,DBNavigation:r},setup(){return{args:e}},template:`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >${e.default}</DBHeader></div>`})},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "default": \`<DBNavigation aria-label="Full" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"><a href="#">Full</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Full disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBHeader,
+      DBBrand,
+      DBButton,
+      DBLink,
+      DBNavigationItem,
+      DBNavigation
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >\${args.default}</DBHeader></div>\`
+  })
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    "width": "medium",
+    "default": \`<DBNavigation aria-label="Medium" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Medium</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Medium disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBHeader,
+      DBBrand,
+      DBButton,
+      DBLink,
+      DBNavigationItem,
+      DBNavigation
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >\${args.default}</DBHeader></div>\`
+  })
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    "width": "large",
+    "default": \`<DBNavigation aria-label="Large" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Large</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Large disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBHeader,
+      DBBrand,
+      DBButton,
+      DBLink,
+      DBNavigationItem,
+      DBNavigation
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >\${args.default}</DBHeader></div>\`
+  })
+}`,...f.parameters?.docs?.source}}},p=[`Full`,`Medium`,`Large`]}))();export{u as Full,f as Large,d as Medium,p as __namedExportsOrder,l as default};
