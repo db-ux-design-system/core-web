188bca1f831bff90bb9aee44b26dc3b3752ac92d
mfranzke <mfranzke@users.noreply.github.com>
2026-04-18 07:07:10 +0000

    deploy: e845fdb4c00a77cf74a09b9e0e3eccb0f1aeda7c

---
+++ review/dependabot-github_actions-github-codeql-action-4.35.1/react-storybook/assets/density.example.stories-Dmtzhbkl.js
@@ -0,0 +1,56 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-DnaggZxp.js";import{D as n,c as r,t as i}from"./src-DK5dTc4z.js";var a,o,s,c,l,u,d;e((()=>{i(),a=t(),{fn:o}=__STORYBOOK_MODULE_TEST__,s={title:`Components/DBStack/Density`,component:r,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{variant:{control:`select`,options:[`simple`,`divider`]},gap:{control:`select`,options:[`none`,`3x-large`,`2x-large`,`x-large`,`large`,`medium`,`small`,`x-small`,`2x-small`,`3x-small`]},direction:{control:`select`,options:[`row`,`column`]},wrap:{control:`boolean`},alignment:{control:`select`,options:[`stretch`,`start`,`end`,`center`]},justifyContent:{control:`select`,options:[`space-between`,`start`,`end`,`center`]},id:{control:`text`},autofocus:{control:`boolean`}}},c={args:{style:{padding:`var(--db-spacing-fixed-xs)`},children:(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(`span`,{className:`dummy-component`,children:(0,a.jsx)(`a`,{href:`#`,children:`Content 1`})}),(0,a.jsx)(`span`,{className:`dummy-component`,children:`Content 2`}),(0,a.jsx)(`span`,{className:`dummy-component`,children:`Content 3`})]})},render:e=>(0,a.jsxs)(`div`,{"data-density":`functional`,style:{alignItems:`flex-start`,alignSelf:`flex-start`,display:`flex`,flexWrap:`nowrap`,flexDirection:`column`,gap:`var(--db-spacing-fixed-sm)`,width:`200px`},children:[(0,a.jsx)(n,{size:`small`,icon:`none`,semantic:`informational`,children:`Functional`}),(0,a.jsx)(r,{...e})]})},l={args:{"data-density":`regular`,style:{padding:`var(--db-spacing-fixed-xs)`},children:(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(`span`,{className:`dummy-component`,children:(0,a.jsx)(`a`,{href:`#`,children:`Content 1`})}),(0,a.jsx)(`span`,{className:`dummy-component`,children:`Content 2`}),(0,a.jsx)(`span`,{className:`dummy-component`,children:`Content 3`})]})},render:e=>(0,a.jsxs)(`div`,{style:{alignItems:`flex-start`,alignSelf:`flex-start`,display:`flex`,flexWrap:`nowrap`,flexDirection:`column`,gap:`var(--db-spacing-fixed-sm)`,width:`200px`},children:[(0,a.jsx)(n,{size:`small`,icon:`none`,semantic:`informational`,children:`(Default) Regular`}),(0,a.jsx)(r,{...e})]})},u={args:{style:{padding:`var(--db-spacing-fixed-xs)`},children:(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(`span`,{className:`dummy-component`,children:(0,a.jsx)(`a`,{href:`#`,children:`Content 1`})}),(0,a.jsx)(`span`,{className:`dummy-component`,children:`Content 2`}),(0,a.jsx)(`span`,{className:`dummy-component`,children:`Content 3`})]})},render:e=>(0,a.jsxs)(`div`,{"data-density":`expressive`,style:{alignItems:`flex-start`,alignSelf:`flex-start`,display:`flex`,flexWrap:`nowrap`,flexDirection:`column`,gap:`var(--db-spacing-fixed-sm)`,width:`200px`},children:[(0,a.jsx)(n,{size:`small`,icon:`none`,semantic:`informational`,children:`Expressive`}),(0,a.jsx)(r,{...e})]})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "children": <><span className="dummy-component"><a href="#">Content 1</a></span><span className="dummy-component">Content 2</span><span className="dummy-component">Content 3</span></>
+  },
+  render: (properties: any) => <div data-density="functional" style={{
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    flexDirection: 'column',
+    gap: 'var(--db-spacing-fixed-sm)',
+    width: '200px'
+  }}><DBInfotext size="small" icon="none" semantic="informational">
+                    Functional
+                </DBInfotext><DBStack {...properties} /></div>
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "children": <><span className="dummy-component"><a href="#">Content 1</a></span><span className="dummy-component">Content 2</span><span className="dummy-component">Content 3</span></>
+  },
+  render: (properties: any) => <div style={{
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    flexDirection: 'column',
+    gap: 'var(--db-spacing-fixed-sm)',
+    width: '200px'
+  }}><DBInfotext size="small" icon="none" semantic="informational">
+                    (Default) Regular
+                </DBInfotext><DBStack {...properties} /></div>
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "children": <><span className="dummy-component"><a href="#">Content 1</a></span><span className="dummy-component">Content 2</span><span className="dummy-component">Content 3</span></>
+  },
+  render: (properties: any) => <div data-density="expressive" style={{
+    alignItems: 'flex-start',
+    alignSelf: 'flex-start',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    flexDirection: 'column',
+    gap: 'var(--db-spacing-fixed-sm)',
+    width: '200px'
+  }}><DBInfotext size="small" icon="none" semantic="informational">
+                    Expressive
+                </DBInfotext><DBStack {...properties} /></div>
+}`,...u.parameters?.docs?.source}}},d=[`Functional`,`DefaultRegular`,`Expressive`]}))();export{l as DefaultRegular,u as Expressive,c as Functional,d as __namedExportsOrder,s as default};
---
+++ review/dependabot-github_actions-github-codeql-action-4.35.1/vue-storybook/assets/examples-floating-label.example.stories-6_cMf49-.js
@@ -0,0 +1,79 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{t,x as n}from"./src-CGBMmFt6.js";var r,i,a,o,s;e((()=>{t(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/DBCustomSelect/Examples Floating label`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],args:{onAmountChange:r(),onOptionSelected:r(),onDropdownToggle:r(),onSearch:r()},argTypes:{options:{control:`object`},label:{control:`text`},placeholder:{control:`text`},id:{control:`text`},multiple:{control:`boolean`},variant:{control:`select`,options:[`above`,`floating`]},values:{control:`object`},showLabel:{control:`boolean`},message:{control:`text`},showMessage:{control:`boolean`},showIcon:{control:`boolean`},validation:{control:`select`,options:[`invalid`,`valid`,`no-validation`]},invalidMessage:{control:`text`},validMessage:{control:`text`},required:{control:`boolean`},showRequiredAsterisk:{control:`boolean`},disabled:{control:`boolean`},name:{control:`text`},form:{control:`text`},ariaDescribedBy:{control:`text`},formFieldWidth:{control:`select`,options:[`full`,`auto`]},dropdownWidth:{control:`select`,options:[`auto`,`fixed`]},placement:{control:`select`,options:[`top`,`bottom`,`top-start`,`top-end`,`bottom-start`,`bottom-end`]},selectedType:{control:`select`,options:[`amount`,`text`,`tag`]},showNoResults:{control:`boolean`},noResultsText:{control:`text`},showLoading:{control:`boolean`},loadingText:{control:`text`},showSearch:{control:`boolean`},showSelectAll:{control:`boolean`},showClearSelection:{control:`boolean`},removeTagsTexts:{control:`object`},searchValue:{control:`text`},searchLabel:{control:`text`},searchPlaceholder:{control:`text`},selectedLabels:{control:`text`},selectedPrefix:{control:`text`},selectAllLabel:{control:`text`},listLabel:{control:`text`},clearSelectionText:{control:`text`},amountText:{control:`text`},mobileCloseButtonText:{control:`text`},open:{control:`boolean`},autofocus:{control:`boolean`},onAmountChange:{action:`onAmountChange`},onOptionSelected:{action:`onOptionSelected`},onDropdownToggle:{action:`onDropdownToggle`},onSearch:{action:`onSearch`}}},a={args:{variant:`floating`,label:`Floating`,listLabel:`id-10300-Floating`,options:[{value:`Option 1`,id:`otbjunoyx`},{value:`Option 2`,id:`ju53v02yg`},{value:`Option 3`,id:`ju53v03yg`},{value:`Option 4`,id:`ju53v04yg`},{value:`Option 5`,id:`ju53v05yg`}],default:``},render:e=>({components:{DBCustomSelect:n},setup(){return{args:e}},template:`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >${e.default}</DBCustomSelect></div>`})},o={args:{variant:`floating`,validation:`invalid`,invalidMessage:`Invalid Message`,label:`Floating`,listLabel:`id-10301-Floating`,options:[{value:`Option 1`,id:`otbjunoyx`},{value:`Option 2`,id:`ju53v02yg`},{value:`Option 3`,id:`ju53v03yg`},{value:`Option 4`,id:`ju53v04yg`},{value:`Option 5`,id:`ju53v05yg`}],default:``},render:e=>({components:{DBCustomSelect:n},setup(){return{args:e}},template:`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >${e.default}</DBCustomSelect></div>`})},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "variant": "floating",
+    "label": "Floating",
+    "listLabel": "id-10300-Floating",
+    "options": [{
+      value: 'Option 1',
+      id: 'otbjunoyx'
+    }, {
+      value: 'Option 2',
+      id: 'ju53v02yg'
+    }, {
+      value: 'Option 3',
+      id: 'ju53v03yg'
+    }, {
+      value: 'Option 4',
+      id: 'ju53v04yg'
+    }, {
+      value: 'Option 5',
+      id: 'ju53v05yg'
+    }],
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomSelect
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >\${args.default}</DBCustomSelect></div>\`
+  })
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "variant": "floating",
+    "validation": "invalid",
+    "invalidMessage": "Invalid Message",
+    "label": "Floating",
+    "listLabel": "id-10301-Floating",
+    "options": [{
+      value: 'Option 1',
+      id: 'otbjunoyx'
+    }, {
+      value: 'Option 2',
+      id: 'ju53v02yg'
+    }, {
+      value: 'Option 3',
+      id: 'ju53v03yg'
+    }, {
+      value: 'Option 4',
+      id: 'ju53v04yg'
+    }, {
+      value: 'Option 5',
+      id: 'ju53v05yg'
+    }],
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCustomSelect
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '200px'
+}"  ><DBCustomSelect v-bind="args"   >\${args.default}</DBCustomSelect></div>\`
+  })
+}`,...o.parameters?.docs?.source}}},s=[`Floating`,`FloatingInvalidMessage`]}))();export{a as Floating,o as FloatingInvalidMessage,s as __namedExportsOrder,i as default};
