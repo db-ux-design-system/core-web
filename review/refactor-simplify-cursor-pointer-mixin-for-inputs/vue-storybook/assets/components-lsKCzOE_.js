20fb9e5bae7e0ee534eba3159962cae16f9873c3
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-04-22 14:51:19 +0000

    deploy: 43bb1124be2d661a1e4b81a8a3b45b24c4d7215f

---
+++ review/refactor-simplify-cursor-pointer-mixin-for-inputs/vue-storybook/assets/components-lsKCzOE_.js
@@ -0,0 +1,1 @@
+import{r as e}from"./chunk-RD3KTAHR-DEK-KY_R.js";import{rt as t}from"./components-BUxG2Ff5.js";t();export{e as createCopyToClipboardFunction};
---
+++ review/refactor-simplify-cursor-pointer-mixin-for-inputs/vue-storybook/assets/direction.example.stories-D5I9BWq-.js
@@ -0,0 +1,88 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{D as t,c as n,t as r}from"./src-CGBMmFt6.js";var i,a,o,s,c;e((()=>{r(),{fn:i}=__STORYBOOK_MODULE_TEST__,a={title:`Components/DBStack/Direction`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{variant:{control:`select`,options:[`simple`,`divider`]},gap:{control:`select`,options:[`none`,`3x-large`,`2x-large`,`x-large`,`large`,`medium`,`small`,`x-small`,`2x-small`,`3x-small`]},direction:{control:`select`,options:[`row`,`column`]},wrap:{control:`boolean`},alignment:{control:`select`,options:[`stretch`,`start`,`end`,`center`]},justifyContent:{control:`select`,options:[`space-between`,`start`,`end`,`center`]},id:{control:`text`},autofocus:{control:`boolean`}}},o={args:{style:{padding:`var(--db-spacing-fixed-xs)`},default:`<span class="dummy-component"><a href="#">Content 1</a></span
+><span class="dummy-component">Content 2</span
+><span class="dummy-component">Content 3</span>`},render:e=>({components:{DBStack:n,DBInfotext:t},setup(){return{args:e}},template:`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  width: '200px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    (Default) Column
+                </DBInfotext><DBStack v-bind="args"   >${e.default}</DBStack></div>`})},s={args:{direction:`row`,style:{padding:`var(--db-spacing-fixed-xs)`},default:`<span class="dummy-component"><a href="#">Content 1</a></span
+><span class="dummy-component">Content 2</span
+><span class="dummy-component">Content 3</span>`},render:e=>({components:{DBStack:n,DBInfotext:t},setup(){return{args:e}},template:`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  height: '100px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    Row
+                </DBInfotext><DBStack v-bind="args"   >${e.default}</DBStack></div>`})},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "default": \`<span class="dummy-component"><a href="#">Content 1</a></span
+><span class="dummy-component">Content 2</span
+><span class="dummy-component">Content 3</span>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBStack,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  width: '200px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    (Default) Column
+                </DBInfotext><DBStack v-bind="args"   >\${args.default}</DBStack></div>\`
+  })
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "direction": "row",
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "default": \`<span class="dummy-component"><a href="#">Content 1</a></span
+><span class="dummy-component">Content 2</span
+><span class="dummy-component">Content 3</span>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBStack,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  height: '100px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    Row
+                </DBInfotext><DBStack v-bind="args"   >\${args.default}</DBStack></div>\`
+  })
+}`,...s.parameters?.docs?.source}}},c=[`DefaultColumn`,`Row`]}))();export{o as DefaultColumn,s as Row,c as __namedExportsOrder,a as default};
