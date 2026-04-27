bb86f44a8a2327c02f3f4627412c9b400b56e0c3
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-04-27 08:24:35 +0000

    deploy: 349f06142a2af6514a75a8195126da623d5ccbd6

---
+++ review/refactor-adapting-moduleresolution-setting/vue-storybook/assets/examples-variant-docked.example.stories-CvADxAqX.js
@@ -0,0 +1,191 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{g as t,p as n,t as r}from"./src-CGBMmFt6.js";var i,a,o,s,c,l,u,d,f,p,m;e((()=>{r(),{fn:i}=__STORYBOOK_MODULE_TEST__,a={title:`Components/DBNotification/Examples - Variant:Docked`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],args:{onClose:i()},argTypes:{headline:{control:`text`},showIcon:{control:`boolean`},variant:{control:`select`,options:[`docked`,`standalone`,`overlay`]},semantic:{control:`select`,options:[`adaptive`,`neutral`,`critical`,`informational`,`warning`,`successful`]},closeable:{control:`boolean`},linkVariant:{control:`select`,options:[`block`,`inline`]},showHeadline:{control:`boolean`},showTimestamp:{control:`boolean`},timestamp:{control:`text`},ariaLive:{control:`select`,options:[`assertive`,`polite`,`off`]},text:{control:`text`},role:{control:`text`},closeButtonId:{control:`text`},closeButtonText:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`},onClose:{action:`onClose`}}},o={args:{default:`Text`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},s={args:{icon:`information_circle`,default:`Text & Icon`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},c={args:{default:`Text & Preview Image<template v-slot:image
+  ><img alt="this is a fancy placeholder" :src="getImage()"
+/></template>`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},l={args:{headline:`Headline`,default:`Text & Headline`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},u={args:{default:`Text & Textlink Block<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},d={args:{linkVariant:`inline`,default:`Text & Textlink Inline<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},f={args:{headline:`Headline`,linkVariant:`inline`,closeable:!0,default:`Text & Headline & Textlink Inline & Closeable<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},p={args:{icon:`information_circle`,headline:`Headline`,linkVariant:`inline`,closeable:!0,default:`Text & Icon & Headline & Textlink Inline & Closeable<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>`},render:e=>({components:{DBNotification:n,DBLink:t},setup(){return{args:e}},template:`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >${e.default}</DBNotification></div>`})},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "default": \`Text\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "information_circle",
+    "default": \`Text & Icon\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "default": \`Text & Preview Image<template v-slot:image
+  ><img alt="this is a fancy placeholder" :src="getImage()"
+/></template>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "headline": "Headline",
+    "default": \`Text & Headline\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "default": \`Text & Textlink Block<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    "linkVariant": "inline",
+    "default": \`Text & Textlink Inline<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    "headline": "Headline",
+    "linkVariant": "inline",
+    "closeable": true,
+    "default": \`Text & Headline & Textlink Inline & Closeable<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    "icon": "information_circle",
+    "headline": "Headline",
+    "linkVariant": "inline",
+    "closeable": true,
+    "default": \`Text & Icon & Headline & Textlink Inline & Closeable<template v-slot:link
+  ><DBLink href="#">Textlink</DBLink></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBNotification,
+      DBLink
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '300px'
+}"  ><DBNotification v-bind="args"   >\${args.default}</DBNotification></div>\`
+  })
+}`,...p.parameters?.docs?.source}}},m=[`Text`,`TextIcon`,`TextPreviewImage`,`TextHeadline`,`TextTextlinkBlock`,`TextTextlinkInline`,`TextHeadlineTextlinkInlineCloseable`,`TextIconHeadlineTextlinkInlineCloseable`]}))();export{o as Text,l as TextHeadline,f as TextHeadlineTextlinkInlineCloseable,s as TextIcon,p as TextIconHeadlineTextlinkInlineCloseable,c as TextPreviewImage,u as TextTextlinkBlock,d as TextTextlinkInline,m as __namedExportsOrder,a as default};
