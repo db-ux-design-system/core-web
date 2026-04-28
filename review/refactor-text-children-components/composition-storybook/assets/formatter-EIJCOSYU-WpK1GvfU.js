ee02acdee856c5cdc66bfc2f494b20d26c13a9fe
nmerget <nmerget@users.noreply.github.com>
2026-04-28 10:00:22 +0000

    deploy: 467096f1e7600ebdf8d9d56e777c851854d776e2

---
+++ review/refactor-text-children-components/composition-storybook/assets/formatter-EIJCOSYU-WpK1GvfU.js
@@ -0,0 +1,1 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{f as t,p as n}from"./iframe-D8z4BZMl.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-BG6YGio4.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
