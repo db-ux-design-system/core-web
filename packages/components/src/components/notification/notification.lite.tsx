c992001ef7f34ccd2ffa724c2a8a7e223812e713
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2026-04-28 06:55:46 +0200

    Update notification.lite.tsx

--- packages/components/src/components/notification/notification.lite.tsx
+++ packages/components/src/components/notification/notification.lite.tsx
@@ -62,7 +62,7 @@ export default function DBNotification(props: DBNotificationProps) {
 			<Show when={stringPropVisible(props.headline, props.showHeadline)}>
 				<header>{props.headline}</header>
 			</Show>
-			<div data-area="contents">
+			<div data-area="content">
 				<Show when={props.text} else={props.children}>
 					{props.text}
 				</Show>
