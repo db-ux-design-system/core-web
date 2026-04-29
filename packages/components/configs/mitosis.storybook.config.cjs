ab5bc394149c992258cd4945d914c9a37f4d0022
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-04-29 19:22:20 +0200

    fix(tabs): rename nested example to .example.lite.tsx and exclude from storybook

--- packages/components/configs/mitosis.storybook.config.cjs
+++ packages/components/configs/mitosis.storybook.config.cjs
@@ -8,6 +8,7 @@ const vue = require('./vue/index.cjs');
  */
 module.exports = {
 	files: ['**/*.example.lite.tsx'],
+	exclude: ['**/tabs/examples/nested.example.lite.tsx'],
 	targets: ['angular', 'react', 'vue'],
 	dest: '../../storybooks',
 	getTargetPath: ({ target }) => {
