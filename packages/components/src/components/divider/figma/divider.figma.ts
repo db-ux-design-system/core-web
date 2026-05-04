f48038a11c988d4bff906dd888355232b9bbfd16
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-05-04 14:19:42 +0200

    feat(code-connect): add figma code connect files for divider

---
+++ packages/components/src/components/divider/figma/divider.figma.ts
@@ -0,0 +1,35 @@
+import { FigmaCodeConnect, FigmaProp } from '../../../shared/figma';
+
+export type FigmaDividerProps = {
+	variant?: string;
+	emphasis?: string;
+};
+
+const dividerProps: Record<string, FigmaProp> = {
+	variant: {
+		type: 'enum',
+		key: 'Variant',
+		value: {
+			'(Def) Horizontal': 'horizontal',
+			Vertical: 'vertical'
+		}
+	},
+	emphasis: {
+		type: 'enum',
+		key: 'Emphasis',
+		value: {
+			'(Def) Weak': 'weak',
+			Strong: 'strong'
+		}
+	}
+};
+
+export const dividers: FigmaCodeConnect = {
+	urls: [
+		'https://www.figma.com/design/FIGMA_FILE?node-id=1:21537',
+		'https://www.figma.com/design/FIGMA_FILE?node-id=1:21539',
+		'https://www.figma.com/design/FIGMA_FILE?node-id=1:21541',
+		'https://www.figma.com/design/FIGMA_FILE?node-id=1:21543'
+	],
+	props: dividerProps
+};
