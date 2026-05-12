ad3a98122a657403200eda1c197dfb5e5835041c
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-05-12 13:24:01 +0200

    chore: update _brand.arg.types.ts

--- packages/components/src/components/brand/examples/_brand.arg.types.ts
+++ packages/components/src/components/brand/examples/_brand.arg.types.ts
@@ -1,8 +1,6 @@
 import type { InputType } from 'storybook/internal/csf';
-import { StorybookIconArgTypes } from '../../../shared/examples/_icons.arg.types';
 
 export const StorybookBrandArgTypes: Record<string, InputType> = {
-	...StorybookIconArgTypes,
 	text: { control: 'text' },
 	id: { control: 'text' },
 	autofocus: { control: 'boolean' }
