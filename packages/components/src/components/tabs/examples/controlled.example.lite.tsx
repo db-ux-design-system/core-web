44125a6a0b3292de29f1803a0c0153b4d18fcb85
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-05-20 08:52:32 +0200

    fix(tabs): wrap controlled example in Fragment for Storybook AST parser

--- packages/components/src/components/tabs/examples/controlled.example.lite.tsx
+++ packages/components/src/components/tabs/examples/controlled.example.lite.tsx
@@ -1,4 +1,4 @@
-import { useMetadata, useStore } from '@builder.io/mitosis';
+import { Fragment, useMetadata, useStore } from '@builder.io/mitosis';
 import DBButton from '../../button/button.lite';
 import DBTabItem from '../../tab-item/tab-item.lite';
 import DBTabList from '../../tab-list/tab-list.lite';
@@ -18,44 +18,46 @@ export default function TabsControlled() {
 	});
 
 	return (
-		<div class="fit-content-container">
-			<div>
-				<DBButton
-					variant="outlined"
-					onClick={() => {
-						state.activeTabIndex = 0;
+		<Fragment>
+			<div class="fit-content-container">
+				<div data-sb-replace="Use external buttons to control active tab">
+					<DBButton
+						variant="outlined"
+						onClick={() => {
+							state.activeTabIndex = 0;
+						}}>
+						Select Tab 1
+					</DBButton>
+					<DBButton
+						variant="outlined"
+						onClick={() => {
+							state.activeTabIndex = 1;
+						}}>
+						Select Tab 2
+					</DBButton>
+					<DBButton
+						variant="outlined"
+						onClick={() => {
+							state.activeTabIndex = 2;
+						}}>
+						Select Tab 3
+					</DBButton>
+				</div>
+				<DBTabs
+					activeIndex={state.activeTabIndex}
+					onIndexChange={(index) => {
+						state.activeTabIndex = index;
 					}}>
-					Select Tab 1
-				</DBButton>
-				<DBButton
-					variant="outlined"
-					onClick={() => {
-						state.activeTabIndex = 1;
-					}}>
-					Select Tab 2
-				</DBButton>
-				<DBButton
-					variant="outlined"
-					onClick={() => {
-						state.activeTabIndex = 2;
-					}}>
-					Select Tab 3
-				</DBButton>
+					<DBTabList>
+						<DBTabItem>Tab 1</DBTabItem>
+						<DBTabItem>Tab 2</DBTabItem>
+						<DBTabItem>Tab 3</DBTabItem>
+					</DBTabList>
+					<DBTabPanel>Content of Tab 1</DBTabPanel>
+					<DBTabPanel>Content of Tab 2</DBTabPanel>
+					<DBTabPanel>Content of Tab 3</DBTabPanel>
+				</DBTabs>
 			</div>
-			<DBTabs
-				activeIndex={state.activeTabIndex}
-				onIndexChange={(index) => {
-					state.activeTabIndex = index;
-				}}>
-				<DBTabList>
-					<DBTabItem>Tab 1</DBTabItem>
-					<DBTabItem>Tab 2</DBTabItem>
-					<DBTabItem>Tab 3</DBTabItem>
-				</DBTabList>
-				<DBTabPanel>Content of Tab 1</DBTabPanel>
-				<DBTabPanel>Content of Tab 2</DBTabPanel>
-				<DBTabPanel>Content of Tab 3</DBTabPanel>
-			</DBTabs>
-		</div>
+		</Fragment>
 	);
 }
