012a441353d5607d25ea9dc35396ccc0fd0e175f
mfranzke <mfranzke@users.noreply.github.com>
2026-05-20 21:41:38 +0000

    deploy: c45af7378bbc594474436eb2fcc49e554df5de89

---
+++ review/refactor-codeowners-tidying-up-files-in-root-and-files-contents/composition-storybook/assets/formatter-EIJCOSYU-Dh4Sdz4w.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CuZz0YX3.js";import{f as t,p as n}from"./iframe-D-wNYSpH.js";import{D as r,E as i,O as a,k as o}from"./blocks-NukeoC9_.js";var s;e((()=>{a(),r(),n(),s=(0,t(o(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
