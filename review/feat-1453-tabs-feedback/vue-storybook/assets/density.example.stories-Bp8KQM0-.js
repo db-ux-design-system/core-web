1701a2f4d3e3cbf454addc2ba0ed801dcafe660f
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-05-26 10:02:04 +0000

    deploy: 25360877cd0ddda06519d7196b8bd618f30e9591

---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/density.example.stories-Bp8KQM0-.js
@@ -0,0 +1,52 @@
+import{i as e}from"./preload-helper-BvLp0nkB.js";import{d as t,l as n,t as r}from"./src-DUmYbXhX.js";var i,a,o,s,c,l;e((()=>{r(),{fn:i}=__STORYBOOK_MODULE_TEST__,a={title:`Components/DBTabItem/Density`,component:t,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{label:{control:`text`},active:{control:`boolean`},disabled:{control:`boolean`},showIcon:{control:`boolean`},showIconLeading:{control:`boolean`},showIconTrailing:{control:`boolean`},id:{control:`text`},autofocus:{control:`boolean`}}},o={args:{label:`Functional`,default:``},render:e=>({components:{DBTabItem:t,DBTabList:n},setup(){return{args:e}},template:`<DBTabList data-density="functional"   ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},s={args:{label:`(Default) Regular`,default:``},render:e=>({components:{DBTabItem:t,DBTabList:n},setup(){return{args:e}},template:`<DBTabList    ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},c={args:{label:`Expressive`,default:``},render:e=>({components:{DBTabItem:t,DBTabList:n},setup(){return{args:e}},template:`<DBTabList data-density="expressive"   ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Functional",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList data-density="functional"   ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "(Default) Regular",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList    ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Expressive",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList data-density="expressive"   ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...c.parameters?.docs?.source}}},l=[`Functional`,`DefaultRegular`,`Expressive`]}))();export{s as DefaultRegular,c as Expressive,o as Functional,l as __namedExportsOrder,a as default};
---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/react-Dvvsrjz3.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-Cb4zmoQg.js";t();export{e as MDXProvider};
