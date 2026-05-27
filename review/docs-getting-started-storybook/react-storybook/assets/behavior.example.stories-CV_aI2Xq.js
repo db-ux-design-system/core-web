413c791d0e51c2fe29b85d8da85ff43b79f9fdc4
nmerget <nmerget@users.noreply.github.com>
2026-05-27 07:52:34 +0000

    deploy: cfef70a75cd3088465737212073384345cba396c

---
+++ review/docs-getting-started-storybook/react-storybook/assets/behavior.example.stories-CV_aI2Xq.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-BwUfbr8U.js";import{t}from"./jsx-runtime-BdAEV34Z.js";import{c as n,t as r,u as i}from"./src-DecBDu97.js";var a,o,s,c,l,u;e((()=>{r(),a=t(),{fn:o}=__STORYBOOK_MODULE_TEST__,s={title:`Components/DBTabItem/Behavior`,component:i,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{label:{control:`text`},active:{control:`boolean`},disabled:{control:`boolean`},showIcon:{control:`boolean`},showIconLeading:{control:`boolean`},showIconTrailing:{control:`boolean`},noText:{control:`boolean`},checked:{control:`boolean`},name:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},c={args:{label:`(Default) Auto Width`},render:e=>(0,a.jsx)(n,{children:(0,a.jsx)(i,{...e})})},l={args:{label:`Width full`},render:e=>(0,a.jsx)(n,{style:{blockSize:`100%`},children:(0,a.jsx)(i,{...e})})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "(Default) Auto Width"
+  },
+  render: (properties: any) => <DBTabList><DBTabItem {...properties} /></DBTabList>
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Width full"
+  },
+  render: (properties: any) => <DBTabList style={{
+    blockSize: '100%'
+  }}><DBTabItem {...properties} /></DBTabList>
+}`,...l.parameters?.docs?.source}}},u=[`DefaultAutoWidth`,`Widthfull`]}))();export{c as DefaultAutoWidth,l as Widthfull,u as __namedExportsOrder,s as default};
