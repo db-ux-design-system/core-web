f4e40157e7f4e322a1ba110bf4f98f3e43da5c06
mfranzke <mfranzke@users.noreply.github.com>
2026-06-01 15:35:59 +0000

    deploy: bfa2864b7db565470a9e2ae247f3e7d74144cc52

---
+++ review/refactor-pr-template-remove-checklist-item-for-branch-update-status/vue-storybook/assets/content-alignment-full-width.example.stories-CNcKMRKa.js
@@ -0,0 +1,43 @@
+import{i as e}from"./preload-helper-Cj6vszNC.js";import{d as t,l as n,t as r}from"./src-DPBcW6iJ.js";var i,a,o,s,c;e((()=>{r(),{fn:i}=__STORYBOOK_MODULE_TEST__,a={title:`Components/DBTabItem/Content Alignment Full Width`,component:t,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{label:{control:`text`},active:{control:`boolean`},disabled:{control:`boolean`},showIcon:{control:`boolean`},showIconLeading:{control:`boolean`},showIconTrailing:{control:`boolean`},noText:{control:`boolean`},checked:{control:`boolean`},name:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},o={args:{label:`Left`,default:``},render:e=>({components:{DBTabItem:t,DBTabList:n},setup(){return{args:e}},template:`<DBTabList  :style="{
+  blockSize: '100%'
+}"  ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},s={args:{label:`Centered`,default:``},render:e=>({components:{DBTabItem:t,DBTabList:n},setup(){return{args:e}},template:`<DBTabList  :style="{
+  blockSize: '100%'
+}"  ><DBTabItem v-bind="args"   >${e.default}</DBTabItem></DBTabList>`})},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Left",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList  :style="{
+  blockSize: '100%'
+}"  ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Centered",
+    "default": \`\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabItem,
+      DBTabList
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<DBTabList  :style="{
+  blockSize: '100%'
+}"  ><DBTabItem v-bind="args"   >\${args.default}</DBTabItem></DBTabList>\`
+  })
+}`,...s.parameters?.docs?.source}}},c=[`Left`,`Centered`]}))();export{s as Centered,o as Left,c as __namedExportsOrder,a as default};
