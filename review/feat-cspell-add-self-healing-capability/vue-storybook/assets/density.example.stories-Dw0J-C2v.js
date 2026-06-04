abeb2cde227c346f0f1fa263ed37cc0b024243a3
mfranzke <mfranzke@users.noreply.github.com>
2026-06-04 17:41:20 +0000

    deploy: 43f60fdba65dcef9fc4b5fab45b4e514b0355e2f

---
+++ review/feat-cspell-add-self-healing-capability/vue-storybook/assets/density.example.stories-Dw0J-C2v.js
@@ -0,0 +1,220 @@
+import{i as e}from"./preload-helper-0Zt2Bsjw.js";import{D as t,P as n,it as r,j as i,k as a,nt as o,t as s}from"./src-C3RVDSqr.js";var c,l,u,d,f,p;e((()=>{s(),{fn:c}=__STORYBOOK_MODULE_TEST__,l={title:`Components/DBHeader/Density`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{width:{control:`select`,options:[`full`,`medium`,`large`,`small`]},forceMobile:{control:`boolean`},drawerOpen:{control:`boolean`},burgerMenuLabel:{control:`text`},closeButtonId:{control:`text`},closeButtonText:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},u={args:{"data-density":`functional`,default:`<DBNavigation aria-label="Functional" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Functional</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Functional disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>`},render:e=>({components:{DBHeader:n,DBBrand:r,DBButton:o,DBLink:i,DBNavigationItem:t,DBNavigation:a},setup(){return{args:e}},template:`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >${e.default}</DBHeader></div>`})},d={args:{"data-density":`regular`,default:`<DBNavigation aria-label="(Default) Regular" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">(Default) Regular</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">(Default) Regular disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>`},render:e=>({components:{DBHeader:n,DBBrand:r,DBButton:o,DBLink:i,DBNavigationItem:t,DBNavigation:a},setup(){return{args:e}},template:`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >${e.default}</DBHeader></div>`})},f={args:{"data-density":`expressive`,default:`<DBNavigation aria-label="Expressive" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Expressive</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Expressive disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>`},render:e=>({components:{DBHeader:n,DBBrand:r,DBButton:o,DBLink:i,DBNavigationItem:t,DBNavigation:a},setup(){return{args:e}},template:`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >${e.default}</DBHeader></div>`})},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "functional",
+    "default": \`<DBNavigation aria-label="Functional" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Functional</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Functional disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBHeader,
+      DBBrand,
+      DBButton,
+      DBLink,
+      DBNavigationItem,
+      DBNavigation
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >\${args.default}</DBHeader></div>\`
+  })
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "default": \`<DBNavigation aria-label="(Default) Regular" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">(Default) Regular</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">(Default) Regular disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBHeader,
+      DBBrand,
+      DBButton,
+      DBLink,
+      DBNavigationItem,
+      DBNavigation
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >\${args.default}</DBHeader></div>\`
+  })
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "expressive",
+    "default": \`<DBNavigation aria-label="Expressive" v-bind="{}"
+  ><DBNavigationItem icon="x_placeholder"
+    ><a href="#">Expressive</a></DBNavigationItem
+  ><DBNavigationItem :disabled="true"
+    ><a href="#">Expressive disabled</a></DBNavigationItem
+  ></DBNavigation
+><template v-slot:brand><DBBrand>DBHeader</DBBrand></template
+><template v-slot:meta-navigation
+  ><DBLink href="#">Imprint</DBLink> <DBLink href="#">Help</DBLink></template
+><template v-slot:primary-action
+  ><DBButton icon="magnifying_glass" variant="ghost" :noText="true">
+    Search
+  </DBButton></template
+><template v-slot:secondary-action
+  ><DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Profile
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Notification
+  </DBButton>
+  <DBButton icon="x_placeholder" variant="ghost" :noText="true">
+    Help
+  </DBButton></template
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBHeader,
+      DBBrand,
+      DBButton,
+      DBLink,
+      DBNavigationItem,
+      DBNavigation
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100%',
+  display: 'block'
+}"  ><DBHeader v-bind="args"   >\${args.default}</DBHeader></div>\`
+  })
+}`,...f.parameters?.docs?.source}}},p=[`Functional`,`DefaultRegular`,`Expressive`]}))();export{d as DefaultRegular,f as Expressive,u as Functional,p as __namedExportsOrder,l as default};
