655b4b1cdca41db1e7fa560e5cc39c2e5f717ee3
nmerget <nmerget@users.noreply.github.com>
2026-06-05 12:26:15 +0000

    deploy: da2f85596488012725585ba6e2361c49043b8808

---
+++ review/fix-get-boolean-as-string-property-name/vue-storybook/assets/formatter-EIJCOSYU-BZGmYc_2.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DlY9uolC.js";import{L as t,R as n}from"./iframe-CCmg5g2a.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-CPf_Kd6J.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
