6f3aa5d63bdd4380a689d9c71ed4eaea256a4695
nmerget <nmerget@users.noreply.github.com>
2026-06-08 19:06:55 +0000

    deploy: 7c1bbf006a55d8638718d867f6d3b92251391412

---
+++ review/fix-preserve-whitespaces-infotext/html-storybook/assets/formatter-EIJCOSYU-3JcAkNDe.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-D4oLJEXY.js";import{h as t,m as n}from"./iframe-B8cAjuTQ.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-CznQkKzl.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
