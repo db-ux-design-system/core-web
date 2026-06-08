ce396a0a65cc1b9af0d424f95e48713218a46ec9
michaelmkraus <michael.m.kraus@deutschebahn.com>
2026-06-08 16:12:29 +0200

    chore: add comment to revisit tab-item screenshot tolerance

--- showcases/e2e/tab-item/tab-item-visual-snapshot.spec.ts
+++ showcases/e2e/tab-item/tab-item-visual-snapshot.spec.ts
@@ -7,6 +7,7 @@ test.describe('DBTabItem', () => {
 		path,
 		// Increased tolerance due to subpixel rendering differences of icon fonts
 		// and the bold-weight reservation pseudo-element across browsers/platforms.
+		// Revisit whether this can be tightened to default once rendering stabilizes.
 		ratio: '0.02'
 	});
 });
