1c3bb478c4e3de5e2721c551dd5ed2fe7f512665
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-06-08 09:10:21 +0200

    fix: add icon and showIcon again to avoid breaking changes

--- packages/components/src/components/brand/model.ts
+++ packages/components/src/components/brand/model.ts
@@ -1,3 +1,4 @@
+import { IconTypes } from '@db-ux/core-foundations';
 import type {
 	GlobalProps,
 	GlobalState,
@@ -10,6 +11,15 @@ export type DBBrandDefaultProps = {
 	 * @deprecated: Disable the default logo svg to pass in a custom `img`
 	 */
 	hideLogo?: boolean;
+	/**
+	 * @deprecated: Define an icon by its identifier (like e.g. _user_, compare to [Icons](https://design-system.deutschebahn.com/core-web/review/main/foundations/icons/overview)) to get displayed in front of the elements content.
+	 */
+	icon?: IconTypes;
+	/**
+	 * @deprecated: Enables or disables the visibility of the icon. The default value depends on the component.
+	 * For many components this property is optional to reflect Figma properties.
+	 */
+	showIcon?: boolean | string;
 };
 
 export type DBBrandProps = DBBrandDefaultProps &
