52a98d74e0cdfeb5d5e084221cd736f675148890
mfranzke <mfranzke@users.noreply.github.com>
2026-06-08 09:08:29 +0000

    deploy: 5ca810edbcae23468de6b489baab49b0cba13ec1

---
+++ review/test-guidepup-enable-Mac-again/vue-storybook/assets/formatter-EIJCOSYU-kIg_x9G8.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DoiZB550.js";import{L as t,R as n}from"./iframe-CVl_XCK4.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-DnPO7WFw.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
