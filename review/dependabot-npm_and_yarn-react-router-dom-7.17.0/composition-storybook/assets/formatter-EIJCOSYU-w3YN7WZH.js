2e59be7d5074217a7aa8732a6eacf93e7ce08a8f
nmerget <nmerget@users.noreply.github.com>
2026-06-09 13:56:19 +0000

    deploy: 19828a7b961b0dd9fdd4d18dcae442a861ac6adb

---
+++ review/dependabot-npm_and_yarn-react-router-dom-7.17.0/composition-storybook/assets/formatter-EIJCOSYU-w3YN7WZH.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-D7lCCUFV.js";import{m as t,p as n}from"./iframe-DESOz65O.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-DVFtfnV8.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
