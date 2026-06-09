482b473090fe4498fb7d7c6738e0e87276f0e44e
nmerget <nmerget@users.noreply.github.com>
2026-06-09 02:55:38 +0000

    deploy: ba2c1b5288aaab24545e756c1797de5963bab4d3

--- review/fix-icon-inline-size/vue-storybook/assets/formatter-EIJCOSYU-CGikmemD.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-fejm8zH8.js";import{L as t,R as n}from"./iframe-DN5wOaVI.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-DP0AU2fi.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
