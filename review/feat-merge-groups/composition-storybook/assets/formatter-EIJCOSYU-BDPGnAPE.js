f53c8cf6f88d957c93b4cba4cf53d5c96557adcb
nmerget <nmerget@users.noreply.github.com>
2026-06-10 07:28:36 +0000

    deploy: f301dd37f07c3259013d1cf7899a5d976276d4c3

---
+++ review/feat-merge-groups/composition-storybook/assets/formatter-EIJCOSYU-BDPGnAPE.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DK0BdC5B.js";import{m as t,p as n}from"./iframe-C4K3w0bc.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-3S3sn3eI.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
---
+++ review/feat-merge-groups/react-storybook/assets/density.example.stories-2xxm_f2O.js
@@ -0,0 +1,19 @@
+import{i as e}from"./preload-helper-CpC7pzhH.js";import{t}from"./jsx-runtime-De9rL7_x.js";import{t as n,vt as r}from"./src-Bg26NJfV.js";var i,a,o,s,c,l,u;e((()=>{n(),i=t(),{fn:a}=__STORYBOOK_MODULE_TEST__,o={title:`Components/DBBrand/Density`,component:r,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{hideLogo:{control:`boolean`},showIcon:{control:`boolean`},text:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},s={args:{"data-density":`functional`,children:`Functional`},render:e=>(0,i.jsx)(r,{...e})},c={args:{"data-density":`regular`,children:`(Default) Regular`},render:e=>(0,i.jsx)(r,{...e})},l={args:{"data-density":`expressive`,children:`Expressive`},render:e=>(0,i.jsx)(r,{...e})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "functional",
+    "children": "Functional"
+  },
+  render: (properties: any) => <DBBrand {...properties} />
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "regular",
+    "children": "(Default) Regular"
+  },
+  render: (properties: any) => <DBBrand {...properties} />
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "data-density": "expressive",
+    "children": "Expressive"
+  },
+  render: (properties: any) => <DBBrand {...properties} />
+}`,...l.parameters?.docs?.source}}},u=[`Functional`,`DefaultRegular`,`Expressive`]}))();export{c as DefaultRegular,l as Expressive,s as Functional,u as __namedExportsOrder,o as default};
