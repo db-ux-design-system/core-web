e0d38e79caa6eb54b7e3d55d3e20bf67ecf1f8a3
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-06-17 11:50:53 +0000

    deploy: 4322631c21cd3ccff402b17085ceb1f2fe4a16cf

---
+++ review/feat-1453-tabs-feedback/html-storybook/assets/divider.example.stories-eTpPHbjJ.js
@@ -0,0 +1,61 @@
+import{i as e}from"./preload-helper-D6-jCGni.js";import{t}from"./iframe--bKScIIP.js";import{ft as n,o as r,t as i}from"./src-CRRFKYZ6.js";import{a,t as o}from"./data-BVLEe0Wd.js";var s,c,l,u,d,f,p,m;e((()=>{i(),a(),s=t(),{fn:c}=__STORYBOOK_MODULE_TEST__,l={title:`Components/DBTable/Divider`,component:r,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{id:{control:`text`},caption:{control:`text`},captionPlain:{control:`text`},data:{control:`object`},divider:{control:`select`,options:[`none`,`both`,`horizontal`,`vertical`]},showCaption:{control:`boolean`},size:{control:`select`,options:[`x-small`,`small`,`medium`,`large`]},variant:{control:`select`,options:[`flat`,`zebra`,`spaced`]},mobileVariant:{control:`select`,options:[`table`,`list`]},stickyHeader:{control:`select`,options:[`none`,`both`,`horizontal`,`vertical`]},columnSizes:{control:`object`}}},u={args:{divider:`none`,captionPlain:`None`,data:o},render:e=>(0,s.jsxs)(`div`,{style:{minInlineSize:`300px`,display:`flex`,flexDirection:`column`,alignItems:`center`,gap:`var(--db-spacing-fixed-md)`},children:[(0,s.jsx)(n,{semantic:`informational`,size:`small`,icon:`none`,children:`None`}),(0,s.jsx)(r,{...e})]})},d={args:{divider:`both`,captionPlain:`Both`,data:o},render:e=>(0,s.jsxs)(`div`,{style:{minInlineSize:`300px`,display:`flex`,flexDirection:`column`,alignItems:`center`,gap:`var(--db-spacing-fixed-md)`},children:[(0,s.jsx)(n,{semantic:`informational`,size:`small`,icon:`none`,children:`Both`}),(0,s.jsx)(r,{...e})]})},f={args:{divider:`horizontal`,captionPlain:`(Default) Horizontal`,data:o},render:e=>(0,s.jsxs)(`div`,{style:{minInlineSize:`300px`,display:`flex`,flexDirection:`column`,alignItems:`center`,gap:`var(--db-spacing-fixed-md)`},children:[(0,s.jsx)(n,{semantic:`informational`,size:`small`,icon:`none`,children:`(Default) Horizontal`}),(0,s.jsx)(r,{...e})]})},p={args:{divider:`vertical`,captionPlain:`Vertical`,data:o},render:e=>(0,s.jsxs)(`div`,{style:{minInlineSize:`300px`,display:`flex`,flexDirection:`column`,alignItems:`center`,gap:`var(--db-spacing-fixed-md)`},children:[(0,s.jsx)(n,{semantic:`informational`,size:`small`,icon:`none`,children:`Vertical`}),(0,s.jsx)(r,{...e})]})},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "divider": "none",
+    "captionPlain": "None",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    None
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    "divider": "both",
+    "captionPlain": "Both",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    Both
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    "divider": "horizontal",
+    "captionPlain": "(Default) Horizontal",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    (Default) Horizontal
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    "divider": "vertical",
+    "captionPlain": "Vertical",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    minInlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 'var(--db-spacing-fixed-md)'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    Vertical
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...p.parameters?.docs?.source}}},m=[`None`,`Both`,`DefaultHorizontal`,`Vertical`]}))();export{d as Both,f as DefaultHorizontal,u as None,p as Vertical,m as __namedExportsOrder,l as default};
--- review/feat-1453-tabs-feedback/html-storybook/assets/formatter-EIJCOSYU-CNfm4nui.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-D6-jCGni.js";import{h as t,m as n}from"./iframe-aTAAhHE9.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-CvJa99ZI.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
