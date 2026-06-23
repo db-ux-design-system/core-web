bc5a815bd7277102dafecacfaf33e95e9f7d6170
nmerget <nmerget@users.noreply.github.com>
2026-06-23 09:16:04 +0000

    deploy: 2f4d8981aff5c2b56fbccf3c6194d593c5bdc717

---
+++ review/dependabot-npm_and_yarn-faker-js-faker-10.5.0/html-storybook/assets/formatter-L2YTDE3Q-D-Kz8aCz.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-C7WVXv2g.js";import{h as t,m as n}from"./iframe-CSCb_1hs.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-56PSpz2Z.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
