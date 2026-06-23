1985b60140e8473007184f8a8ae6c6b7b3f23fc0
nmerget <nmerget@users.noreply.github.com>
2026-06-23 07:53:41 +0000

    deploy: eab5e6be3f247833ac8a546652ae22a9888f0baf

---
+++ review/dependabot-npm_and_yarn-markdownlint-cli-0.49.0/composition-storybook/assets/formatter-L2YTDE3Q-BskkiqAk.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-orlquoeV.js";import{m as t,p as n}from"./iframe-CG0ANlit.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-C0V7h3du.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
