718103942fadef5389257ddea8ce497164403c50
mfranzke <mfranzke@users.noreply.github.com>
2026-07-01 17:14:09 +0000

    deploy: fe63b5527b63b7927b25008645c73f9494fdf0c7

---
+++ version/v4.13.1-angular-signal-forms5/react-storybook/assets/formatter-L2YTDE3Q-BFuwOKNu.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CO1WHk-U.js";import{d as t,f as n}from"./iframe-K2YRTsTr.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-xhya71rQ.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
