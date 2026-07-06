1750ad4d4fc80c2c1be7a89e6f0594299d4ae934
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-07-06 08:25:45 +0200

    fix: issues with failing tests

--- packages/components/src/components/control-panel-mobile/showcase/control-panel-mobile.showcase.lite.tsx
+++ packages/components/src/components/control-panel-mobile/showcase/control-panel-mobile.showcase.lite.tsx
@@ -2,7 +2,6 @@ import { PatternhubProps } from '../../../shared/model';
 import CardWrapperShowcase from '../../../shared/showcase/card-wrapper.showcase.lite';
 import ContainerWrapperShowcase from '../../../shared/showcase/container-wrapper.showcase.lite';
 import LinkWrapperShowcase from '../../../shared/showcase/link-wrapper.showcase.lite';
-import ControlPanelMobileDensity from '../examples/density.example.lite';
 import ControlPanelMobileExamplesBottom from '../examples/examples-bottom.example.lite';
 import ControlPanelMobileExamples from '../examples/examples.example.lite';
 import ControlPanelMobilePosition from '../examples/position.example.lite';
@@ -12,11 +11,6 @@ export default function ControlPanelMobileShowcase(props: PatternhubProps) {
 		<ContainerWrapperShowcase
 			title="DBControlPanelMobile"
 			isPatternhub={props.isPatternhub}>
-			<LinkWrapperShowcase exampleName="Density">
-				<CardWrapperShowcase>
-					<ControlPanelMobileDensity />
-				</CardWrapperShowcase>
-			</LinkWrapperShowcase>
 			<LinkWrapperShowcase exampleName="Position">
 				<CardWrapperShowcase>
 					<ControlPanelMobilePosition />
