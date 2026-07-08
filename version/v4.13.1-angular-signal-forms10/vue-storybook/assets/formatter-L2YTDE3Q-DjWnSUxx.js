5a66776084cfc83450614228fe90abccd3449bbd
mfranzke <mfranzke@users.noreply.github.com>
2026-07-08 14:26:54 +0000

    deploy: 4a8c8e18030d8e44467b7944a40e8c11b48795b8

---
+++ version/v4.13.1-angular-signal-forms10/vue-storybook/assets/formatter-L2YTDE3Q-DjWnSUxx.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-BeoTemxV.js";import{U as t,W as n}from"./iframe-BVmZN0EH.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-Buc8hdVT.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
