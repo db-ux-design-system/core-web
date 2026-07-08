96322659b220161c1f1885b1ef66ec0d5fc3ba96
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-07-08 11:09:04 +0000

    deploy: 4c925376a3ca17f2e6160a80526d9e7aa574bc13

---
+++ review/fix-accordion-variant-background-6879/react-storybook/assets/width.example.stories-BTFy4J59.js
@@ -0,0 +1,27 @@
+import{i as e}from"./preload-helper-BG3cxSFj.js";import{t}from"./jsx-runtime-DQ7lR6b4.js";import{ft as n,o as r,t as i}from"./src-CrsPTufx.js";import{a,t as o}from"./data-DteGQj6o.js";var s,c,l,u,d,f;e((()=>{i(),a(),s=t(),{fn:c}=__STORYBOOK_MODULE_TEST__,l={title:`Components/DBTable/Width`,component:r,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{id:{control:`text`},caption:{control:`text`},captionPlain:{control:`text`},data:{control:`object`},divider:{control:`select`,options:[`none`,`both`,`horizontal`,`vertical`]},showCaption:{control:`boolean`},size:{control:`select`,options:[`x-small`,`small`,`medium`,`large`]},variant:{control:`select`,options:[`flat`,`zebra`,`spaced`]},mobileVariant:{control:`select`,options:[`table`,`list`]},stickyHeader:{control:`select`,options:[`none`,`both`,`horizontal`,`vertical`]},columnSizes:{control:`object`}}},u={args:{width:`full`,captionPlain:`(Default) Full`,data:o},render:e=>(0,s.jsxs)(`div`,{style:{inlineSize:`300px`,display:`flex`,flexDirection:`column`},children:[(0,s.jsx)(n,{semantic:`informational`,size:`small`,icon:`none`,children:`(Default) Full`}),(0,s.jsx)(r,{...e})]})},d={args:{width:`auto`,captionPlain:`Auto`,data:o},render:e=>(0,s.jsxs)(`div`,{style:{inlineSize:`300px`,display:`flex`,flexDirection:`column`},children:[(0,s.jsx)(n,{semantic:`informational`,size:`small`,icon:`none`,children:`Auto`}),(0,s.jsx)(r,{...e})]})},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "width": "full",
+    "captionPlain": "(Default) Full",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    inlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    (Default) Full
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    "width": "auto",
+    "captionPlain": "Auto",
+    "data": defaultTable
+  },
+  render: (properties: any) => <div style={{
+    inlineSize: '300px',
+    display: 'flex',
+    flexDirection: 'column'
+  }}><DBInfotext semantic="informational" size="small" icon="none">
+                    Auto
+                </DBInfotext><DBTable {...properties} /></div>
+}`,...d.parameters?.docs?.source}}},f=[`DefaultFull`,`Auto`]}))();export{d as Auto,u as DefaultFull,f as __namedExportsOrder,l as default};
