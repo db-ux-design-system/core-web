d079a0b3a610b28f7631a1bb59566a93f5dc20c4
michaelmkraus <michaelmkraus@users.noreply.github.com>
2026-07-09 10:29:38 +0000

    deploy: c6f91db1435646636d1a9fe8d11ad30659113206

---
+++ review/fix-accordion-variant-background-6879/vue-storybook/assets/formatter-L2YTDE3Q-eTttTtBf.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-jFZ1aNo8.js";import{U as t,W as n}from"./iframe-Dodg9YnD.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-BGFiDQ81.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
