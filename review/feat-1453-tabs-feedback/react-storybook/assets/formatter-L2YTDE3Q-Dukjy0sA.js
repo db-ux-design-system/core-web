21528e034b6f53f029455ee3af3aa22b55853c44
mfranzke <mfranzke@users.noreply.github.com>
2026-07-10 17:33:35 +0000

    deploy: 5a0bb6e7da86aed0e0a22e03ee9401bd99722443

---
+++ review/feat-1453-tabs-feedback/react-storybook/assets/formatter-L2YTDE3Q-Dukjy0sA.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DwLSiSdQ.js";import{m as t,p as n}from"./iframe-Dek0PDK4.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-z8FJ8yZU.js";var s;e((()=>{a(),i(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
