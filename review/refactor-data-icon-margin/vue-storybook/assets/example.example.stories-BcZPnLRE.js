5dc02ce1349e7e025fb06d9610c7918f1bf2bdc9
nmerget <nmerget@users.noreply.github.com>
2026-07-14 06:59:56 +0000

    deploy: d9f9046f0e21bd8a92ee6d49d3004f2a6c4aa59f

---
+++ review/refactor-data-icon-margin/vue-storybook/assets/example.example.stories-BcZPnLRE.js
@@ -0,0 +1,22 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t,ut as n}from"./src-78vey0-7.js";var r,i,a,o;e((()=>{t(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/DBCheckbox/Example`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{name:{control:`text`},disabled:{control:`boolean`},checked:{control:`boolean`},indeterminate:{control:`boolean`},validation:{control:`select`,options:[`invalid`,`valid`,`no-validation`]},invalidMessage:{control:`text`},validMessage:{control:`text`},message:{control:`text`},showMessage:{control:`boolean`},size:{control:`select`,options:[`small`,`medium`]},required:{control:`boolean`},showLabel:{control:`boolean`},showRequiredAsterisk:{control:`boolean`},label:{control:`text`},placeholder:{control:`text`},variant:{control:`select`,options:[`above`,`floating`]},value:{control:`text`},form:{control:`text`},ariaDescribedBy:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},a={args:{name:`Example`,checked:!0,default:`Long label`},render:e=>({components:{DBCheckbox:n},setup(){return{args:e}},template:`<div  :style="{
+  width: '100px'
+}"  ><DBCheckbox v-bind="args"   >${e.default}</DBCheckbox></div>`})},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    "name": "Example",
+    "checked": true,
+    "default": \`Long label\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBCheckbox
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  width: '100px'
+}"  ><DBCheckbox v-bind="args"   >\${args.default}</DBCheckbox></div>\`
+  })
+}`,...a.parameters?.docs?.source}}},o=[`Longlabel`]}))();export{a as Longlabel,o as __namedExportsOrder,i as default};
