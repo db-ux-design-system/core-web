946ab35e60f3ad3164abeed0b1abb2e72dac2b6f
nmerget <nmerget@users.noreply.github.com>
2026-07-14 14:03:30 +0000

    deploy: 4cd5f80e1c153fcbb10876afcb08a85d71449141

---
+++ review/feat-drawer/vue-storybook/assets/vertical-alignment.example.stories-DA73vRfi.js
@@ -0,0 +1,217 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{_ as t,bt as n,gt as r,h as i,l as a,p as o,s,t as c,y as l}from"./src-v1eyMR6s.js";var u,d,f,p,m,h;e((()=>{c(),{fn:u}=__STORYBOOK_MODULE_TEST__,d={title:`Components/DBTable/Vertical Alignment`,component:s,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{id:{control:`text`},caption:{control:`text`},captionPlain:{control:`text`},data:{control:`object`},divider:{control:`select`,options:[`none`,`both`,`horizontal`,`vertical`]},showCaption:{control:`boolean`},size:{control:`select`,options:[`x-small`,`small`,`medium`,`large`]},variant:{control:`select`,options:[`flat`,`zebra`,`spaced`]},mobileVariant:{control:`select`,options:[`table`,`list`]},stickyHeader:{control:`select`,options:[`none`,`both`,`horizontal`,`vertical`]},columnSizes:{control:`object`}}},f={args:{captionPlain:`Start`,divider:`both`,default:`<DBTableHead
+  ><DBTableRow
+    ><DBTableHeaderCell scope="col" verticalAlignment="start">
+      Header A </DBTableHeaderCell
+    ><DBTableHeaderCell scope="col" verticalAlignment="start">
+      Header B
+    </DBTableHeaderCell></DBTableRow
+  ></DBTableHead
+><DBTableBody
+  ><DBTableRow
+    ><DBTableHeaderCell scope="row" verticalAlignment="start">
+      Comp 1 </DBTableHeaderCell
+    ><DBTableDataCell verticalAlignment="start"
+      ><DBButton>Click</DBButton></DBTableDataCell
+    ></DBTableRow
+  ></DBTableBody
+>`},render:e=>({components:{DBTable:s,DBButton:n,DBInfotext:r,DBTableBody:o,DBTableDataCell:l,DBTableHead:a,DBTableHeaderCell:t,DBTableRow:i},setup(){return{args:e}},template:`<div  :style="{
+  minInlineSize: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--db-spacing-fixed-md)'
+}"  ><DBInfotext semantic="informational" size="small" icon="none"   >
+                    Start
+                </DBInfotext><DBTable v-bind="args"   >${e.default}</DBTable></div>`})},p={args:{captionPlain:`(Default) Center`,divider:`both`,default:`<DBTableHead
+  ><DBTableRow
+    ><DBTableHeaderCell scope="col" verticalAlignment="center">
+      Header A </DBTableHeaderCell
+    ><DBTableHeaderCell scope="col" verticalAlignment="center">
+      Header B
+    </DBTableHeaderCell></DBTableRow
+  ></DBTableHead
+><DBTableBody
+  ><DBTableRow
+    ><DBTableHeaderCell scope="row" verticalAlignment="center">
+      Comp 1 </DBTableHeaderCell
+    ><DBTableDataCell verticalAlignment="center"
+      ><DBButton>Click</DBButton></DBTableDataCell
+    ></DBTableRow
+  ></DBTableBody
+>`},render:e=>({components:{DBTable:s,DBButton:n,DBInfotext:r,DBTableBody:o,DBTableDataCell:l,DBTableHead:a,DBTableHeaderCell:t,DBTableRow:i},setup(){return{args:e}},template:`<div  :style="{
+  minInlineSize: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--db-spacing-fixed-md)'
+}"  ><DBInfotext semantic="informational" size="small" icon="none"   >
+                    (Default) Center
+                </DBInfotext><DBTable v-bind="args"   >${e.default}</DBTable></div>`})},m={args:{captionPlain:`End`,divider:`both`,default:`<DBTableHead
+  ><DBTableRow
+    ><DBTableHeaderCell scope="col" verticalAlignment="end">
+      Header A </DBTableHeaderCell
+    ><DBTableHeaderCell scope="col" verticalAlignment="end">
+      Header B
+    </DBTableHeaderCell></DBTableRow
+  ></DBTableHead
+><DBTableBody
+  ><DBTableRow
+    ><DBTableHeaderCell scope="row" verticalAlignment="end">
+      Comp 1 </DBTableHeaderCell
+    ><DBTableDataCell verticalAlignment="end"
+      ><DBButton>Click</DBButton></DBTableDataCell
+    ></DBTableRow
+  ></DBTableBody
+>`},render:e=>({components:{DBTable:s,DBButton:n,DBInfotext:r,DBTableBody:o,DBTableDataCell:l,DBTableHead:a,DBTableHeaderCell:t,DBTableRow:i},setup(){return{args:e}},template:`<div  :style="{
+  minInlineSize: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--db-spacing-fixed-md)'
+}"  ><DBInfotext semantic="informational" size="small" icon="none"   >
+                    End
+                </DBInfotext><DBTable v-bind="args"   >${e.default}</DBTable></div>`})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "Start",
+    "divider": "both",
+    "default": \`<DBTableHead
+  ><DBTableRow
+    ><DBTableHeaderCell scope="col" verticalAlignment="start">
+      Header A </DBTableHeaderCell
+    ><DBTableHeaderCell scope="col" verticalAlignment="start">
+      Header B
+    </DBTableHeaderCell></DBTableRow
+  ></DBTableHead
+><DBTableBody
+  ><DBTableRow
+    ><DBTableHeaderCell scope="row" verticalAlignment="start">
+      Comp 1 </DBTableHeaderCell
+    ><DBTableDataCell verticalAlignment="start"
+      ><DBButton>Click</DBButton></DBTableDataCell
+    ></DBTableRow
+  ></DBTableBody
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTable,
+      DBButton,
+      DBInfotext,
+      DBTableBody,
+      DBTableDataCell,
+      DBTableHead,
+      DBTableHeaderCell,
+      DBTableRow
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  minInlineSize: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--db-spacing-fixed-md)'
+}"  ><DBInfotext semantic="informational" size="small" icon="none"   >
+                    Start
+                </DBInfotext><DBTable v-bind="args"   >\${args.default}</DBTable></div>\`
+  })
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "(Default) Center",
+    "divider": "both",
+    "default": \`<DBTableHead
+  ><DBTableRow
+    ><DBTableHeaderCell scope="col" verticalAlignment="center">
+      Header A </DBTableHeaderCell
+    ><DBTableHeaderCell scope="col" verticalAlignment="center">
+      Header B
+    </DBTableHeaderCell></DBTableRow
+  ></DBTableHead
+><DBTableBody
+  ><DBTableRow
+    ><DBTableHeaderCell scope="row" verticalAlignment="center">
+      Comp 1 </DBTableHeaderCell
+    ><DBTableDataCell verticalAlignment="center"
+      ><DBButton>Click</DBButton></DBTableDataCell
+    ></DBTableRow
+  ></DBTableBody
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTable,
+      DBButton,
+      DBInfotext,
+      DBTableBody,
+      DBTableDataCell,
+      DBTableHead,
+      DBTableHeaderCell,
+      DBTableRow
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  minInlineSize: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--db-spacing-fixed-md)'
+}"  ><DBInfotext semantic="informational" size="small" icon="none"   >
+                    (Default) Center
+                </DBInfotext><DBTable v-bind="args"   >\${args.default}</DBTable></div>\`
+  })
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    "captionPlain": "End",
+    "divider": "both",
+    "default": \`<DBTableHead
+  ><DBTableRow
+    ><DBTableHeaderCell scope="col" verticalAlignment="end">
+      Header A </DBTableHeaderCell
+    ><DBTableHeaderCell scope="col" verticalAlignment="end">
+      Header B
+    </DBTableHeaderCell></DBTableRow
+  ></DBTableHead
+><DBTableBody
+  ><DBTableRow
+    ><DBTableHeaderCell scope="row" verticalAlignment="end">
+      Comp 1 </DBTableHeaderCell
+    ><DBTableDataCell verticalAlignment="end"
+      ><DBButton>Click</DBButton></DBTableDataCell
+    ></DBTableRow
+  ></DBTableBody
+>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTable,
+      DBButton,
+      DBInfotext,
+      DBTableBody,
+      DBTableDataCell,
+      DBTableHead,
+      DBTableHeaderCell,
+      DBTableRow
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  minInlineSize: '300px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: 'var(--db-spacing-fixed-md)'
+}"  ><DBInfotext semantic="informational" size="small" icon="none"   >
+                    End
+                </DBInfotext><DBTable v-bind="args"   >\${args.default}</DBTable></div>\`
+  })
+}`,...m.parameters?.docs?.source}}},h=[`Start`,`DefaultCenter`,`End`]}))();export{p as DefaultCenter,m as End,f as Start,h as __namedExportsOrder,d as default};
