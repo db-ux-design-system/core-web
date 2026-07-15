98021dc7e5fa99e6aa5ac6df7bf746eeeacb0420
mfranzke <mfranzke@users.noreply.github.com>
2026-07-15 06:37:00 +0000

    deploy: a729a1764d560c7bf7cd110479c0364bdcf37161

---
+++ review/feat-1453-tabs-feedback/vue-storybook/assets/icons.example.stories-Cnpvokn-.js
@@ -0,0 +1,107 @@
+import{i as e}from"./preload-helper-S1gXuCIc.js";import{C as t,T as n,a as r,ft as i,t as a,x as o}from"./src-CDgVQ_UP.js";var s,c,l,u,d,f;e((()=>{a(),{fn:s}=__STORYBOOK_MODULE_TEST__,c={title:`Components/DBTabs/Icons`,component:r,parameters:{layout:`centered`},tags:[`autodocs`],args:{onIndexChange:s(),onTabSelect:s()},argTypes:{orientation:{control:`select`,options:[`horizontal`,`vertical`]},tabItemWidth:{control:`select`,options:[`full`,`auto`]},tabItemAlignment:{control:`select`,options:[`start`,`center`,`end`]},behavior:{control:`select`,options:[`scrollbar`,`arrows`]},initialSelectedIndex:{control:`number`},initialSelectedMode:{control:`select`,options:[`auto`,`manually`]},label:{control:`text`},tabs:{control:`object`},arrowScrollDistance:{control:`number`},id:{control:`text`},autofocus:{control:`boolean`},onIndexChange:{action:`onIndexChange`},onTabSelect:{action:`onTabSelect`}}},l={args:{default:`<DBTabList
+  ><DBTabItem icon="house" :showIcon="true"> Home </DBTabItem
+  ><DBTabItem icon="magnifying_glass" :showIcon="true"> Search </DBTabItem
+  ><DBTabItem icon="calendar" :showIcon="true"> Calendar </DBTabItem></DBTabList
+><DBTabPanel>Home content</DBTabPanel><DBTabPanel>Search content</DBTabPanel
+><DBTabPanel>Calendar content</DBTabPanel>`},render:e=>({components:{DBTabs:r,DBInfotext:i,DBTabItem:n,DBTabList:t,DBTabPanel:o},setup(){return{args:e}},template:`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    with leading icons:
+                </DBInfotext><DBTabs v-bind="args"   >${e.default}</DBTabs></div>`})},u={args:{default:`<DBTabList
+  ><DBTabItem iconTrailing="exclamation_mark_circle" :showIconTrailing="true">
+    Notifications </DBTabItem
+  ><DBTabItem iconTrailing="information_circle" :showIconTrailing="true">
+    Info </DBTabItem
+  ><DBTabItem iconTrailing="circular_arrows" :showIconTrailing="true">
+    Settings
+  </DBTabItem></DBTabList
+><DBTabPanel>Notifications content</DBTabPanel
+><DBTabPanel>Info content</DBTabPanel><DBTabPanel>Settings content</DBTabPanel>`},render:e=>({components:{DBTabs:r,DBInfotext:i,DBTabItem:n,DBTabList:t,DBTabPanel:o},setup(){return{args:e}},template:`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    with trailing icons:
+                </DBInfotext><DBTabs v-bind="args"   >${e.default}</DBTabs></div>`})},d={args:{initialSelectedMode:`manually`,default:`<DBTabList
+  ><DBTabItem icon="house" aria-label="Test 1"></DBTabItem
+  ><DBTabItem>Test 2</DBTabItem><DBTabItem>Test 3</DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>`},render:e=>({components:{DBTabs:r,DBInfotext:i,DBTabItem:n,DBTabList:t,DBTabPanel:o},setup(){return{args:e}},template:`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    icon only:
+                </DBInfotext><DBTabs v-bind="args"   >${e.default}</DBTabs></div>`})},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "default": \`<DBTabList
+  ><DBTabItem icon="house" :showIcon="true"> Home </DBTabItem
+  ><DBTabItem icon="magnifying_glass" :showIcon="true"> Search </DBTabItem
+  ><DBTabItem icon="calendar" :showIcon="true"> Calendar </DBTabItem></DBTabList
+><DBTabPanel>Home content</DBTabPanel><DBTabPanel>Search content</DBTabPanel
+><DBTabPanel>Calendar content</DBTabPanel>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabs,
+      DBInfotext,
+      DBTabItem,
+      DBTabList,
+      DBTabPanel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    with leading icons:
+                </DBInfotext><DBTabs v-bind="args"   >\${args.default}</DBTabs></div>\`
+  })
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "default": \`<DBTabList
+  ><DBTabItem iconTrailing="exclamation_mark_circle" :showIconTrailing="true">
+    Notifications </DBTabItem
+  ><DBTabItem iconTrailing="information_circle" :showIconTrailing="true">
+    Info </DBTabItem
+  ><DBTabItem iconTrailing="circular_arrows" :showIconTrailing="true">
+    Settings
+  </DBTabItem></DBTabList
+><DBTabPanel>Notifications content</DBTabPanel
+><DBTabPanel>Info content</DBTabPanel><DBTabPanel>Settings content</DBTabPanel>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabs,
+      DBInfotext,
+      DBTabItem,
+      DBTabList,
+      DBTabPanel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    with trailing icons:
+                </DBInfotext><DBTabs v-bind="args"   >\${args.default}</DBTabs></div>\`
+  })
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    "initialSelectedMode": "manually",
+    "default": \`<DBTabList
+  ><DBTabItem icon="house" aria-label="Test 1"></DBTabItem
+  ><DBTabItem>Test 2</DBTabItem><DBTabItem>Test 3</DBTabItem></DBTabList
+><DBTabPanel>Tab Panel 1</DBTabPanel><DBTabPanel>Tab Panel 2</DBTabPanel
+><DBTabPanel>Tab Panel 3</DBTabPanel>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBTabs,
+      DBInfotext,
+      DBTabItem,
+      DBTabList,
+      DBTabPanel
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div class="fit-content-container"   ><DBInfotext icon="none" size="small" semantic="informational"   >
+                    icon only:
+                </DBInfotext><DBTabs v-bind="args"   >\${args.default}</DBTabs></div>\`
+  })
+}`,...d.parameters?.docs?.source}}},f=[`withleadingicons`,`withtrailingicons`,`TabsIcons2`]}))();export{d as TabsIcons2,f as __namedExportsOrder,c as default,l as withleadingicons,u as withtrailingicons};
