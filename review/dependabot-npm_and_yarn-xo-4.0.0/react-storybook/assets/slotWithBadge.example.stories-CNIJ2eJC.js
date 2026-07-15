2a07d2c6d8e746c0ee277b60ffc647d7940eaf4a
mfranzke <mfranzke@users.noreply.github.com>
2026-07-15 21:12:28 +0000

    deploy: a08ab47c56f19972c4b37bdc2b6e8a367310c74c

---
+++ review/dependabot-npm_and_yarn-xo-4.0.0/react-storybook/assets/slotWithBadge.example.stories-CNIJ2eJC.js
@@ -0,0 +1,24 @@
+import{i as e}from"./preload-helper-CeXJGNC1.js";import{t}from"./jsx-runtime-CVu96F3I.js";import{S as n,b as r,bt as i,ft as a,i as o,t as s,w as c}from"./src-CsUEkiNw.js";var l,u,d,f,p,m;e((()=>{s(),l=t(),{fn:u}=__STORYBOOK_MODULE_TEST__,d={title:`Components/DBTabs/Slot with Badge`,component:o,parameters:{layout:`centered`},tags:[`autodocs`],args:{onIndexChange:u(),onTabSelect:u()},argTypes:{orientation:{control:`select`,options:[`horizontal`,`vertical`]},tabItemWidth:{control:`select`,options:[`full`,`auto`]},tabItemAlignment:{control:`select`,options:[`start`,`center`,`end`]},behavior:{control:`select`,options:[`scrollbar`,`arrows`]},initialSelectedIndex:{control:`number`},initialSelectedMode:{control:`select`,options:[`auto`,`manually`]},label:{control:`text`},tabs:{control:`object`},arrowScrollDistance:{control:`number`},id:{control:`text`},autofocus:{control:`boolean`},onIndexChange:{action:`onIndexChange`},onTabSelect:{action:`onTabSelect`}}},f={args:{children:(0,l.jsxs)(l.Fragment,{children:[(0,l.jsxs)(n,{children:[(0,l.jsx)(c,{endSlot:(0,l.jsx)(i,{semantic:`informational`,children:`134`}),children:`Messages`}),(0,l.jsx)(c,{endSlot:(0,l.jsx)(i,{semantic:`neutral`,children:`433`}),children:`Notifications and very long content`}),(0,l.jsx)(c,{children:`Settings`})]}),(0,l.jsx)(r,{children:`Messages content`}),(0,l.jsx)(r,{children:`Notifications content`}),(0,l.jsx)(r,{children:`Settings content`})]})},render:e=>(0,l.jsxs)(`div`,{className:`fit-content-container`,children:[(0,l.jsx)(a,{icon:`none`,size:`small`,semantic:`informational`,children:`Horizontal:`}),(0,l.jsx)(o,{...e})]})},p={args:{orientation:`vertical`,children:(0,l.jsxs)(l.Fragment,{children:[(0,l.jsxs)(n,{children:[(0,l.jsx)(c,{endSlot:(0,l.jsx)(i,{semantic:`informational`,children:`134`}),children:`Messages`}),(0,l.jsx)(c,{endSlot:(0,l.jsx)(i,{semantic:`neutral`,children:`433`}),children:`Notifications and very long content`}),(0,l.jsx)(c,{children:`Settings`})]}),(0,l.jsx)(r,{children:`Messages content`}),(0,l.jsx)(r,{children:`Notifications content`}),(0,l.jsx)(r,{children:`Settings content`})]})},render:e=>(0,l.jsxs)(`div`,{className:`fit-content-container`,children:[(0,l.jsx)(a,{icon:`none`,size:`small`,semantic:`informational`,children:`Vertical:`}),(0,l.jsx)(o,{...e})]})},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  args: {
+    "children": <><DBTabList><DBTabItem endSlot={<DBBadge semantic="informational">134</DBBadge>}>
+                            Messages
+                        </DBTabItem><DBTabItem endSlot={<DBBadge semantic="neutral">433</DBBadge>}>
+                            Notifications and very long content
+                        </DBTabItem><DBTabItem>Settings</DBTabItem></DBTabList><DBTabPanel>Messages content</DBTabPanel><DBTabPanel>Notifications content</DBTabPanel><DBTabPanel>Settings content</DBTabPanel></>
+  },
+  render: (properties: any) => <div className="fit-content-container"><DBInfotext icon="none" size="small" semantic="informational">
+                    Horizontal:
+                </DBInfotext><DBTabs {...properties} /></div>
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    "orientation": "vertical",
+    "children": <><DBTabList><DBTabItem endSlot={<DBBadge semantic="informational">134</DBBadge>}>
+                            Messages
+                        </DBTabItem><DBTabItem endSlot={<DBBadge semantic="neutral">433</DBBadge>}>
+                            Notifications and very long content
+                        </DBTabItem><DBTabItem>Settings</DBTabItem></DBTabList><DBTabPanel>Messages content</DBTabPanel><DBTabPanel>Notifications content</DBTabPanel><DBTabPanel>Settings content</DBTabPanel></>
+  },
+  render: (properties: any) => <div className="fit-content-container"><DBInfotext icon="none" size="small" semantic="informational">
+                    Vertical:
+                </DBInfotext><DBTabs {...properties} /></div>
+}`,...p.parameters?.docs?.source}}},m=[`Horizontal`,`Vertical`]}))();export{f as Horizontal,p as Vertical,m as __namedExportsOrder,d as default};
