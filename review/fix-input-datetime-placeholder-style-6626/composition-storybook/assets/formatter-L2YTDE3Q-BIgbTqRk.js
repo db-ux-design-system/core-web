2b446f7e062c0aa12fe99312ef8c32183a37421d
nmerget <nmerget@users.noreply.github.com>
2026-07-16 13:48:49 +0000

    deploy: d28030726cee83c376677069b4fc7dd49c560391

---
+++ review/fix-input-datetime-placeholder-style-6626/composition-storybook/assets/formatter-L2YTDE3Q-BIgbTqRk.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CdIaqvlT.js";import{_ as t,d as n,f as r,g as i}from"./iframe-DWpuMCoW.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DTIqNUK2.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
