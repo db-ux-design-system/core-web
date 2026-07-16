fc927061ea516e17105102ed54e17b7e6f1d30d6
mfranzke <mfranzke@users.noreply.github.com>
2026-07-16 06:36:27 +0000

    deploy: 9f3fc7b59845f76469dd0dfb6bd3fce83305b8c0

---
+++ review/refactor-tabs-remove-obsolete-code/react-storybook/assets/formatter-L2YTDE3Q-DHjLEfWA.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-By1bm7X1.js";import{_ as t,d as n,f as r,g as i}from"./iframe-TW35C63t.js";import{n as a,t as o}from"./chunk-DRQKCCAG-Ct-kLwb-.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
