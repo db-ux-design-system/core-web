107eff5f2e916f40ab89a97649b97d0dc3c53948
mfranzke <mfranzke@users.noreply.github.com>
2026-07-16 05:32:58 +0000

    deploy: 52c3b94a51da1aecbf67cb050b81e2ff93ce96bf

---
+++ review/dependabot-npm_and_yarn-patch-dependencies-f436965d76/react-storybook/assets/formatter-L2YTDE3Q-BsETUOKN.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DESVifaE.js";import{_ as t,d as n,f as r,g as i}from"./iframe-AkZ2CUqA.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DYGfvNS-.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
