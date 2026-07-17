c5b60eee569c6b6958d27aa4bcb87707cd7d5d89
nmerget <nmerget@users.noreply.github.com>
2026-07-17 05:49:01 +0000

    deploy: e88cf250aae6ef1225fc22a91e1be3b3f6df3185

---
+++ review/dependabot-npm_and_yarn-typescript-3c5f774e48/react-storybook/assets/size.example.stories-DP6_eRI6.js
@@ -0,0 +1,12 @@
+import{i as e}from"./preload-helper-dRrCoIae.js";import{t}from"./jsx-runtime-DEC2EYNe.js";import{t as n,wt as r}from"./src-DN2sW5C7.js";var i,a,o,s,c,l;e((()=>{n(),i=t(),{fn:a}=__STORYBOOK_MODULE_TEST__,o={title:`Components/DBBadge/Size`,component:r,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{emphasis:{control:`select`,options:[`weak`,`strong`]},semantic:{control:`select`,options:[`adaptive`,`neutral`,`critical`,`informational`,`warning`,`successful`]},size:{control:`select`,options:[`small`,`medium`]},placement:{control:`select`,options:[`inline`,`corner-top-left`,`corner-top-right`,`corner-center-left`,`corner-center-right`,`corner-bottom-left`,`corner-bottom-right`]},label:{control:`text`},text:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`}}},s={args:{children:`(Default) Small`},render:e=>(0,i.jsx)(r,{...e})},c={args:{size:`medium`,children:`Medium`},render:e=>(0,i.jsx)(r,{...e})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "children": "(Default) Small"
+  },
+  render: (properties: any) => <DBBadge {...properties} />
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "size": "medium",
+    "children": "Medium"
+  },
+  render: (properties: any) => <DBBadge {...properties} />
+}`,...c.parameters?.docs?.source}}},l=[`DefaultSmall`,`Medium`]}))();export{s as DefaultSmall,c as Medium,l as __namedExportsOrder,o as default};
