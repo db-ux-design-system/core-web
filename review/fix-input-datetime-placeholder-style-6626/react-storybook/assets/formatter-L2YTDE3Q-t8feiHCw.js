5163802d6150911f5b9afcc8eadf7476c5779a8c
mfranzke <mfranzke@users.noreply.github.com>
2026-07-20 08:43:35 +0000

    deploy: 4576ed0143180d31d56ea4b70b1a93a6180f8513

---
+++ review/fix-input-datetime-placeholder-style-6626/react-storybook/assets/formatter-L2YTDE3Q-t8feiHCw.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-C60UaCHX.js";import{_ as t,d as n,f as r,g as i}from"./iframe-CaWh16lI.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DOG3K5NA.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
