28f74d1b88a8d58b8c6a3a345e990be8ab235479
mfranzke <mfranzke@users.noreply.github.com>
2026-07-22 15:13:35 +0000

    deploy: 1b90ca4400db57592863acac14e80dc911d5789c

---
+++ review/7347-dblink-underline-of-external-link-icon-is-misaligned-at-110-browser-zoom/vue-storybook/assets/formatter-L2YTDE3Q-fBGhdazp.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DtQjRLtD.js";import{H as t,K as n,V as r,q as i}from"./iframe-CRTFquDm.js";import{n as a,t as o}from"./chunk-DRQKCCAG-D8T_m_se.js";var s;e((()=>{o(),i(),t(),s=(0,r(a(),1).default)(2)(async(e,t)=>e===!1?t:n(t))}))();export{s as formatter};
