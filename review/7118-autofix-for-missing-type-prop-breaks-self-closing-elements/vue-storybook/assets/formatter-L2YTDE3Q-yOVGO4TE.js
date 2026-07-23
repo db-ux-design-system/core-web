26bc7525e8261d4cad0c0e6422baa97d5b5d67de
mfranzke <mfranzke@users.noreply.github.com>
2026-07-23 17:38:17 +0000

    deploy: bd32022e2e8bc71244229c08171c0cabf03544a2

---
+++ review/7118-autofix-for-missing-type-prop-breaks-self-closing-elements/vue-storybook/assets/formatter-L2YTDE3Q-yOVGO4TE.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DNSX3-F6.js";import{H as t,K as n,V as r,q as i}from"./iframe-DslsZIvp.js";import{n as a,t as o}from"./chunk-DRQKCCAG-C58sEfcy.js";var s;e((()=>{o(),i(),t(),s=(0,r(a(),1).default)(2)(async(e,t)=>e===!1?t:n(t))}))();export{s as formatter};
