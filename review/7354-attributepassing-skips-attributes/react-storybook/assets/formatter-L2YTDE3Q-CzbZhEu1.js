dbee74849f5d335372f5d791b762393e97f49056
mfranzke <mfranzke@users.noreply.github.com>
2026-07-23 06:35:30 +0000

    deploy: 13018d1ca92d883e5319edf0a49b6197680b67d7

---
+++ review/7354-attributepassing-skips-attributes/react-storybook/assets/formatter-L2YTDE3Q-CzbZhEu1.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-BEM2D_Sk.js";import{_ as t,d as n,f as r,g as i}from"./iframe-CY8tXn_F.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DPE2-brE.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
---
+++ review/7354-attributepassing-skips-attributes/vue-storybook/assets/formatter-L2YTDE3Q-Bf8t067h.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CDhdJpfe.js";import{H as t,K as n,V as r,q as i}from"./iframe-ynxjO485.js";import{n as a,t as o}from"./chunk-DRQKCCAG-vckNailU.js";var s;e((()=>{o(),i(),t(),s=(0,r(a(),1).default)(2)(async(e,t)=>e===!1?t:n(t))}))();export{s as formatter};
