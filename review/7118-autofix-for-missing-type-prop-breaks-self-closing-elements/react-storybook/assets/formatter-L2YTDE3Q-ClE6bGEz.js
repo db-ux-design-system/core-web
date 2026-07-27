6f00295c738cf089955d31e81db45284164c1380
mfranzke <mfranzke@users.noreply.github.com>
2026-07-27 09:47:28 +0000

    deploy: 1565ca91de60837ac6a0f1cf693dcb91f11c4e5f

---
+++ review/7118-autofix-for-missing-type-prop-breaks-self-closing-elements/react-storybook/assets/formatter-L2YTDE3Q-ClE6bGEz.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-DJNLoL5p.js";import{_ as t,d as n,f as r,g as i}from"./iframe-ehC-Ncc_.js";import{n as a,t as o}from"./chunk-DRQKCCAG-Bzp3zMzB.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
