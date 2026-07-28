239cfb1a6d46f4a3c6730ab6fa9af48d4201ddbd
mfranzke <mfranzke@users.noreply.github.com>
2026-07-28 16:21:59 +0000

    deploy: 2ac81f7cfdd9e515450c09c8ff3e1e9961854c82

--- review/refactor-drawer-replaced-js-by-baseline-css-declarations/react-storybook/sb-manager/globals-runtime.js
+++ review/refactor-drawer-replaced-js-by-baseline-css-declarations/react-storybook/sb-manager/globals-runtime.js
@@ -14522,8 +14522,7 @@ var import_react13, import_memoizerific2, supportedLanguages, themedSyntax, Wrap
         display: "flex",
         flexWrap: "wrap",
         overflow: "hidden",
-        color: theme3.color.defaultText,
-        colorScheme: theme3.base
+        color: theme3.color.defaultText
       }),
       ({ theme: theme3, bordered }) => bordered ? {
         border: `1px solid ${theme3.appBorderColor}`,
@@ -63119,7 +63118,7 @@ init_dist();
 var import_memoizerific9 = __toESM(require_memoizerific(), 1), import_semver = __toESM(require_semver2(), 1);
 
 // src/manager-api/version.ts
-var version = "10.5.4";
+var version = "10.5.2";
 
 // src/manager-api/modules/versions.ts
 var { VERSIONCHECK } = scope, getVersionCheckData = (0, import_memoizerific9.default)(1)(() => {
