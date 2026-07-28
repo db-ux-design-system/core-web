36f091e332ce8a868dcc03e8846ac50bfd9af4e6
mfranzke <mfranzke@users.noreply.github.com>
2026-07-28 06:09:19 +0000

    deploy: fb86eee780584cb223f403f1be013f28266118bb

---
+++ review/7371-patternhub-close-button-in-drawer-doesnt-have-a-text/react-storybook/assets/formatter-L2YTDE3Q-BE1Wse-M.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-BCEpL7Bp.js";import{_ as t,d as n,f as r,g as i}from"./iframe-CI1z3vrJ.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DCQQLX-p.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
