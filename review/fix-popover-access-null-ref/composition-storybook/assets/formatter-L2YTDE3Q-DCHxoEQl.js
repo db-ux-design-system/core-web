150e75a4b14d37509d3e64726f749ea88ca5a178
mfranzke <mfranzke@users.noreply.github.com>
2026-07-28 11:08:57 +0000

    deploy: 2004ee53e1330f110abc8ef6cfa1cab18da81fb4

---
+++ review/fix-popover-access-null-ref/composition-storybook/assets/formatter-L2YTDE3Q-DCHxoEQl.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-CiW3_q28.js";import{_ as t,d as n,f as r,g as i}from"./iframe-DArnrhDn.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DKVPcxgl.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
