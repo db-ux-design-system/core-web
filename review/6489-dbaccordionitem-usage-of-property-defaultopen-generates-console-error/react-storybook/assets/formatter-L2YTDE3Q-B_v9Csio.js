cfcc7c2e75b9974aa2b602894b6a98c88f2f3c37
mfranzke <mfranzke@users.noreply.github.com>
2026-07-29 15:27:04 +0000

    deploy: 50fce3a9734b25ce87ea443010979d87d750a01a

--- review/6489-dbaccordionitem-usage-of-property-defaultopen-generates-console-error/react-storybook/assets/formatter-L2YTDE3Q-B_v9Csio.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-5_4P3HKN.js";import{_ as t,d as n,f as r,g as i}from"./iframe-CKdl4-ii.js";import{n as a,t as o}from"./chunk-DRQKCCAG-DF4bf5WZ.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
