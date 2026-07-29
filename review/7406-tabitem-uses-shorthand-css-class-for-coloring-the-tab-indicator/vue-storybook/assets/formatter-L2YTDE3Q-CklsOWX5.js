c1cf209c687a0c5cd288b50d494163165594448d
mfranzke <mfranzke@users.noreply.github.com>
2026-07-29 09:53:24 +0000

    deploy: da06c753a590601717cd9b1e6c54a95cb03d8d23

--- review/7406-tabitem-uses-shorthand-css-class-for-coloring-the-tab-indicator/vue-storybook/assets/formatter-L2YTDE3Q-CklsOWX5.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-CnNcC60R.js";import{H as t,K as n,V as r,q as i}from"./iframe-Crj8IqNE.js";import{n as a,t as o}from"./chunk-DRQKCCAG-ifVzM0uH.js";var s;e((()=>{o(),i(),t(),s=(0,r(a(),1).default)(2)(async(e,t)=>e===!1?t:n(t))}))();export{s as formatter};
