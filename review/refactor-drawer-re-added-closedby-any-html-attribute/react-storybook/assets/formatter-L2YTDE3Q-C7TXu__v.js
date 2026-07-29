5475cfde485609dc8f3c03d8e2c1abcfed489208
mfranzke <mfranzke@users.noreply.github.com>
2026-07-29 14:34:35 +0000

    deploy: 5849902a6835c14cb2918d98fe27dcd31756867e

---
+++ review/refactor-drawer-re-added-closedby-any-html-attribute/react-storybook/assets/formatter-L2YTDE3Q-C7TXu__v.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-BRjlzLLI.js";import{_ as t,d as n,f as r,g as i}from"./iframe-DpWl8uBu.js";import{n as a,t as o}from"./chunk-DRQKCCAG-b1MGYrgB.js";var s;e((()=>{o(),t(),r(),s=(0,n(a(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
