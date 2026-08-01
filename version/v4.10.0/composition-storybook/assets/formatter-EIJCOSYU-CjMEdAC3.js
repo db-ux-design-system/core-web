51f396843d34c6dca4abff4c6dc36e8e23fa7e2f
nmerget <nmerget@users.noreply.github.com>
2026-08-01 02:35:55 +0000

    deploy: 3da4fc56dfbfac43fd86d3d39cb86cdc2ed34eae

--- version/v4.10.0/composition-storybook/assets/formatter-EIJCOSYU-CjMEdAC3.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-DNTV9o91.js";import{m as t,p as n}from"./iframe-BQLQNQmF.js";import{D as r,E as i,T as a,w as o}from"./blocks-CkeO_0qe.js";var s;e((()=>{i(),a(),t(),s=(0,n(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
