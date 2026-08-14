d51054dfc44fd405bbefb91283cbcc1c3cea32ba
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2026-08-14 11:47:53 +0200

    refactor: restore the default export for this Next.js page

--- showcases/patternhub/pages/foundations/icons/overview.tsx
+++ showcases/patternhub/pages/foundations/icons/overview.tsx
@@ -1,12 +1,14 @@
 import DefaultPage from '../../../components/default-page';
 
-export const IconOverview = () => (
-	<DefaultPage>
-		<h1>Icon overview</h1>
-		<p>
-			Please use the [icon
-			overview](https://design-system.deutschebahn.com/documentation/icons/)
-			within our main documentation.
-		</p>
-	</DefaultPage>
-);
+export default function IconOverview() {
+	return (
+		<DefaultPage>
+			<h1>Icon overview</h1>
+			<p>
+				Please use the [icon
+				overview](https://design-system.deutschebahn.com/documentation/icons/)
+				within our main documentation.
+			</p>
+		</DefaultPage>
+	);
+}
