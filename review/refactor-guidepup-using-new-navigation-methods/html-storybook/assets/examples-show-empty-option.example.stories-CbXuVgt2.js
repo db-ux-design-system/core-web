a79ddffad2acb65051c62117a95a3c1d2fd72f3e
nmerget <nmerget@users.noreply.github.com>
2026-08-14 09:09:00 +0000

    deploy: ec659bece4bf22d5cb26670c96202f4acd2a095c

---
+++ review/refactor-guidepup-using-new-navigation-methods/html-storybook/assets/examples-show-empty-option.example.stories-CbXuVgt2.js
@@ -0,0 +1,40 @@
+import{n as e}from"./iframe-CF6fiXBv.js";import{n as t,t as n}from"./select-CVvF7Prt.js";import{n as r}from"./rolldown-runtime-DkW27tQK.js";var i,a,o,s,c,l;function u(){return(u=r((()=>{t(),i=e(),{fn:a}=__STORYBOOK_MODULE_TEST__,o={title:`Components/DBSelect/Examples showEmptyOption property`,component:n,parameters:{layout:`centered`},tags:[`autodocs`],args:{onClick:a()},argTypes:{options:{control:`object`},label:{control:`text`},placeholder:{control:`text`},variant:{control:`select`,options:[`above`,`floating`]},value:{control:`text`},showLabel:{control:`boolean`},message:{control:`text`},showMessage:{control:`boolean`},validation:{control:`select`,options:[`invalid`,`valid`,`no-validation`]},invalidMessage:{control:`text`},validMessage:{control:`text`},disabled:{control:`boolean`},showIcon:{control:`boolean`},required:{control:`boolean`},showRequiredAsterisk:{control:`boolean`},name:{control:`text`},form:{control:`text`},ariaDescribedBy:{control:`text`},size:{control:`number`},multiple:{control:`boolean`},showEmptyOption:{control:`boolean`},autocomplete:{control:`text`},messageIcon:{control:`text`},id:{control:`text`},autofocus:{control:`boolean`},onClick:{action:`onClick`}}},s={args:{label:`Label`,placeholder:`(Default)`,options:[{value:`Option 1`},{value:`Option 2`},{value:`Option 3`},{value:`Option 4`},{value:`Option 5`}]},render:e=>(0,i.jsx)(`div`,{style:{width:`300px`},children:(0,i.jsx)(n,{...e})})},c={args:{label:`Label`,placeholder:`showEmptyOption=false`,options:[{value:`Option 1`},{value:`Option 2`},{value:`Option 3`},{value:`Option 4`},{value:`Option 5`}],showEmptyOption:!1},render:e=>(0,i.jsx)(`div`,{style:{width:`300px`},children:(0,i.jsx)(n,{...e})})},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Label",
+    "placeholder": "(Default)",
+    "options": [{
+      value: 'Option 1'
+    }, {
+      value: 'Option 2'
+    }, {
+      value: 'Option 3'
+    }, {
+      value: 'Option 4'
+    }, {
+      value: 'Option 5'
+    }]
+  },
+  render: (properties: any) => <div style={{
+    width: '300px'
+  }}><DBSelect {...properties} /></div>
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    "label": "Label",
+    "placeholder": "showEmptyOption=false",
+    "options": [{
+      value: 'Option 1'
+    }, {
+      value: 'Option 2'
+    }, {
+      value: 'Option 3'
+    }, {
+      value: 'Option 4'
+    }, {
+      value: 'Option 5'
+    }],
+    "showEmptyOption": false
+  },
+  render: (properties: any) => <div style={{
+    width: '300px'
+  }}><DBSelect {...properties} /></div>
+}`,...c.parameters?.docs?.source}}},l=[`Default`,`showEmptyOptionfalse`]})))()}u();export{s as Default,l as __namedExportsOrder,o as default,c as showEmptyOptionfalse};
---
+++ review/refactor-guidepup-using-new-navigation-methods/vue-storybook/assets/variant.example.stories-D5lVkvRZ.js
@@ -0,0 +1,90 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,t as n}from"./infotext-C_e81XZV.js";import{n as r,t as i}from"./divider-BojRJL7I.js";import{n as a,t as o}from"./stack-B-jJH1si.js";var s,c,l,u,d;function f(){return(f=e((()=>{r(),t(),o(),{fn:s}=__STORYBOOK_MODULE_TEST__,c={title:`Components/DBStack/Variant`,component:a,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{variant:{control:`select`,options:[`simple`,`divider`]},gap:{control:`select`,options:[`none`,`3x-large`,`2x-large`,`x-large`,`large`,`medium`,`small`,`x-small`,`2x-small`,`3x-small`]},direction:{control:`select`,options:[`row`,`column`]},wrap:{control:`boolean`},alignment:{control:`select`,options:[`stretch`,`start`,`end`,`center`]},justifyContent:{control:`select`,options:[`space-between`,`start`,`end`,`center`]},id:{control:`text`},autofocus:{control:`boolean`}}},l={args:{style:{padding:`var(--db-spacing-fixed-xs)`},default:`<span class="dummy-component"><a href="#">Content 1</a></span
+><span class="dummy-component">Content 2</span
+><span class="dummy-component">Content 3</span>`},render:e=>({components:{DBStack:a,DBDivider:i,DBInfotext:n},setup(){return{args:e}},template:`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  width: '200px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    (Default) Simple
+                </DBInfotext><DBStack v-bind="args"   >${e.default}</DBStack></div>`})},u={args:{variant:`divider`,style:{padding:`var(--db-spacing-fixed-xs)`},default:`<span class="dummy-component"><a href="#">Content 1</a></span
+><DBDivider></DBDivider><span class="dummy-component">Content 2</span
+><DBDivider></DBDivider><span class="dummy-component">Content 3</span>`},render:e=>({components:{DBStack:a,DBDivider:i,DBInfotext:n},setup(){return{args:e}},template:`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  width: '200px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    Divider
+                </DBInfotext><DBStack v-bind="args"   >${e.default}</DBStack></div>`})},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "default": \`<span class="dummy-component"><a href="#">Content 1</a></span
+><span class="dummy-component">Content 2</span
+><span class="dummy-component">Content 3</span>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBStack,
+      DBDivider,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  width: '200px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    (Default) Simple
+                </DBInfotext><DBStack v-bind="args"   >\${args.default}</DBStack></div>\`
+  })
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    "variant": "divider",
+    "style": {
+      padding: 'var(--db-spacing-fixed-xs)'
+    },
+    "default": \`<span class="dummy-component"><a href="#">Content 1</a></span
+><DBDivider></DBDivider><span class="dummy-component">Content 2</span
+><DBDivider></DBDivider><span class="dummy-component">Content 3</span>\`
+  },
+  render: (args: any) => ({
+    components: {
+      DBStack,
+      DBDivider,
+      DBInfotext
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<div  :style="{
+  alignItems: 'flex-start',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexWrap: 'nowrap',
+  flexDirection: 'column',
+  gap: 'var(--db-spacing-fixed-sm)',
+  width: '200px'
+}"  ><DBInfotext size="small" icon="none" semantic="informational"   >
+                    Divider
+                </DBInfotext><DBStack v-bind="args"   >\${args.default}</DBStack></div>\`
+  })
+}`,...u.parameters?.docs?.source}}},d=[`DefaultSimple`,`Divider`]})))()}f();export{l as DefaultSimple,u as Divider,d as __namedExportsOrder,c as default};
