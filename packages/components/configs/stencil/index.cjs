ad717a3a73ccf08cf89644db9c73b2a53621e1da
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2026-08-23 14:31:01 +0200

    refactor: removed unnecessary plugin
    this is now provided by Stencil types directly

--- packages/components/configs/stencil/index.cjs
+++ packages/components/configs/stencil/index.cjs
@@ -4,7 +4,6 @@ const slotsPlugin = require('../plugins/stencil/slots.cjs');
 const stencilPlugin = require('../plugins/stencil/index.cjs');
 const esmExtensionsPlugin = require('../plugins/esm-extensions.cjs');
 const focusgroupPlugin = require('../plugins/stencil/focusgroup.cjs');
-const closedbyPlugin = require('../plugins/stencil/closedby.cjs');
 const requestClosePlugin = require('../plugins/stencil/request-close.cjs');
 
 /**
@@ -23,7 +22,6 @@ module.exports = {
 		onClickPlugin,
 		esmExtensionsPlugin,
 		focusgroupPlugin,
-		closedbyPlugin,
 		requestClosePlugin
 	]
 };
