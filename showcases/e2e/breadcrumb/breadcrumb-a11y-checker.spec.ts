0ddbe08ee47e8cd1a5f7f0d02d29ce56bb0e3c78
Nicolas Merget <nicolas.merget@deutschebahn.com>
2026-09-23 13:05:57 +0200

    chore: ignore false positive for test

--- showcases/e2e/breadcrumb/breadcrumb-a11y-checker.spec.ts
+++ showcases/e2e/breadcrumb/breadcrumb-a11y-checker.spec.ts
@@ -1,6 +1,11 @@
 import { test } from '@playwright/test';
 import { runA11yCheckerTest } from '../default.ts';
 
+const aCheckerDisableRules = ['aria_attribute_valid']; // TODO: This is a false positive -> add an issue in https://github.com/IBMa/equal-access
+
 test.describe('DBBreadcrumb', () => {
-	runA11yCheckerTest({ path: '05/breadcrumb' });
+	runA11yCheckerTest({
+		path: '05/breadcrumb',
+		aCheckerDisableRules
+	});
 });
